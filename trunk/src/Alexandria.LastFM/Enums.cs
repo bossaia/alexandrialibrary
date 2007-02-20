706ef107a4bcda573bc44141f594ab04a539c31f
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-02-20 17:52:35 +0000

    Added a folder remotely

---
+++ trunk/src/Alexandria.LastFM/Enums.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlexandriaOrg.Alexandria.LastFM
+{
+	#region State
+	public enum State
+	{
+		IDLE,
+		NEED_HANDSHAKE,
+		NEED_TRANSMIT,
+		WAITING_FOR_REQ_STREAM,
+		WAITING_FOR_HANDSHAKE_RESP,
+		WAITING_FOR_RESP
+	};
+	#endregion
+}
---
+++ trunk/src/Alexandria.LastFM/TransmitState.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlexandriaOrg.Alexandria.LastFM
+{
+	/// <summary>
+	/// Async code for transmitting the current queue of tracks
+	/// </summary>
+	public class TransmitState
+	{
+		public StringBuilder StringBuilder;
+		public int Count;
+	}
+}
