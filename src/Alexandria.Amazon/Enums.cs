cc76efffa29102cc1d56a705625139c8f4f542d0
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-02-20 17:33:04 +0000

    Added a folder remotely

---
+++ src/Alexandria.Amazon/Enums.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlexandriaOrg.Alexandria.Amazon
+{
+	#region SearchMode
+	public enum SearchMode
+	{
+		Books = 0,
+		Music,
+		DVDs
+	}
+	#endregion
+}
---
+++ src/Alexandria.Amazon/WebServiceClient.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AlexandriaOrg.Alexandria;
+
+namespace AlexandriaOrg.Alexandria.Amazon
+{
+	public class WebServiceClient
+	{
+		#region Constructors
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public WebServiceClient()
+		{
+		}
+		#endregion
+		
+		#region Private Fields
+		private const string wsdlPath = "http://soap.amazon.com/schemas3/AmazonWebServices.wsdl";
+		private const string accessKeyId = "0NN3FG2840KM384025G2";
+		#endregion
+		
+		#region Public Methods
+		/// <summary>
+		/// A Basic Amazon Search
+		/// </summary>
+		/// <param name="mode">The search mode to use</param>
+		/// <param name="search">The search</param>
+		/// <see cref="http://msdn.microsoft.com/coding4fun/web/services/article.aspx?articleid=912260"/>
+		public string BasicSearch(SearchMode mode, string search)
+		{
+			System.Text.StringBuilder details = new StringBuilder("Search Results:\n\n");
+		
+			try
+			{
+				AmazonWebService.KeywordRequest request = new AmazonWebService.KeywordRequest();
+				request.locale = "us";
+				request.type = "lite";
+				request.sort = "reviewrank";
+				request.mode = mode.ToString();
+				request.keyword = search;
+				request.tag = accessKeyId;
+				request.devtag = accessKeyId;
+				
+				AmazonWebService.AmazonSearchService searchService = new AlexandriaOrg.Alexandria.Amazon.AmazonWebService.AmazonSearchService();
+				AmazonWebService.ProductInfo info = searchService.KeywordSearchRequest(request);
+				foreach(AmazonWebService.Details detail in info.Details)
+				{
+					details.Append(detail.ProductName + " [ ASIN: " + detail.Asin);					
+					if (!string.IsNullOrEmpty(detail.OurPrice))
+						details.Append(" Amazon: " + detail.OurPrice);
+					int usedCount;
+					int.TryParse(detail.UsedCount, out usedCount);
+					if (usedCount > 0 && !string.IsNullOrEmpty(detail.UsedPrice))
+						details.Append(", Used: " + detail.UsedPrice);
+					details.Append("]\n");
+					
+					System.Diagnostics.Debug.WriteLine("Product Name: " + detail.ProductName);
+				}
+				
+				return details.ToString();
+			}
+			catch (Exception ex)
+			{
+				throw new AlexandriaException("The was an error attempting to search Amazon", ex);
+			}
+		}
+		#endregion
+	}
+}
