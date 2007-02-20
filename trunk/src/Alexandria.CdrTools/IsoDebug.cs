572bb1a30be728744847f26a0e9657b96e5ede79
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-02-20 17:39:19 +0000

    Added a folder remotely

---
+++ trunk/src/Alexandria.CdrTools/IsoDebug.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AlexandriaOrg.Alexandria;
+
+namespace AlexandriaOrg.Alexandria.CdrTools
+{
+	public class IsoDebug : ConsoleTool
+	{
+		#region Constructors
+		public IsoDebug(string path) : base(path)
+		{
+		}
+		#endregion
+
+		#region Protected Methods
+		protected override void Execute()
+		{
+			//base.Execute();
+		}
+		#endregion
+	}
+}
