8be633e50f5958686e9d2ade2ef43cd75fb99676
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-02-20 17:39:19 +0000

    Added a folder remotely

---
+++ src/Alexandria.CdrTools/MakeIsoFileSystem.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AlexandriaOrg.Alexandria;
+
+namespace AlexandriaOrg.Alexandria.CdrTools
+{
+	public class MakeIsoFileSystem : ConsoleTool
+	{
+		#region Constructors
+		public MakeIsoFileSystem(string path) : base(path)
+		{
+		}
+		#endregion
+
+		#region Protected Methods
+		protected override void Execute()
+		{
+			//base.Execute();
+		}
+		#endregion
+	}
+}
