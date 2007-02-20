aa7b5031808c9355c4bc17bbffe7190d66b1ba86
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-02-20 17:35:59 +0000

    Added a folder remotely

---
+++ src/Alexandria.AsciiGenerator/Version.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlexandriaOrg.Alexandria.AsciiGenerator
+{
+	/// <summary>
+	/// Class Holding the current application version
+	/// </summary>
+	public abstract class Version
+	{
+		/// <summary>
+		/// Build and return the current application version
+		/// </summary>
+		/// <returns>The current version as a string</returns>
+		public static string GetVersion()
+		{
+			return Major.ToString() + "." + Minor.ToString() + "." + Patch.ToString() + Suffix;
+		}
+
+		/// <summary>Major version number</summary>
+		public const int Major = 0;
+
+		/// <summary>Minor version number</summary>
+		public const int Minor = 7;
+
+		/// <summary>Patch version number</summary>
+		public const int Patch = 2;
+
+		/// <summary>Version Suffix</summary>
+		public const string Suffix = "";
+
+		/// <summary>Version Suffix Number</summary>
+		public const int SuffixNumber = 1;
+	}
+}
