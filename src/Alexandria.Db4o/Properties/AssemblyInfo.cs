eb0fc2bad076fb7c36f2cde358769a7b98e3bba7
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-02-20 17:42:17 +0000

    Added a folder remotely

---
+++ src/Alexandria.Db4o/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+[assembly: CLSCompliant(true)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution = true, UnmanagedCode = true)]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AlexandriaOrg.Alexandria.Db4o")]
+[assembly: AssemblyDescription("Alexandria Db4o Object Database Plugin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The Alexandria Foundation")]
+[assembly: AssemblyProduct("AlexandriaOrg.Alexandria.Db4o")]
+[assembly: AssemblyCopyright("Copyright ©  2006")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7e3b29b9-6a5b-4fe1-a239-e8fd2a672c94")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
