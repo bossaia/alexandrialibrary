02f9cbb9dd111effd0a06cb003ad06b63fd70165
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-02-20 17:58:37 +0000

    Added a folder remotely

---
+++ trunk/src/Alexandria.Server/AlexandriaService.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+
+namespace AlexandriaOrg.Alexandria.Server
+{
+	public partial class AlexandriaService : ServiceBase
+	{
+		#region Constructors
+		public AlexandriaService()
+		{
+			InitializeComponent();
+		}
+		#endregion
+
+		#region Protected Methods
+		protected override void OnStart(string[] args)
+		{
+			// TODO: Add code here to start your service.
+		}
+
+		protected override void OnStop()
+		{
+			// TODO: Add code here to perform any tear-down necessary to stop your service.
+		}
+		#endregion
+	}
+}
