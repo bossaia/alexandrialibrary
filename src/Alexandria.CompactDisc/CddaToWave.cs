3fdf0e9438b60d27c845b9ead02b39c92507087b
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-03-04 23:55:58 +0000

    \src\AlexandriaLibrary.sln \src\Alexandria\Alexandria.csproj \src\Alexandria\CompactDisc.cs \src\Alexandria\CDTableOfContents.cs \src\Alexandria.CompactDisc \src\Alexandria.CompactDisc\Alexandria.CompactDisc.csproj \src\Alexandria.CompactDisc\Properties \src\Alexandria.CompactDisc\Properties\AssemblyInfo.cs \src\Alexandria.CompactDisc\CddaToWave.cs \src\Alexandria.CompactDisc\CDRecord.cs \src\Alexandria.CompactDisc\CDTableOfContents.cs \src\Alexandria.CompactDisc\DeviceDump.cs \src\Alexandria.CompactDisc\Disc.cs \src\Alexandria.CompactDisc\IsoDebug.cs \src\Alexandria.CompactDisc\IsoDump.cs \src\Alexandria.CompactDisc\IsoInfo.cs \src\Alexandria.CompactDisc\IsoVerify.cs \src\Alexandria.CompactDisc\MakeIsoFileSystem.cs \src\Alexandria.CompactDisc\ReadCD.cs Added new CompactDisc project and moved CdrTools classes there

---
+++ src/Alexandria.CompactDisc/CddaToWave.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Alexandria;
+
+namespace AlexandriaOrg.Alexandria.CdrTools
+{
+	public class CddaToWave : ConsoleTool
+	{
+		#region Constructors
+		public CddaToWave(string path) : base(path)
+		{
+		}
+		#endregion
+		
+		#region Protected Methods
+		protected override void Execute()
+		{
+			//base.Execute();
+		}
+		#endregion
+	}
+}
---
+++ src/Alexandria.CompactDisc/Disc.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Alexandria;
+
+namespace Alexandria.CompactDisc
+{
+	public abstract class Disc : IResource
+	{
+		#region IResource Members
+
+		public ResourceFormat Format
+		{
+			get { throw new Exception("The method or operation is not implemented."); }
+		}
+
+		public Uri Uri
+		{
+			get { throw new Exception("The method or operation is not implemented."); }
+		}
+
+		public IDictionary<object, IResource> Resources
+		{
+			get { throw new Exception("The method or operation is not implemented."); }
+		}
+
+		#endregion
+	}
+}
