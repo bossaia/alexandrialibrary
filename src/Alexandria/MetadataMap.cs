00ce40b732f1e8c906fb8927cf4156b69fc237b3
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-03-18 08:09:07 +0000

    \src\Alexandria\Alexandria.csproj \src\Alexandria\MetadataMap.cs Created the MetadataMap class

---
+++ src/Alexandria/MetadataMap.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexandria
+{
+	public class MetadataMap<K, V> : IDictionary<K, V>
+	{
+		#region Constructors
+		public MetadataMap()
+		{
+		}
+		
+		public MetadataMap(Guid guid, string name, Uri uri)
+		{
+			this.guid = guid;
+			this.name = name;
+			this.uri = uri;
+		}
+		#endregion
+		
+		#region Private Fields
+		private Guid guid;
+		private string name;
+		private Uri uri;
+		private IDictionary<K, V> map = new Dictionary<K,V>();
+		#endregion
+		
+		#region Public Properties
+		public Guid Guid
+		{
+			get { return guid; }
+		}
+		
+		public string Name
+		{
+			get { return name; }
+		}
+		
+		public Uri Uri
+		{
+			get { return uri; }
+		}
+		#endregion
+		
+		#region IDictionary<K,V> Members
+		public void Add(K key, V value)
+		{
+			map.Add(key, value);
+		}
+		
+		public bool ContainsKey(K key)
+		{
+			return map.ContainsKey(key);
+		}
+
+		public ICollection<K> Keys
+		{
+			get { return map.Keys; }
+		}
+
+		public bool Remove(K key)
+		{
+			return map.Remove(key);
+		}
+
+		public bool TryGetValue(K key, out V value)
+		{
+			return map.TryGetValue(key, out value);
+		}
+
+		public ICollection<V> Values
+		{
+			get { return map.Values; }
+		}
+
+		public V this[K key]
+		{
+			get { return map[key]; }
+			set { map[key] = value; }
+		}
+		#endregion
+
+		#region ICollection<KeyValuePair<K,V>> Members
+		public void Add(KeyValuePair<K, V> item)
+		{
+			map.Add(item.Key, item.Value);
+		}
+
+		public void Clear()
+		{
+			map.Clear();
+		}
+
+		public bool Contains(KeyValuePair<K, V> item)
+		{
+			return map.Contains(item);
+		}
+
+		public void CopyTo(KeyValuePair<K, V>[] array, int arrayIndex)
+		{
+			map.CopyTo(array, arrayIndex);
+		}
+
+		public int Count
+		{
+			get { return map.Count; }
+		}
+
+		public bool IsReadOnly
+		{
+			get { return map.IsReadOnly; }
+		}
+
+		public bool Remove(KeyValuePair<K, V> item)
+		{
+			return map.Remove(item.Key);
+		}
+		#endregion
+
+		#region IEnumerable<KeyValuePair<K,V>> Members
+		public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+		{
+			foreach(KeyValuePair<K, V> item in map)
+				yield return item;
+		}
+		#endregion
+
+		#region IEnumerable Members
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			foreach (KeyValuePair<K, V> item in map)
+				yield return item;
+		}
+		#endregion
+	}
+}
