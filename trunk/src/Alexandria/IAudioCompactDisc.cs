9f84813754890cef31d445976a48144f8d0e7ee5
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-04-08 19:32:03 +0000

    \src\Alexandria\Alexandria.cd \src\Alexandria\IAudioCompactDisc.cs More work on refactoring interfaces

--- trunk/src/Alexandria/IAudioCompactDisc.cs
+++ trunk/src/Alexandria/IAudioCompactDisc.cs
@@ -5,10 +5,10 @@ using System.Text;
 namespace Alexandria
 {
 	public interface IAudioCompactDisc : IResource
-	{
-		int NumberOfTracks { get; }
+	{		
 		int Minutes { get; }
 		int Seconds { get; }
 		int Frames { get; }
+		IList<IAudio> Tracks { get; }
 	}
 }
