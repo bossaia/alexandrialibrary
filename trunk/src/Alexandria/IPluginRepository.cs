d0deac6cc9e319940967578a14d8bf494216cf6d
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-05-19 08:37:31 +0000

    \src\Alexandria\Alexandria.cd \src\Alexandria\IPluginRepository.cs Simplified this interface

--- trunk/src/Alexandria/IPluginRepository.cs
+++ trunk/src/Alexandria/IPluginRepository.cs
@@ -9,11 +9,8 @@ namespace Alexandria
 		IDictionary<string, IMediaFactory> MediaFactories { get; }
 		IDictionary<string, ITagFactory> TagFactores { get; }
 		IDictionary<string, IMetadataFactory> MetadataFactories { get; }
-		IMediaFactory GetMediaFactoryFor<T>(IPluginOptions options) where T: IMedia;
-		IMediaFactory GetMediaFactoryForEach<T>(IPluginOptions options) where T: IList<IMedia>;
-		ITagFactory GetTagFactoryFor<T>(IPluginOptions options) where T: ITag;
-		ITagFactory GetTagFactoryForEach<T>(IPluginOptions options) where T : IList<ITag>;
-		IMetadataFactory GetMetadataFactoryFor<T>(IPluginOptions options) where T: IMetadata;
-		IMetadataFactory GetMetadataFactoryForEach<T>(IPluginOptions options) where T: IList<IMetadata>;
+		IMediaFactory GetMediaFactory(IPluginOptions options);
+		ITagFactory GetTagFactory(IPluginOptions options);		
+		IMetadataFactory GetMetadataFactory(IPluginOptions options);		
 	}
 }
