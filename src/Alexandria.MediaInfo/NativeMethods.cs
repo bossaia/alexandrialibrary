d9750b5cecbb0f78ed024cdece48b8b3054476a2
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-05-28 20:38:07 +0000

    \src\Alexandria.MediaInfo\Alexandria.MediaInfo.csproj \src\Alexandria.MediaInfo\Info.cs \src\Alexandria.MediaInfo\InfoOptions.cs \src\Alexandria.MediaInfo\InfoType.cs \src\Alexandria.MediaInfo\NativeMethods.cs \src\Alexandria.MediaInfo\StreamInfo.cs \src\Alexandria.MediaInfo\StreamType.cs Split enums into seperate files Renamed Info class to SteamInfo Cleaned up NativeMetods

--- src/Alexandria.MediaInfo/NativeMethods.cs
+++ src/Alexandria.MediaInfo/NativeMethods.cs
@@ -5,37 +5,40 @@ using System.Text;
 
 namespace Alexandria.MediaInfo
 {
+	/// <summary>
+	/// Import of native functions from MediaInfo.dll
+	/// </summary>
+	/// <remarks>Do NOT use CoTaskMemAlloc to allocate memory</remarks>
 	internal static class NativeMethods
 	{
-		//Import of DLL functions. DO NOT USE until you know what you do (MediaInfo DLL do NOT use CoTaskMemAlloc to allocate memory)  
 		[DllImport("MediaInfo.dll")]
-		public static extern IntPtr MediaInfo_New();
+		internal static extern IntPtr MediaInfo_New();
 		
 		[DllImport("MediaInfo.dll")]
-		public static extern void MediaInfo_Delete(IntPtr handle);
+		internal static extern void MediaInfo_Delete(IntPtr handle);
 		
 		[DllImport("MediaInfo.dll")]
-		public static extern int MediaInfo_Open(IntPtr handle, [MarshalAs(UnmanagedType.LPWStr)] string fileName);
+		internal static extern int MediaInfo_Open(IntPtr handle, [MarshalAs(UnmanagedType.LPWStr)] string fileName);
 		
 		[DllImport("MediaInfo.dll")]
-		public static extern void MediaInfo_Close(IntPtr handle);
+		internal static extern void MediaInfo_Close(IntPtr handle);
 		
 		[DllImport("MediaInfo.dll")]
-		public static extern IntPtr MediaInfo_Inform(IntPtr handle, [MarshalAs(UnmanagedType.U4)] uint reserved);
+		internal static extern IntPtr MediaInfo_Inform(IntPtr handle, [MarshalAs(UnmanagedType.U4)] uint reserved);
 		
 		[DllImport("MediaInfo.dll")]
-		public static extern IntPtr MediaInfo_GetI(IntPtr handle, [MarshalAs(UnmanagedType.U4)] StreamType streamKind, uint streamNumber, uint parameter, [MarshalAs(UnmanagedType.U4)] InfoType KindOfInfo);
+		internal static extern IntPtr MediaInfo_GetI(IntPtr handle, [MarshalAs(UnmanagedType.U4)] StreamType streamKind, uint streamNumber, uint parameter, [MarshalAs(UnmanagedType.U4)] InfoType KindOfInfo);
 		
 		[DllImport("MediaInfo.dll")]
-		public static extern IntPtr MediaInfo_Get(IntPtr handle, [MarshalAs(UnmanagedType.U4)] StreamType streamKind, uint StreamNumber, [MarshalAs(UnmanagedType.LPWStr)] string parameter, [MarshalAs(UnmanagedType.U4)] InfoType KindOfInfo, [MarshalAs(UnmanagedType.U4)] InfoType KindOfSearch);
+		internal static extern IntPtr MediaInfo_Get(IntPtr handle, [MarshalAs(UnmanagedType.U4)] StreamType streamKind, uint StreamNumber, [MarshalAs(UnmanagedType.LPWStr)] string parameter, [MarshalAs(UnmanagedType.U4)] InfoType KindOfInfo, [MarshalAs(UnmanagedType.U4)] InfoType KindOfSearch);
 		
 		[DllImport("MediaInfo.dll")]
-		public static extern IntPtr MediaInfo_Option(IntPtr handle, [MarshalAs(UnmanagedType.LPWStr)] string option, [MarshalAs(UnmanagedType.LPWStr)] string value);
+		internal static extern IntPtr MediaInfo_Option(IntPtr handle, [MarshalAs(UnmanagedType.LPWStr)] string option, [MarshalAs(UnmanagedType.LPWStr)] string value);
 		
 		[DllImport("MediaInfo.dll")]
-		public static extern int MediaInfo_State_Get(IntPtr handle);
+		internal static extern int MediaInfo_State_Get(IntPtr handle);
 		
 		[DllImport("MediaInfo.dll")]
-		public static extern int MediaInfo_Count_Get(IntPtr handle, [MarshalAs(UnmanagedType.U4)] StreamType StreamKind, int streamNumber);
+		internal static extern int MediaInfo_Count_Get(IntPtr handle, [MarshalAs(UnmanagedType.U4)] StreamType StreamKind, int streamNumber);
 	}
 }
