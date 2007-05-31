0b8e71c8f8d14c0a57108fb3fd95f134bda96466
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-05-31 06:31:19 +0000

    \src\Alexandria.Client\MainForm.cs \src\Alexandria.SQLite\SQLiteDataProvider.cs Added to SQLite test

--- src/Alexandria.SQLite/SQLiteDataProvider.cs
+++ src/Alexandria.SQLite/SQLiteDataProvider.cs
@@ -16,24 +16,29 @@ namespace Alexandria.SQLite
 			try
 			{
 				System.Data.IDbConnection cnn;
-				using(cnn = new Mono.Data.SqliteClient.SqliteConnection(@"URI=file:C:\Dev\Data\SQLite\test.db"))
+				string dbDir = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "\\Alexandria\\";
+				if (!System.IO.Directory.Exists(dbDir))
+					System.IO.Directory.CreateDirectory(dbDir);
+				string dbPath = dbDir + "Alexandria.db";
+				
+				using(cnn = new Mono.Data.SqliteClient.SqliteConnection("URI=file:" + dbPath)) //C:\Dev\Data\SQLite\test.db"))
 				{
 					cnn.Open();
 					
 					IDbCommand createTable = cnn.CreateCommand();
-					createTable.CommandText = "CREATE TABLE artist ( ArtistId TEXT PRIMARY KEY, Name TEXT NOT NULL, DateOfBirth INTEGER)";
+					createTable.CommandText = "CREATE TABLE audiotrack ( Id TEXT PRIMARY KEY, IdType TEXT NOT NULL, Name TEXT NOT NULL, Artist TEXT NOT NULL, Album TEXT NOT NULL, Duration INTEGER NOT NULL, ReleaseDate INTEGER NOT NULL, TrackNumber INTEGER NOT NULL, Format TEXT NOT NULL)";
 					createTable.ExecuteNonQuery();
 					
 					IDbCommand insertData = cnn.CreateCommand();
-					insertData.CommandText = "INSERT INTO artist (ArtistId, Name, DateOfBirth) VALUES ('AAAA', 'David Bowie', 19470108)";
+					insertData.CommandText = "INSERT INTO audiotrack (Id, IdType, Name, Artist, Album, Duration, ReleaseDate, TrackNumber, Format) VALUES ('5dc9742c-c68b-4534-b0f4-91b341ad09d3', 'MusicBrainzID', 'H.', 'Tool', 'Ænima', 369000, 1996, 3, 'ogg')";
 					insertData.ExecuteNonQuery();
 					
 					IDbCommand selectData = cnn.CreateCommand();
-					selectData.CommandText = "SELECT * FROM artist";
+					selectData.CommandText = "SELECT * FROM audiotrack";
 					IDataReader reader = selectData.ExecuteReader();
 					while(reader.Read())
 					{
-						data += "Id=" + reader.GetString(0) + " Name=" + reader.GetString(1) + "\n";
+						data += "Id=" + reader.GetString(0) + " Name=" + reader.GetString(2) + "\n";
 					}
 					
 					cnn.Close();
