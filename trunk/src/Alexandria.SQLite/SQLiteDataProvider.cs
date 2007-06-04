f150740d081919c7f5e26554ce8a7ed98c5b4bf0
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-06-04 07:43:21 +0000

    \src\Alexandria.SQLite\SQLiteDataProvider.cs Started work on the helper methods for performing database functions

--- trunk/src/Alexandria.SQLite/SQLiteDataProvider.cs
+++ trunk/src/Alexandria.SQLite/SQLiteDataProvider.cs
@@ -1,82 +1,178 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Common;
 using System.Data.SQLite;
+using System.IO;
+using System.Reflection;
 using System.Text;
-//using Mono.Data.SqliteClient;
 using Alexandria;
 
 namespace Alexandria.SQLite
 {
 	public class SQLiteDataProvider
-	{
-		public string Test()
-		{
-			string data = string.Empty;
-		
-			try
-			{
-				System.Data.Common.DbConnection cnn;
-				string dbDir = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "\\Alexandria\\";
-				if (!System.IO.Directory.Exists(dbDir))
-					System.IO.Directory.CreateDirectory(dbDir);
-				string dbPath = dbDir + "Alexandria.db";
-				
-				bool newDatabase = false;
-				if (!System.IO.File.Exists(dbPath))
-					newDatabase = true;
-				
-				string connectString = string.Format("Data Source={0};New={1};UTF8Encoding=True;Version=3", dbPath, newDatabase);
-				
-				
-				using(cnn = new System.Data.SQLite.SQLiteConnection(connectString))
-				{
-					bool tableExists = false;
-				
-					cnn.Open();
-					
-					DataTable schema = cnn.GetSchema("Tables");
-					foreach(DataRow row in schema.Rows)
-					{
-						if (string.Compare(row["TABLE_NAME"].ToString(), "AudioTrack", true) == 0)
-							tableExists = true;
-							
-						//int i = 0;
-						//foreach(object item in row.ItemArray)
-						//{							
-							//System.Diagnostics.Debug.WriteLine(string.Format("Column={0} Value={1}", schema.Columns[i].ColumnName, item));
-							//i++;
-						//}
-					}
-					
-					if (!tableExists)
-					{
-						IDbCommand createTable = cnn.CreateCommand();
-						createTable.CommandText = "CREATE TABLE audiotrack ( Id TEXT PRIMARY KEY, IdType TEXT NOT NULL, Name TEXT NOT NULL, Artist TEXT NOT NULL, Album TEXT NOT NULL, Duration INTEGER NOT NULL, ReleaseDate INTEGER NOT NULL, TrackNumber INTEGER NOT NULL, Format TEXT NOT NULL)";
-						createTable.ExecuteNonQuery();
-					}
-					
-					//IDbCommand insertData = cnn.CreateCommand();
-					//insertData.CommandText = "INSERT INTO audiotrack (Id, IdType, Name, Artist, Album, Duration, ReleaseDate, TrackNumber, Format) VALUES ('5dc9742c-c68b-4534-b0f4-91b341ad09d3', 'MusicBrainzID', 'H.', 'Tool', 'Ænima', 369000, 1996, 3, 'ogg')";
-					//insertData.ExecuteNonQuery();
-					
-					IDbCommand selectData = cnn.CreateCommand();
-					selectData.CommandText = "SELECT * FROM audiotrack";
-					IDataReader reader = selectData.ExecuteReader();
-					while(reader.Read())
-					{
-						data += "Id=" + reader.GetString(0) + " Name=" + reader.GetString(2) + "\n";
-					}
-					
-					cnn.Close();
-				}
-			}
-			catch (Exception ex)
-			{
-				throw new AlexandriaException("SQLite Error: " + ex.Message, ex);
-			}
-			
-			return data;
+	{			
+		#region Constructors
+		public SQLiteDataProvider(string databasePath)
+		{		
+			this.databasePath = databasePath;
 		}
+		#endregion
+		
+		#region Private Constants
+		private const string SCHEMA_TABLES = "Tables";
+		private const string SCHEMA_TABLE_NAME = "TABLE_NAME";
+		#endregion
+		
+		#region Private Fields
+		string databasePath;
+		#endregion
+		
+		#region Private Methods
+		
+		#region GetConnectionString
+		private string GetConnectionString()
+		{
+			bool databaseIsNew = false;
+			databaseIsNew = (!File.Exists(databasePath));
+			return string.Format("Data Source={0};New={1};UTF8Encoding=True;Version=3", databasePath, databaseIsNew);		
+		}
+		#endregion
+		
+		#region GetSQLiteConnection
+		private SQLiteConnection GetSQLiteConnection(string connectionString)
+		{
+			return new SQLiteConnection(connectionString);
+		}
+		#endregion
+
+		#region TableExists
+		private bool TableExists(string tableName)
+		{
+			SQLiteConnection connection = GetSQLiteConnection(GetConnectionString());
+			DataTable tables = connection.GetSchema(SCHEMA_TABLES);
+			foreach (DataRow tableInfo in tables.Rows)
+			{
+				if (string.Compare(tableInfo[SCHEMA_TABLE_NAME].ToString(), tableName, true) == 0)
+					return true;
+			}
+			return false;
+		}
+
+		private bool TableExists<T>(T table) where T : IMetadata
+		{
+			if (table != null)
+			{
+				string tableName = table.GetType().Name;
+				return TableExists(tableName);
+			}
+			else throw new ArgumentNullException("table");
+		}
+		#endregion
+		
+		#region CreateTable
+		private bool CreateTable(DataTable table)
+		{
+			bool tableCreated = false;
+		
+			if (table != null)
+			{
+				StringBuilder sql = new StringBuilder();
+				sql.AppendFormat("CREATE TABLE IF NOT EXISTS {0} (", table.TableName);
+				
+				int count = 0;
+				foreach(DataColumn column in table.Columns)
+				{
+					string delimiter = string.Empty;
+					if (count > 0) delimiter = ",";
+					
+					string columnType = string.Empty;
+					string columnConstraint = string.Empty;
+					
+					if (string.Compare(column.ColumnName, "ID", true) == 0)
+						columnConstraint += " PRIMARY KEY";
+					 
+					if (column.DataType == typeof(decimal) ||
+						column.DataType == typeof(string))
+					{
+						columnType = "TEXT";
+					}
+					else 
+					if (column.DataType == typeof(float) ||
+						column.DataType == typeof(double))
+					{
+						columnType = "REAL";
+						//columnConstraint += " NOT NULL";
+					}
+					else
+					if (column.DataType == typeof(bool) ||
+						column.DataType == typeof(byte) ||
+						column.DataType == typeof(sbyte) ||
+						column.DataType == typeof(ushort) ||
+						column.DataType == typeof(short) ||
+						column.DataType == typeof(uint) ||
+						column.DataType == typeof(int) ||
+						column.DataType == typeof(ulong) ||
+						column.DataType == typeof(long) ||
+						column.DataType == typeof(DateTime) ||
+						column.DataType == typeof(TimeSpan))
+					{
+						columnType = "INTEGER";
+						//columnConstraint += " NOT NULL";
+					}
+					
+					sql.AppendFormat("{0}{1} {2} {3}", delimiter, column.ColumnName, columnType, columnConstraint);
+					
+					count++;
+				}
+				sql.Append(")");
+				
+				SQLiteConnection connection = GetSQLiteConnection(GetConnectionString());
+				IDbCommand command = new SQLiteCommand(sql.ToString(), connection);
+				int result = command.ExecuteNonQuery();
+			}
+			else throw new ArgumentNullException("table");
+			
+			return tableCreated;
+		}
+		#endregion
+		
+		#endregion
+	
+		#region Public Methods
+
+		#region GetConnection
+		public IDbConnection GetConnection()
+		{
+			return GetSQLiteConnection(GetConnectionString());
+		}
+		#endregion
+		
+		#region Lookup
+		public T Lookup<T>(string id) where T: IMetadata
+		{
+			return default(T);
+		}
+		#endregion
+		
+		#region Save
+		public bool Save<T>(T row) where T: IMetadata
+		{
+			bool saved = false;
+			
+			return saved;
+		}
+		#endregion
+		
+		#region Delete
+		public bool Delete<T>(T row) where T: IMetadata
+		{
+			bool deleted = false;
+			
+			return deleted;
+		}
+		#endregion
+		
+		#endregion
 	}
 }
