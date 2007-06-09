9157f4eede7f0a814cb25435f4e5a43cd0351c0e
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-06-09 02:28:12 +0000

    \src\Alexandria\BaseMetadata.cs \src\Alexandria\IStorageEngine.cs \src\Alexandria\PersistanceOptionsAttribute.cs \src\Alexandria.SQLite\SQLiteDataProvider.cs More work on persistance functions

--- trunk/src/Alexandria/IStorageEngine.cs
+++ trunk/src/Alexandria/IStorageEngine.cs
@@ -6,7 +6,7 @@ namespace Alexandria
 {
 	public interface IStorageEngine
 	{
-		T Lookup<T>(Guid id) where T: IPersistant;
+		T Lookup<T>(Guid id) where T: class,IPersistant;
 		void Save(IPersistant record);
 		void Delete(IPersistant record);
 	}
