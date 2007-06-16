afe91bcb2d8c7444e0018ec0af5eca54acc1d558
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-06-16 21:52:16 +0000

    \src\Alexandria\Alexandria.csproj \src\Alexandria\BaseAlbum.cs \src\Alexandria\BaseMetadata.cs \src\Alexandria\BaseAudioTrack.cs \src\Alexandria\BaseIdentifier.cs \src\Alexandria\BaseArtist.cs \src\Alexandria\GuidIdentifier.cs \src\Alexandria\IAlbum.cs \src\Alexandria\IGenre.cs \src\Alexandria\IArtist.cs \src\Alexandria\IAudioRelease.cs \src\Alexandria\MetadataIdentifier.cs \src\Alexandria\IAudioTrack.cs \src\Alexandria\IMedia.cs \src\Alexandria\IBuffered.cs \src\Alexandria\IAudible.cs \src\Alexandria\Playback\ \src\Alexandria\IAudio.cs \src\Alexandria\IAudioCompactDisc.cs \src\Alexandria\BufferState.cs \src\Alexandria\IdentificationResult.cs \src\Alexandria\Data\BaseAlbum.cs \src\Alexandria\Data\BaseArtist.cs \src\Alexandria\Data\BaseAudioTrack.cs \src\Alexandria\Data\BaseIdentifier.cs \src\Alexandria\Data\BaseMetadata.cs \src\Alexandria\Data\GuidIdentifier.cs \src\Alexandria\Data\IAlbum.cs \src\Alexandria\Data\IArtist.cs \src\Alexandria\Data\IAudioRelease.cs \src\Alexandria\Data\IAudioTrack.cs \src\Alexandria\Data\IdentificationResult.cs \src\Alexandria\Data\IGenre.cs \src\Alexandria\Data\MetadataIdentifier.cs \src\Alexandria\Media\BufferState.cs \src\Alexandria\Media\IAudible.cs \src\Alexandria\Media\IAudio.cs \src\Alexandria\Media\IAudioCompactDisc.cs \src\Alexandria\Media\IBuffered.cs \src\Alexandria\Media\IMedia.cs \src\Alexandria\IHasRawAudioData.cs Rearranging Alexandria namespace hierarchy

---
+++ trunk/src/Alexandria/IHasRawAudioData.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexandria
+{
+	[CLSCompliant(false)]
+	public interface IHasRawAudioData
+	{
+		int NumberOfBytes { get; }
+		int NumberOfSamples { get; }
+		int SampleRate { get; }
+		bool IsStereo { get; }
+		[CLSCompliant(false)]
+		IntPtr ReadData(uint length);
+		void CleanupData(IntPtr buffer);
+	}
+}
