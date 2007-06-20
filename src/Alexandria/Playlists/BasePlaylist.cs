6552a9957817e2c538092410dda8a18ce61f4ead
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-06-20 08:11:37 +0000

    \src\Alexandria\Alexandria.csproj \src\Alexandria\Catalog\BaseCatalog.cs \src\Alexandria\Catalog\ICatalog.cs \src\Alexandria\Media\IPlaylistItem.cs \src\Alexandria\Media\IPlaylist.cs \src\Alexandria\Playlists\MediaPlaylist.cs \src\Alexandria\Playlists\BasePlaylist.cs \src\Alexandria\Playlists\IPlaylist.cs \src\Alexandria\Playlists\IPlaylistItem.cs \src\Alexandria\Playlists\M3uPlaylist.cs \src\Alexandria\Playlists\XmlShareablePlaylist.cs More refactoring of Alexandria.Playlists files

--- src/Alexandria/Playlists/BasePlaylist.cs
+++ src/Alexandria/Playlists/BasePlaylist.cs
@@ -1,12 +1,39 @@
+#region License
+/*
+Copyright (c) 2007 Dan Poage
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+#endregion
+
 using System;
 using System.Collections.Generic;
 using System.Text;
 using Alexandria;
 using Alexandria.Media;
 
-namespace Alexandria.Playlist
+namespace Alexandria.Playlists
 {
-	public class MediaPlaylist : IPlaylist
+	public class BasePlaylist : IPlaylist
 	{
 		#region Private Fields
 		private string path;
@@ -18,7 +45,7 @@ namespace Alexandria.Playlist
 		#endregion				
 		
 		#region Constructors
-		protected MediaPlaylist(string path)
+		protected BasePlaylist(string path)
 		{
 			this.path = path;
 			//files = new List<MediaFile>();
@@ -26,7 +53,7 @@ namespace Alexandria.Playlist
 			//Load();
 		}
 		
-		protected MediaPlaylist(string path, string name, string version) : this(path)
+		protected BasePlaylist(string path, string name, string version) : this(path)
 		{
 			this.name = name;
 			this.version = version;
