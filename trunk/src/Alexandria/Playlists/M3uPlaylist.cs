dbce0524ec3e0341724f78c7ae01807742e88b1a
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-06-20 07:58:49 +0000

    \src\AlexandriaLibrary.sln \src\Alexandria\Alexandria.csproj \src\Alexandria\Media\FileIdentifier.cs \src\Alexandria\Playlists\ \src\Alexandria\Playlists\M3uPlaylist.cs \src\Alexandria\Playlists\MediaPlaylist.cs \src\Alexandria\Playlists\XmlShareablePlaylist.cs \src\Alexandria.Client\Alexandria.Client.csproj \src\Alexandria.CompactDisc\Alexandria.CompactDiscTools.csproj \src\Alexandria.CompactDisc\Alexandria.CompactDisc.csproj Created Playlists folder Renamed CompactDisc project to CompactDiscTools Removed Alexandria.Playlist project

---
+++ trunk/src/Alexandria/Playlists/M3uPlaylist.cs
@@ -0,0 +1,43 @@
+//Created by: Edward Knapp
+//date created: 11/9/06
+//date edited: 11/9/06
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Alexandria.Playlist
+{
+    public class M3uPlaylist : MediaPlaylist
+    {
+        #region Constructors
+        public M3uPlaylist(string path): base(path)
+        {
+
+        }
+        #endregion
+
+        #region Public Methods
+        public override void Load()
+        {
+            FileInfo playlistInfo = new FileInfo(Path);
+            StreamReader reader = playlistInfo.OpenText();
+            while(!reader.EndOfStream)
+            {
+                string filePath = reader.ReadLine();
+                if (filePath != null)
+                {
+                    //MediaFile file = MediaFile.Load(playlistInfo, filePath);
+                    //if (file != null)
+                    //{
+                        //Files.Add(file);
+                    //}
+                }
+                
+            }
+           
+        }
+        #endregion
+    }
+}
