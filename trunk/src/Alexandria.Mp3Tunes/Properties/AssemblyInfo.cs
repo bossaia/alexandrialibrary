cd9accac69b1a79f80c138f73ff0db5c8403a3c2
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-06-30 07:00:44 +0000

    \src\Alexandria.Mp3Tunes\Properties\AssemblyInfo.cs Changed the title to match the correct capitalization

--- trunk/src/Alexandria.Mp3Tunes/Properties/AssemblyInfo.cs
+++ trunk/src/Alexandria.Mp3Tunes/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MP3Tunes Client Plugin")]
+[assembly: AssemblyTitle("MP3tunes Client Plugin")]
 [assembly: AssemblyDescription("Media Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
