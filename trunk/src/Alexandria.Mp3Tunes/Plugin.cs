86ec80ff2c562742d980845f48ae47419154e3ce
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-08-14 05:17:51 +0000

    Renamed ConfigurationSetting classes to Plugin

---
+++ trunk/src/Alexandria.Mp3Tunes/Plugin.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using Alexandria.Plugins;
+
+namespace Alexandria.Mp3Tunes
+{
+	public class Plugin : IPluginSettings
+	{
+		#region Constructors
+		public Plugin()
+		{
+		}
+		#endregion
+	
+		#region Private Fields
+		private bool enabled;
+		private ConfigurationMap configurationMap;
+		private string userName;
+		private string password;
+		private LockerSynch lockerSynch = LockerSynch.None;
+		#endregion
+	
+		#region IPluginSettings Members
+		public bool Enabled
+		{
+			get { return enabled; }
+			set { enabled = value; }
+		}
+
+		public ConfigurationMap ConfigurationMap
+		{
+			get { return configurationMap; }
+			set { configurationMap = value; }
+		}
+
+		public void Load()
+		{
+			if (ConfigurationMap != null)
+				ConfigurationMap.Load();
+		}
+
+		public void Save()
+		{
+			if (ConfigurationMap != null)
+				ConfigurationMap.Save();
+		}
+		#endregion
+		
+		#region Public Properties
+		[PluginSetting("The user name for acessing your MP3tunes Music Locker")]
+		public string UserName
+		{
+			get { return userName; }
+			set { userName = value; }
+		}
+
+		[PluginSetting("The password for accessing your MP3tunes Music Locker")]
+		public string Password
+		{
+			get { return password; }
+			set { password = value; }
+		}
+		
+		[PluginSetting("Indicates how Alexandria should synchronize your local metadata with your MP3tunes Music Locker")]
+		public LockerSynch LockerSynch
+		{
+			get { return lockerSynch; }
+			set { lockerSynch = value; }
+		}
+		#endregion
+	}
+}
---
+++ trunk/src/Alexandria.Tao/PlaybackPlugin.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using Alexandria.Plugins;
+
+namespace Alexandria.Tao
+{
+	public class PlaybackPlugin : IPluginSettings
+	{
+		#region Constructors
+		public PlaybackPlugin()
+		{
+		}
+		#endregion
+
+		#region Private Fields
+		private bool enabled;
+		private ConfigurationMap configurationMap;
+		#endregion
+
+		#region IPluginSettings Members
+		public bool Enabled
+		{
+			get { return enabled; }
+			set { enabled = value; }
+		}
+
+		public ConfigurationMap ConfigurationMap
+		{
+			get { return configurationMap; }
+			set { configurationMap = value; }
+		}
+
+		public void Load()
+		{
+			if (ConfigurationMap != null)
+				ConfigurationMap.Load();
+		}
+
+		public void Save()
+		{
+			if (ConfigurationMap != null)
+				ConfigurationMap.Save();
+		}
+		#endregion
+	}
+}
