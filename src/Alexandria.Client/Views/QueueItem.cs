27229072951b0871d980f85eb75b73a978a8471b
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-08-26 23:51:43 +0000

    \src\Alexandria.Client\Alexandria.Client.csproj \src\Alexandria.Client\Controllers\QueueController.cs \src\Alexandria.Client\Views\ \src\Alexandria.Client\Views\IQueueView.cs \src\Alexandria.Client\Views\QueueItem.cs \src\Alexandria.Client\MainForm.cs More work on PAC model

---
+++ src/Alexandria.Client/Views/QueueItem.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexandria.Client.Views
+{
+	public class QueueItem
+	{
+		private string name;
+		private string artist;
+		private string album;
+		private DateTime releaseDate;
+		private TimeSpan duration;
+		private Uri path;
+		private int trackNumber;
+		private string format;
+	}
+}
