5ad398359ef21341d0017e5fb6ed1af1a8426e88
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-09-11 06:38:26 +0000

    \src\Alexandria\Media\IO\AudioPlayer.cs \src\Alexandria\Media\IO\IAudioPlayer.cs \src\Alexandria.Client\Controllers\QueueController.cs \src\Alexandria.Client\Controllers\TrackSource.cs \src\Alexandria.Client\MainForm.cs Fixed a bug with the way that the event handlers were bound in AudioPlayer Got the ToolBox drag-and-drop functionality working again Fixed a bug with CD playback where the track index was 1 too high

--- trunk/src/Alexandria/Media/IO/AudioPlayer.cs
+++ trunk/src/Alexandria/Media/IO/AudioPlayer.cs
@@ -1,6 +1,32 @@
+#region License (MIT)
+/***************************************************************************
+ *  Copyright (C) 2007 Dan Poage
+ ****************************************************************************/
+
+/*  THIS FILE IS LICENSED UNDER THE MIT LICENSE AS OUTLINED IMMEDIATELY BELOW: 
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),  
+ *  to deal in the Software without restriction, including without limitation  
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,  
+ *  and/or sell copies of the Software, and to permit persons to whom the  
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ *  DEALINGS IN THE SOFTWARE.
+ */
+#endregion
+
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Alexandria.Media.IO
 {
@@ -46,9 +72,16 @@ namespace Alexandria.Media.IO
 		private IAudioStreamFactory audioStreamFactory;
 		private IAudioStream currentAudioStream;
 		private EventHandler<EventArgs> currentAudioStreamChanged;
+		private EventHandler<EventArgs> currentAudioStreamEnded;
+		private EventHandler<MediaStateChangedEventArgs> bufferStateChanged;
+		private EventHandler<MediaStateChangedEventArgs> networkStateChanged;
+		private EventHandler<MediaStateChangedEventArgs> playbackStateChanged;
+		private EventHandler<MediaStateChangedEventArgs> seekStateChanged;
+		private EventHandler<AudioStateChangedEventArgs> volumeChanged;
 		private bool muteToggles;
 		private bool playToggles;
 		private bool seekIsPending;
+		private bool isPlaying;
 		#endregion
 		
 		#region Private Event Methods
@@ -134,11 +167,8 @@ namespace Alexandria.Media.IO
 
 		public EventHandler<MediaStateChangedEventArgs> BufferStateChanged
 		{
-			get { return currentAudioStream != null ? currentAudioStream.BufferStateChanged : null; }
-			set {
-				if (currentAudioStream != null)
-					currentAudioStream.BufferStateChanged = value;
-			}
+			get { return bufferStateChanged; }
+			set { bufferStateChanged = value; }
 		}
 
 		public EventHandler<EventArgs> CurrentAudioStreamChanged
@@ -147,44 +177,34 @@ namespace Alexandria.Media.IO
 			set { currentAudioStreamChanged = value; }
 		}
 
+		public EventHandler<EventArgs> CurrentAudioStreamEnded
+		{
+			get { return currentAudioStreamEnded; }
+			set { currentAudioStreamEnded = value; }
+		}
+
 		public EventHandler<MediaStateChangedEventArgs> NetworkStateChanged
 		{
-			get { return currentAudioStream != null ? currentAudioStream.NetworkStateChanged : null; }
-			set
-			{
-				if (currentAudioStream != null)
-					currentAudioStream.NetworkStateChanged = value;
-			}
+			get { return networkStateChanged; }
+			set { networkStateChanged = value; }
 		}
 
 		public EventHandler<MediaStateChangedEventArgs> PlaybackStateChanged
 		{
-			get { return currentAudioStream != null ? currentAudioStream.PlaybackStateChanged : null; }
-			set
-			{
-				if (currentAudioStream != null)
-					currentAudioStream.PlaybackStateChanged = value;
-			}
+			get { return playbackStateChanged; }
+			set { playbackStateChanged = value; }
 		}
 
 		public EventHandler<MediaStateChangedEventArgs> SeekStateChanged
 		{
-			get { return currentAudioStream != null ? currentAudioStream.SeekStateChanged : null; }
-			set
-			{
-				if (currentAudioStream != null)
-					currentAudioStream.SeekStateChanged = value;
-			}
+			get { return seekStateChanged; }
+			set { seekStateChanged = value; }
 		}
 
 		public EventHandler<AudioStateChangedEventArgs> VolumeChanged
 		{
-			get { return currentAudioStream != null ? currentAudioStream.VolumeChanged : null; }
-			set
-			{
-				if (currentAudioStream != null)
-					currentAudioStream.VolumeChanged = value;
-			}
+			get { return volumeChanged; }
+			set { volumeChanged = value; }
 		}
 
 		public void BeginSeek()
@@ -285,10 +305,21 @@ namespace Alexandria.Media.IO
 		{
 			if (currentAudioStream != null)
 			{
+				if (isPlaying && currentAudioStream.Elapsed == currentAudioStream.Duration)
+				{
+					if (CurrentAudioStreamEnded != null)
+						CurrentAudioStreamEnded(this, EventArgs.Empty);
+				}
+			
 				currentAudioStream.RefreshBufferState();
 				currentAudioStream.RefreshNetworkState();
 				currentAudioStream.RefreshPlaybackState();
 				currentAudioStream.RefreshSeekState();
+				isPlaying = (currentAudioStream.PlaybackState == PlaybackState.Playing);
+			}
+			else
+			{
+				isPlaying = false;
 			}
 		}
 
