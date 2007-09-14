2b8a3f51df8b5527ae7bb369f22a085e9dd778e1
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-09-14 04:28:56 +0000

    \src\AlexandriaLibrary.sln \src\Alexandria.Client\Properties\AssemblyInfo.cs \src\Alexandria.Console \src\Alexandria.Console\Alexandria.Console.csproj \src\Alexandria.Console\Properties \src\Alexandria.Console\Properties\AssemblyInfo.cs \src\Alexandria.Console\Alexandria.Console.exe.config \src\Alexandria.Console\Alexandria.Console.ico \src\Alexandria.Console\Program.cs Created Alexandria.Console project

--- src/Alexandria.Console/Properties/AssemblyInfo.cs
+++ src/Alexandria.Console/Properties/AssemblyInfo.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security.Permissions;
-
-[assembly: CLSCompliant(true)]
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution = true, UnmanagedCode = true)]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Alexandria Client")]
-[assembly: AssemblyDescription("A reference GUI client for the Alexandria Media Library")]
+[assembly: AssemblyTitle("Alexandria Console")]
+[assembly: AssemblyDescription("A reference console client for the Alexandria Media Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Telesophy Foundation")]
 [assembly: AssemblyProduct("Alexandria Media Library")]
@@ -25,7 +20,7 @@ using System.Security.Permissions;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3e74ca5e-4a42-4dd4-b3d9-3533a6e4d068")]
+[assembly: Guid("8c3c39b0-990f-4d42-b1c4-f2baa25a3f3c")]
 
 // Version information for an assembly consists of the following four values:
 //
