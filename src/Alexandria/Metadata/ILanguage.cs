89204c586295840faf89a5230e1cc49802949512
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-10-03 06:39:24 +0000

    \src\Alexandria\Alexandria.csproj \src\Alexandria\Metadata\Coordinate.cs \src\Alexandria\Metadata\IEntity.cs \src\Alexandria\Metadata\ILanguage.cs \src\Alexandria\Metadata\ILocation.cs More work on metadata classes and interfaces

---
+++ src/Alexandria/Metadata/ILanguage.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexandria.Metadata
+{
+	public interface ILanguage
+	{
+		string Name { get; }
+		string Code { get; }
+	}
+}
