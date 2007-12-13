cddaee94e8cc87067449432d0d026f5c1f2385b8
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-12-13 09:30:46 +0000

    \src\Alexandria.Client\Properties\AssemblyInfo.cs \src\Alexandria.Client\MainForm.cs \src\Alexandria.Client\MainForm.Designer.cs \src\Alexandria.Client\MainForm.resx Wired a context menu for the sortListView with Clear Selected and Clear All menu items Incremented the build number

--- trunk/src/Alexandria.Client/Properties/AssemblyInfo.cs
+++ trunk/src/Alexandria.Client/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Security.Permissions;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.8")]
-[assembly: AssemblyFileVersion("1.0.0.8")]
+[assembly: AssemblyVersion("1.0.0.9")]
+[assembly: AssemblyFileVersion("1.0.0.9")]
