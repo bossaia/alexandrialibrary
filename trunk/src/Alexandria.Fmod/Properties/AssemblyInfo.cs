b796f9c9515f88548872476dc3b4b1e631da8ac0
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2007-12-14 22:17:30 +0000

    \src\Alexandria.Fmod\Properties\AssemblyInfo.cs \src\Alexandria.TagLib\Asf\AsfTag.cs Fixed a couple of fatal bugs in the parsing logic of AsfTag

--- trunk/src/Alexandria.Fmod/Properties/AssemblyInfo.cs
+++ trunk/src/Alexandria.Fmod/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Security.Permissions;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FMOD Sound System")]
-[assembly: AssemblyDescription("Supports playback of common digital audio formats (flac, mp3, vorbis, wav)")]
+[assembly: AssemblyDescription("Supports playback of common digital audio formats (flac, mp3, vorbis, wav, wma)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Telesophy Foundation")]
 [assembly: AssemblyProduct("Alexandria Media Library")]
