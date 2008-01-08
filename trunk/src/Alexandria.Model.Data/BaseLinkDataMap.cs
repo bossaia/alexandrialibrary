f9c43bcf44fa4ee9e4380d55c8b49d3405fa055c
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2008-01-08 11:00:10 +0000

    \src\Alexandria.Model.Data\BaseLinkDataMap.cs \src\Alexandria.Model.Data\MediaSetContentDataMap.cs \src\Alexandria.Model.Data\MediaSetDataMap.cs \src\Alexandria.Persistence.SQLite\SQLiteEngine.cs More work on persistence logic for MediaSet and MediaContent link table

--- trunk/src/Alexandria.Model.Data/BaseLinkDataMap.cs
+++ trunk/src/Alexandria.Model.Data/BaseLinkDataMap.cs
@@ -40,30 +40,109 @@ namespace Telesophy.Alexandria.Model.Data
 		where Child: IModel
 	{
 		#region Constructors
-		public BaseLinkDataMap()
+		public BaseLinkDataMap(string tableName, BaseSimpleDataMap<Parent> parentMap, string parentIdColumn, BaseSimpleDataMap<Child> childMap, string childIdColumn)
 		{
+			Table = new DataTable(tableName);
+			Table.Columns.Add(parentIdColumn, typeof(Guid));
+			Table.Columns.Add(childIdColumn, typeof(Guid));
+			Table.Constraints.Add(new UniqueConstraint(new DataColumn[]{Table.Columns[parentIdColumn], Table.Columns[childIdColumn]}, true));
+			
+			this.tableName = tableName;
+			this.parentMap = parentMap;
+			this.parentIdColumn = parentIdColumn;
+			this.childMap = childMap;
+			this.childIdColumn = childIdColumn;
 		}
 		#endregion
-
+		
 		#region Private Fields
-		private IPersistenceEngine engine;
-		private DataTable table;
+		private string tableName;
+		private BaseSimpleDataMap<Parent> parentMap;
+		private string parentIdColumn;
+		private BaseSimpleDataMap<Child> childMap;
+		private string childIdColumn;
 		#endregion
 		
-		#region Protected Methods
-		protected virtual IList<Child> GetChildList(string filter)
+		#region Protected Properties
+		protected BaseSimpleDataMap<Parent> ParentMap
 		{
-			//TODO: finish implementing
-			return null;
+			get { return parentMap; }
 		}
 		
-		protected abstract Parent GetParentFromRow(DataRow row);
+		protected BaseSimpleDataMap<Child> ChildMap
+		{
+			get { return childMap; }
+		}
+		#endregion
 		
-		protected abstract Child GetChildFromRow(DataRow row);
+		#region Protected Methods		
+		protected virtual IList<Child> GetChildList(Parent parent)
+		{
+			IList<Child> list = new List<Child>();
 		
-		protected abstract void FillRowForParent(DataRow row, Parent parent);
-		
-		protected abstract void FillRowForChild(DataRow row, Child child);
+			if (parent != null)
+			{
+				Engine.FillTable(Table, parent.Id);
+				foreach(DataRow row in Table.Rows)
+				{
+					Guid childId = GetValue<Guid>(row[childIdColumn]);
+					if (childId != default(Guid))
+					{
+						Child child = ChildMap.LookupModel(childId);
+						if (child != null)
+						{
+							list.Add(child);
+						}
+					}
+				}
+			}
+			
+			return list;
+		}
+
+		protected virtual Parent GetParentFromRow(DataRow row)
+		{
+			if (row != null)
+			{
+				Guid parentId = GetValue<Guid>(row[parentIdColumn]);
+				if (parentId != default(Guid))
+				{
+					return ParentMap.LookupModel(parentId);
+				}
+			}
+
+			return default(Parent);
+		}
+
+		protected virtual Child GetChildFromRow(DataRow row)
+		{
+			if (row != null)
+			{
+				Guid childId = GetValue<Guid>(row[childIdColumn]);
+				if (childId != default(Guid))
+				{
+					return ChildMap.LookupModel(childId);
+				}
+			}
+
+			return default(Child);
+		}
+
+		protected virtual void FillRowForParent(DataRow row, Parent parent)
+		{
+			if (row != null && parent != null)
+			{
+				row[parentIdColumn] = parent.Id;
+			}
+		}
+
+		protected virtual void FillRowForChild(DataRow row, Child child)
+		{
+			if (row != null && child != null)
+			{
+				row[childIdColumn] = child.Id;
+			}
+		}
 		#endregion
 	}
 }
