f23557377da84a0c0e4763932297f32e75de0444
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2008-03-04 01:57:24 +0000

    \src\Babel.Persistence.SQLite\Babel.Persistence.SQLite.csproj \src\Babel.Persistence.SQLite\Engine.cs Created a bare-bones Engine class to implement the new IEngine interface

---
+++ src/Babel.Persistence.SQLite/Engine.cs
@@ -0,0 +1,75 @@
+﻿#region License (MIT)
+/***************************************************************************
+ *  Copyright (C) 2008 Dan Poage
+ ****************************************************************************/
+
+/*  THIS FILE IS LICENSED UNDER THE MIT LICENSE AS OUTLINED IMMEDIATELY BELOW: 
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),  
+ *  to deal in the Software without restriction, including without limitation  
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,  
+ *  and/or sell copies of the Software, and to permit persons to whom the  
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ *  DEALINGS IN THE SOFTWARE.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Telesophy.Babel.Persistence.SQLite
+{
+	public class Engine : EngineBase
+	{
+		#region Constructors
+		public Engine() : base("SQLite Database Engine")
+		{
+		}
+		#endregion
+		
+		#region Private Fields
+		private IDataConverter dataConverter = new DataConverter();
+		#endregion
+		
+		#region Private Methods
+		#endregion
+		
+		#region Public Overrides
+		public override IDataConverter DataConverter
+		{
+			get { return dataConverter; }
+		}
+		
+		public override void Initialize(DataSet dataSet)
+		{
+		}
+
+		public override DataSet Lookup(DataSet dataSet, IQuery query)
+		{
+			return null;
+		}
+
+		public override void Save(DataSet dataSet)
+		{
+		}
+
+		public override void Delete(DataSet dataSet)
+		{
+		}
+		#endregion
+	}
+}
