fed419af9bd7ec0498152903cc2c5bff867aba7f
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2008-03-07 06:40:37 +0000

    \src\Babel.Persistence\Babel.Persistence.csproj \src\Babel.Persistence\Aggregate.cs \src\Babel.Persistence\Association.cs \src\Babel.Persistence\Entity.cs \src\Babel.Persistence\Field.cs \src\Babel.Persistence\IEngine.cs \src\Babel.Persistence\IExpression.cs \src\Babel.Persistence\IOperator.cs \src\Babel.Persistence\NamedItem.cs \src\Babel.Persistence\NamedItemCollection.cs \src\Babel.Persistence\Relationship.cs \src\Babel.Persistence\Schema.cs \src\Babel.Persistence\SchemaItemCollection.cs First pass on the new aggregate/entity-based API for Babel

---
+++ trunk/src/Babel.Persistence/Aggregate.cs
@@ -0,0 +1,90 @@
+﻿#region License (MIT)
+/***************************************************************************
+ *  Copyright (C) 2008 Dan Poage
+ ****************************************************************************/
+
+/*  THIS FILE IS LICENSED UNDER THE MIT LICENSE AS OUTLINED IMMEDIATELY BELOW: 
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),  
+ *  to deal in the Software without restriction, including without limitation  
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,  
+ *  and/or sell copies of the Software, and to permit persons to whom the  
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ *  DEALINGS IN THE SOFTWARE.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telesophy.Babel.Persistence
+{
+	public class Aggregate : NamedItem
+	{
+		#region Constructors
+		public Aggregate(string name, Schema schema, Entity root)
+			: base(name)
+		{
+			this.schema = schema;
+			this.root = root;
+		}
+		#endregion
+		
+		#region Private Fields
+		private Schema schema;
+		private Entity root;
+		private SchemaItemCollection<Association> associations = new SchemaItemCollection<Association>();
+		#endregion
+		
+		#region Public Properties
+		public Schema Schema
+		{
+			get { return schema; }
+		}
+		
+		public Entity Root
+		{
+			get { return root; }
+		}
+		
+		public SchemaItemCollection<Association> Associations
+		{
+			get { return associations; }
+		}
+		#endregion
+	}
+	
+	public class Aggregate<T> : Aggregate
+	{
+		#region Constructors
+		public Aggregate(string name, Schema schema, Entity<T> root) : base(name, schema, root)
+		{
+			this.root = root;
+		}
+		#endregion
+		
+		#region Private
+		private Entity<T> root;
+		#endregion
+		
+		#region Public Properties
+		public new Entity<T> Root
+		{
+			get { return root; }
+		}
+		#endregion
+	}
+}
---
+++ trunk/src/Babel.Persistence/Field.cs
@@ -0,0 +1,122 @@
+﻿#region License (MIT)
+/***************************************************************************
+ *  Copyright (C) 2008 Dan Poage
+ ****************************************************************************/
+
+/*  THIS FILE IS LICENSED UNDER THE MIT LICENSE AS OUTLINED IMMEDIATELY BELOW: 
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),  
+ *  to deal in the Software without restriction, including without limitation  
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,  
+ *  and/or sell copies of the Software, and to permit persons to whom the  
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ *  DEALINGS IN THE SOFTWARE.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telesophy.Babel.Persistence
+{
+	public class Field : NamedItem
+	{
+		#region Constructors
+		public Field(string name, Entity parent, Type type)
+			: base(name)
+		{
+			this.parent = parent;
+			this.type = type;
+		}
+
+		public Field(string name, Entity parent, Type type, bool isRequired)
+			: this(name, parent, type)
+		{
+			this.isRequired = isRequired;
+		}
+
+		public Field(string name, Entity parent, Type type, bool isRequired, bool isUnique)
+			: this(name, parent, type, isRequired)
+		{
+			this.isUnique = isUnique;
+		}
+
+		public Field(string name, Entity parent, Type type, bool isRequired, bool isUnique, bool isHidden)
+			: this(name, parent, type, isRequired, isUnique)
+		{
+			this.isHidden = isHidden;
+		}
+		#endregion
+		
+		#region Private Fields
+		private Entity parent;
+		private Type type;
+		private bool isRequired = false;
+		private bool isUnique = false;
+		private bool isHidden = false;
+		#endregion
+		
+		#region Public Properties
+		public Entity Parent
+		{
+			get { return parent; }
+		}
+		
+		public Type Type
+		{
+			get { return type; }
+		}
+		
+		public bool IsRequired
+		{
+			get { return isRequired; }
+		}
+		
+		public bool IsUnique
+		{
+			get { return isUnique; }
+		}
+		
+		public bool IsHidden
+		{
+			get { return isHidden; }
+		}
+		#endregion
+		
+		#region Public Overrides
+		public override bool Equals(object obj)
+		{
+			if (obj != null && obj is Field)
+			{
+				Field other = (Field)obj;
+				return this.ToString().Equals(other.ToString());
+			}
+			
+			return false;
+		}
+		
+		public override int GetHashCode()
+		{
+			return this.ToString().GetHashCode();
+		}
+
+		public override string ToString()
+		{
+			return string.Format("{0}.{1}.{2}", Parent.Schema.Namespace, Parent.Name, Name);
+		}
+		#endregion
+	}
+}
