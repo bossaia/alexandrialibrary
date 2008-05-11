233efb26cdb26ef4333ea88d6eae3fc57d7c7eda
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2008-05-11 21:21:03 +0000

    \src\Alexandria2.sln \src\Alexandria.Core \src\Alexandria.Core\Alexandria.Core.csproj \src\Alexandria.Core\Properties \src\Alexandria.Core\Properties\AssemblyInfo.cs \src\Alexandria.Core\Capabilities.cs \src\Alexandria.Core\IMessage.cs Started work on Alexandria.Core project for Alexandria 2.0

---
+++ trunk/src/Alexandria.Core/Capabilities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telesophy.Alexandria.Core
+{
+	public struct Capabilities
+	{
+		private bool usesFileSystem;
+		private bool usesNetwork;
+		
+		public Capabilities(bool usesFileSystem, bool usesNetwork)
+		{
+			this.usesFileSystem = usesFileSystem;
+			this.usesNetwork = usesNetwork;
+		}
+		
+		public bool UsesFileSystem
+		{
+			get { return usesFileSystem; }
+		}
+		
+		public bool UsesNetwork
+		{
+			get { return usesNetwork; }
+		}
+	}
+}
---
+++ trunk/src/Alexandria.Core/IMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telesophy.Alexandria.Core
+{
+	public interface IMessage
+	{
+		string Type { get; }
+		Capabilities Capabilities { get; }
+		object Data { get; }
+	}
+}
