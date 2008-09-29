75d8c86023922565749cd20ac46c15ab7a8a832f
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2008-09-29 05:55:02 +0000

    Started work on FluentNHibernate persistence functionality

---
+++ Zeta/src/Zeta.Core/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Zeta.Core
+{
+    public interface IRepository
+    {
+        T Find<T>(Guid id) where T : IModel;
+        void Delete<T>(T target);
+        T[] Query<T>(Expression<Func<T, bool>> where);
+        T FindBy<T, U>(Expression<Func<T, U>> expression, U search) where T : class;
+        T FindBy<T>(Expression<Func<T, bool>> where);
+        void Save<T>(T target);
+    }
+}
---
+++ Zeta/src/Zeta.NHibernate/MapBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using FluentNHibernate.Mapping;
+
+using Zeta.Core;
+
+namespace Zeta.NHibernate
+{
+    public abstract class MapBase<T> : ClassMap<T>
+        where T: IModel
+    {
+        protected MapBase()
+            : base()
+        {
+            Id(x => x.Id).GeneratedBy.GuidComb();
+        }
+    }
+}
