76ff1bf304a834ca22797ef02cd6251a50cd97f5
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2009-04-10 03:34:12 +0000

    More work on schema for v2 object model

--- trunk/alxlib/src/Alexandria.Resources.Media/Schema/SchemaConstants.cs
+++ trunk/alxlib/src/Alexandria.Resources.Media/Schema/SchemaConstants.cs
@@ -7,11 +7,41 @@ namespace Alexandria.Resources.Media.Schema
 {
 	public static class SchemaConstants
 	{
-		public const string UriBase = "http://www.alxlib.com/";
-		public const string SchemaBase = UriBase + "schema/1/";
-		public const string TypesBase = SchemaBase + "types/";
-		public const string AggregatesBase = TypesBase + "aggregates/";
-		public const string EntitiesBase = TypesBase + "entities/";
-		public const string ValuesBase = TypesBase + "values/";
+		public const string Host = "http://www.alxlib.com/";
+		
+		public const string Media = Host + "media/";
+		public const string MediaAlbums = Media + "albums/";
+		public const string MediaArtists = Media + "artists/";
+		public const string MediaCatalogs = Media + "catalogs/";
+		public const string MediaDates = Media + "dates/";
+		public const string MediaDurations = Media + "durations/";
+		public const string MediaEvents = Media + "events/";
+		public const string MediaFiles = Media + "files/";
+		public const string MediaNames = Media + "names/";
+		public const string MediaNumbers = Media + "numbers/";
+		public const string MediaPlaylists = Media + "playlists/";
+		public const string MediaTags = Media + "tags/";
+		public const string MediaTracks = Media + "tracks/";
+		public const string MediaUsers = Media + "users/";
+
+		public const string Schema = Host + "schema/1/types/";
+		public const string SchemaAggregates = Schema + "aggregates/";
+		public const string SchemaAggregatesAlbumWithTracks = SchemaAggregates + "album_with_tracks";
+		public const string SchemaAggregatesFileWithTrack = SchemaAggregates + "file_with_track";
+		public const string SchemaEntities = Schema + "entities/";
+		public const string SchemaEntitiesAlbum = SchemaEntities + "album";
+		public const string SchemaEntitiesArtist = SchemaEntities + "artist";
+		public const string SchemaEntitiesCatalog = SchemaEntities + "catalog";
+		public const string SchemaEntitiesEvent = SchemaEntities + "event";
+		public const string SchemaEntitiesFile = SchemaEntities + "file";
+		public const string SchemaEntitiesPlaylist = SchemaEntities + "playlist";
+		public const string SchemaEntitiesTag = SchemaEntities + "tag";
+		public const string SchemaEntitiesTrack = SchemaEntities + "track";
+		public const string SchemaEntitiesUser = SchemaEntities + "user";
+		public const string SchemaValues = Schema + "values/";
+		public const string SchemaValuesDate = SchemaValues + "date";
+		public const string SchemaValuesDuration = SchemaValues + "duration";
+		public const string SchemaValuesName = SchemaValues + "name";
+		public const string SchemaValuesNumber = SchemaValues + "number";
 	}
 }
