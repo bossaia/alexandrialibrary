b069b8a5bfa7de40f09b3d16ff9810bff267e4a9
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2009-11-12 04:37:16 +0000

    Started work on primary interfaces in Sophia.Core

---
+++ Sophia/src/Sophia.Core/IRouter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sophia.Core
+{
+	public interface IRouter
+		: IReceiver
+	{
+		IEnumerable<INode> Nodes { get; }
+	}
+}
