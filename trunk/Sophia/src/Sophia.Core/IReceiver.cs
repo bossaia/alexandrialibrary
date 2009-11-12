9a3273ff0d80a56d16f129ab74e8373c46f2d148
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2009-11-12 04:37:16 +0000

    Started work on primary interfaces in Sophia.Core

---
+++ trunk/Sophia/src/Sophia.Core/IReceiver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sophia.Core
+{
+	public interface IReceiver
+	{
+		void Receive(IMessage message);
+	}
+}
