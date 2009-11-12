79c84fa1d0582d316cb80df44b3431c4bfbd27c7
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2009-11-12 04:01:29 +0000

    Added Baoth project Added Alexandria.mm

---
+++ trunk/Alexandria/Baoth/IName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Baoth
+{
+	public interface IName
+	{
+		string Value { get; }
+		int EncodingCount { get; }
+		IEnumerable<INameEncoding> Encodings { get; }
+
+	}
+}
