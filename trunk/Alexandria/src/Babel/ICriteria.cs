3cde61019a76081fdac3e6b20b84e502bb6942c2
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2009-12-27 10:31:15 +0000

    Moved Criteria and Expression logic into new Babel project

---
+++ trunk/Alexandria/src/Babel/ICriteria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Babel
+{
+	public interface ICriteria<T>
+	{
+		ICriteria<T> And { get; }
+		ICriteria<T> Or { get; }
+		ICriteria<T> That(Expression<Func<T, object>> expression);
+	}
+}
