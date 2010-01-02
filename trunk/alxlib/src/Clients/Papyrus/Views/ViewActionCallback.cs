e6bcc3ec15b9cde1c9676d508f6a017f39b839f4
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2010-01-02 11:33:46 +0000

    Removed alxlib project

--- trunk/alxlib/src/Clients/Papyrus/Views/ViewActionCallback.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Papyrus.Views
-{
-	public delegate void ViewActionCallback(ViewAction action);
-}
