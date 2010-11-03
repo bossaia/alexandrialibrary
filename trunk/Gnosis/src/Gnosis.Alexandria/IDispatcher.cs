1c5cbdfc61a9677bd15320679c5fe8e8da9b04c7
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2010-11-03 22:27:00 +0000

    Setup Alexandria WPF application

---
+++ trunk/Gnosis/src/Gnosis.Alexandria/IDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Alexandria
+{
+    public interface IDispatcher
+    {
+        void Dispatch<T>(Guid sender, T message)
+            where T : IMessage;
+    }
+}
---
+++ trunk/Gnosis/src/Gnosis.Alexandria/IHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Alexandria
+{
+    public interface IHandler<T>
+        where T : IMessage
+    {
+        void Handle(T message);
+    }
+}
