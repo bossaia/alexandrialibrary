a0d268e4e3b51ecb993960d9a929a026fbd553cd
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2010-11-05 01:52:27 +0000

    Added close button to tab headers

--- trunk/Gnosis/src/Gnosis.Alexandria/Controllers/TabController.cs
+++ trunk/Gnosis/src/Gnosis.Alexandria/Controllers/TabController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 using Gnosis.Alexandria.Handlers;
 using Gnosis.Alexandria.Messages;
@@ -26,11 +28,42 @@ namespace Gnosis.Alexandria.Controllers
         {
             AddChild(tabView);
             var tabItem = new TabItem();
-            tabItem.Header = header;
+
+            var headerBlock = new TextBlock();
+            headerBlock.Text = header;
+            headerBlock.Margin = new Thickness(0, 1, 8, 0);
+            var closeImage = new Image();
+            closeImage.Source = (ImageSource)new ImageSourceConverter().ConvertFromString("pack://application:,,,/Alexandria;component/Images/Controls/CloseTab.png");
+            var closeButton = new Button();
+            closeButton.Tag = tabView.Id;
+            closeButton.Width = 18;
+            closeButton.Height = 18;
+            closeButton.Content = closeImage;
+            closeButton.Focusable = false;
+            closeButton.Margin = new Thickness(10, 0, 0, 0);
+            closeButton.VerticalAlignment = VerticalAlignment.Top;
+            closeButton.HorizontalAlignment = HorizontalAlignment.Right;
+            closeButton.VerticalContentAlignment = VerticalAlignment.Center;
+            closeButton.Click += new RoutedEventHandler(CloseTab);
+            var headerPanel = new DockPanel();
+            headerPanel.Children.Add(headerBlock);
+            headerPanel.Children.Add(closeButton);
+            headerPanel.HorizontalAlignment = HorizontalAlignment.Stretch;
+            DockPanel.SetDock(headerBlock, Dock.Left);
+            DockPanel.SetDock(closeButton, Dock.Right);
+            tabItem.Header = headerPanel;
             tabItem.Content = tabView;
             _control.Items.Add(tabItem);
             _tabMap.Add(tabView.Id, tabItem);
             _control.SelectedItem = tabItem;
+        }
+
+
+        private void CloseTab(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var id = (Guid)button.Tag;
+            RemoveTab(id);
         }
 
         public void RemoveTab(Guid id)
