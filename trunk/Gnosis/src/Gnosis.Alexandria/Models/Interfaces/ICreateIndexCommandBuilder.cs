bf3ef50bb850db449a61814cdbacf6c0de64e3cb
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2010-11-10 05:49:00 +0000

    Started work on schema command builders.

---
+++ trunk/Gnosis/src/Gnosis.Alexandria/Models/Interfaces/ICreateIndexCommandBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Alexandria.Models.Interfaces
+{
+    public interface ICreateIndexCommandBuilder
+    {
+        ICreateIndexCommandBuilder CreateUniqueIndex(string name);
+        ICreateIndexCommandBuilder CreateIndex(string name);
+
+        ICreateIndexCommandBuilder IfNotExists { get; }
+
+        ICreateIndexCommandBuilder On(string table);
+        ICreateIndexCommandBuilder Ascending(string column);
+        ICreateIndexCommandBuilder Descending(string column);
+
+        ICommand ToCommand();
+    }
+}
---
+++ trunk/Gnosis/src/Gnosis.Alexandria/Models/Interfaces/ICreateViewCommandBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Alexandria.Models.Interfaces
+{
+    public interface ICreateViewCommandBuilder
+    {
+        ICreateViewCommandBuilder CreateView(string name);
+        ICreateViewCommandBuilder CreateTempView(string name);
+        ICreateViewCommandBuilder IfNotExists { get; }
+        ICreateViewCommandBuilder As(ICommand select);
+
+        ICommand ToCommand();
+    }
+}
