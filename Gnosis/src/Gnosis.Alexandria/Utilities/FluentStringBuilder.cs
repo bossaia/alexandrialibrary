0aead29b32b0f2c2663f6710ff3f062620c747a7
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2010-11-10 00:17:46 +0000

    Created IStringBuilder interface and FluentStringBuilder implementation in new Utilities namespace Created IQueryCommandBuilder and IPersistCommandBuilder interfaces Created QueryCommandBuilder implementation

---
+++ Gnosis/src/Gnosis.Alexandria/Utilities/FluentStringBuilder.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gnosis.Alexandria.Utilities.Interfaces;
+
+namespace Gnosis.Alexandria.Utilities
+{
+    public class FluentStringBuilder : IStringBuilder
+    {
+        public FluentStringBuilder()
+        {
+            ClauseDelimiter = ", ";
+            TokenDelimiter = " ";
+        }
+
+        public FluentStringBuilder(string clauseDelimiter, string tokenDelimiter)
+        {
+            ClauseDelimiter = clauseDelimiter;
+            TokenDelimiter = tokenDelimiter;
+        }
+
+        private readonly StringBuilder _builder = new StringBuilder();
+
+        public string ClauseDelimiter { get; set; }
+
+        public string TokenDelimiter { get; set; }
+
+        public IStringBuilder Append(string value)
+        {
+            _builder.Append(value);
+            return this;
+        }
+
+        public IStringBuilder Append(IStringBuilder builder)
+        {
+            if (builder == null)
+                return this;
+
+            _builder.Append(builder.ToString());
+            return this;
+        }
+
+        public IStringBuilder AppendClause(params string[] tokens)
+        {
+            if (_builder.Length > 0)
+                Append(ClauseDelimiter);
+
+            var first = true;
+            foreach (var token in tokens)
+            {
+                if (!first)
+                    Append(TokenDelimiter);
+
+                Append(token);
+                first = false;
+            }
+
+            return this;
+        }
+
+        public IStringBuilder AppendFormat(string format, params object[] args)
+        {
+            _builder.AppendFormat(format, args);
+            return this;
+        }
+
+        public IStringBuilder AppendLine(string value)
+        {
+            _builder.AppendLine(value);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return _builder.ToString();
+        }
+    }
+}
---
+++ Gnosis/src/Gnosis.Alexandria/Utilities/Interfaces/IStringBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Alexandria.Utilities.Interfaces
+{
+    public interface IStringBuilder
+    {
+        string ClauseDelimiter { get; set; }
+        string TokenDelimiter { get; set; }
+        IStringBuilder Append(string value);
+        IStringBuilder Append(IStringBuilder builder);
+        IStringBuilder AppendClause(params string[] tokens);
+        IStringBuilder AppendFormat(string format, params object[] args);
+        IStringBuilder AppendLine(string value);
+
+        string ToString();
+    }
+}
