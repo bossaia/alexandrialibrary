b3333a0ca93a6af4f6ad6b27e4004059c1ed0db7
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2010-11-18 23:56:04 +0000

    Moved IPredicate to Gnosis.Babel.SQLite namespace Added fluent Append methods to Statement Started work implementing Predicate

--- trunk/Gnosis/src/Gnosis.Babel/SQLite/IPredicate.cs
+++ trunk/Gnosis/src/Gnosis.Babel/SQLite/IPredicate.cs
@@ -1,28 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 
-namespace Gnosis.Babel
+namespace Gnosis.Babel.SQLite
 {
-    public interface IPredicate<S> where S : IStatement
+    public interface IPredicate<in S> where S : IStatement
     {
         S IsEqualTo(string expression);
-        S IsEqualTo(string expression, object value);
+        S IsEqualTo(string name, object value);
         S IsEqualTo<T>(Expression<Func<T, object>> expression);
         S IsEqualTo<T>(Expression<Func<T, object>> expression, object value);
         S IsNotEqualTo(string expression);
-        S IsNotEqualTo(string expression, object value);
+        S IsNotEqualTo(string name, object value);
         S IsNotEqualTo<T>(Expression<Func<T, object>> expression);
         S IsNotEqualTo<T>(Expression<Func<T, object>> expression, object value);
 
         S IsLessThan(string expression);
-        S IsLessThan(string expression, object value);
+        S IsLessThan(string name, object value);
         S IsLessThan<T>(Expression<Func<T, object>> expression);
         S IsLessThan<T>(Expression<Func<T, object>> expression, object value);
         S IsLessThanOrEqualTo(string expression);
-        S IsLessThanOrEqualTo(string expression, object value);
+        S IsLessThanOrEqualTo(string name, object value);
         S IsLessThanOrEqualTo<T>(Expression<Func<T, object>> expression);
         S IsLessThanOrEqualTo<T>(Expression<Func<T, object>> expression, object value);
         S IsGreaterThan(string expression);
---
+++ trunk/Gnosis/src/Gnosis.Babel/SQLite/Where.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Gnosis.Babel.SQLite
+{
+    public class Where : Statement, IWhere
+    {
+        public IPredicate<IWhere> And<T>(Expression<Func<T, object>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPredicate<IWhere> Or<T>(Expression<Func<T, object>> expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
