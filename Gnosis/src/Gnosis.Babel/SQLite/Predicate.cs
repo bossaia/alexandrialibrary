07057c9ce6a95eb9b0ad6f2f7e21568c85853ea7
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2010-11-18 23:56:04 +0000

    Moved IPredicate to Gnosis.Babel.SQLite namespace Added fluent Append methods to Statement Started work implementing Predicate

---
+++ Gnosis/src/Gnosis.Babel/SQLite/Predicate.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Gnosis.Babel.SQLite
+{
+    public class Predicate<TInterface, TConcrete> : Statement, IPredicate<TInterface>
+        where TInterface : IStatement
+        where TConcrete : Statement, TInterface, new()
+    {
+        #region Private Constants
+
+        private const string OpEqualTo = "=";
+        private const string OpNotEqualTo = "<>";
+        private const string OpLessThan = "<";
+        private const string OpLessThanOrEqualTo = "<=";
+        private const string OpGreaterThan = ">";
+        private const string OpGreaterThanOrEqualTo = ">=";
+        private const string OpInPrefix = "in (";
+        private const string OpNotInPrefix = "not in (";
+        private const string OpInSuffix = ")";
+        private const string OpNull = "isnull";
+        private const string OpNotNull = "not null";
+        private const string OpLike = "like";
+        private const string OpNotLike = "not like";
+
+        #endregion
+
+        public TInterface IsEqualTo(string expression)
+        {
+            AppendWord(OpEqualTo);
+            return AppendWord<TInterface, TConcrete>(expression);
+        }
+
+        public TInterface IsEqualTo(string name, object value)
+        {
+            AppendWord(OpEqualTo);
+            return AppendParameter<TInterface, TConcrete>(name, value);
+        }
+
+        public TInterface IsEqualTo<T>(Expression<Func<T, object>> expression)
+        {
+            AppendWord(OpEqualTo);
+            return AppendWord<TInterface, TConcrete>(expression.ToName());
+        }
+
+        public TInterface IsEqualTo<T>(Expression<Func<T, object>> expression, object value)
+        {
+            AppendWord(OpEqualTo);
+            return AppendParameter<TInterface, TConcrete>(expression.ToName(), value);
+        }
+
+        public TInterface IsNotEqualTo(string expression)
+        {
+            AppendWord(OpNotEqualTo);
+            return AppendWord<TInterface, TConcrete>(expression);
+        }
+
+        public TInterface IsNotEqualTo(string name, object value)
+        {
+            AppendWord(OpNotEqualTo);
+            return AppendParameter<TInterface, TConcrete>(name, value);
+        }
+
+        public TInterface IsNotEqualTo<T>(Expression<Func<T, object>> expression)
+        {
+            AppendWord(OpNotEqualTo);
+            return AppendWord<TInterface, TConcrete>(expression.ToString());
+        }
+
+        public TInterface IsNotEqualTo<T>(Expression<Func<T, object>> expression, object value)
+        {
+            AppendWord(OpNotEqualTo);
+            return AppendParameter<TInterface, TConcrete>(expression.ToName(), value);
+        }
+
+        public TInterface IsLessThan(string expression)
+        {
+            AppendWord(OpLessThan);
+            return AppendWord<TInterface, TConcrete>(expression);
+        }
+
+        public TInterface IsLessThan(string name, object value)
+        {
+            AppendWord(OpLessThan);
+            return AppendParameter<TInterface, TConcrete>(name, value);
+        }
+
+        public TInterface IsLessThan<T>(Expression<Func<T, object>> expression)
+        {
+            AppendWord(OpLessThan);
+            return AppendWord<TInterface, TConcrete>(expression.ToName());
+        }
+
+        public TInterface IsLessThan<T>(Expression<Func<T, object>> expression, object value)
+        {
+            AppendWord(OpLessThan);
+            return AppendParameter<TInterface, TConcrete>(expression.ToName(), value);
+        }
+
+        public TInterface IsLessThanOrEqualTo(string expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsLessThanOrEqualTo(string name, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsLessThanOrEqualTo<T>(Expression<Func<T, object>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsLessThanOrEqualTo<T>(Expression<Func<T, object>> expression, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsGreaterThan(string expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsGreaterThan(string expression, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsGreaterThan<T>(System.Linq.Expressions.Expression<Func<T, object>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsGreaterThan<T>(System.Linq.Expressions.Expression<Func<T, object>> expression, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsGreaterThanOrEqualTo(string expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsGreaterThanOrEqualTo(string expression, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsGreaterThanOrEqualTo<T>(System.Linq.Expressions.Expression<Func<T, object>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsGreaterThanOrEqualTo<T>(System.Linq.Expressions.Expression<Func<T, object>> expression, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsLike(string expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsLike(string expression, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsLike<T>(System.Linq.Expressions.Expression<Func<T, object>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsLike<T>(System.Linq.Expressions.Expression<Func<T, object>> expression, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsNotLike(string expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsNotLike(string expression, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsNotLike<T>(System.Linq.Expressions.Expression<Func<T, object>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsNotLike<T>(System.Linq.Expressions.Expression<Func<T, object>> expression, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsIn(string expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsIn(string expression, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsNotIn(string expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsNotIn(string expression, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TInterface IsNull
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public TInterface IsNotNull
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
--- Gnosis/src/Gnosis.Babel/Statement.cs
+++ Gnosis/src/Gnosis.Babel/Statement.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text;
 
 namespace Gnosis.Babel
@@ -28,13 +26,87 @@ namespace Gnosis.Babel
 
         private readonly StringBuilder _text;
         private readonly IDictionary<string, object> _parameters = new Dictionary<string, object>();
-        private bool listMode = false;
-        private bool hasParentheses = false;
+        private bool _listMode = false;
+        private bool _hasParentheses = false;
 
         private void AddParameter(string name, object value)
         {
+            if (!name.StartsWith("@"))
+                name = string.Format("@{0}", name);
+
             _parameters[name] = value;
         }
+
+        private void AddParameters(IEnumerable<KeyValuePair<string, object>> parameters)
+        {
+            parameters.Each(x => AddParameter(x.Key, x.Value));
+        }
+
+        #region Fluent Append Methods
+
+        protected TInterface AppendWord<TInterface, TConcrete>(string word)
+            where TInterface : IStatement
+            where TConcrete : Statement, TInterface, new()
+        {
+            var concrete = new TConcrete();
+            concrete.AddParameters(_parameters);
+            concrete.AppendWord(_text.ToString());
+            concrete.AppendWord(word);
+
+            return concrete;
+        }
+
+        protected TInterface AppendClause<TInterface, TConcrete>(string clause)
+            where TInterface : IStatement
+            where TConcrete : Statement, TInterface, new()
+        {
+            var concrete = new TConcrete();
+            concrete.AddParameters(_parameters);
+            concrete.AppendWord(_text.ToString());
+            concrete.AppendClause(clause);
+
+            return concrete;
+        }
+
+        protected TInterface AppendListItem<TInterface, TConcrete>(string item)
+            where TInterface : IStatement
+            where TConcrete : Statement, TInterface, new()
+        {
+            var concrete = new TConcrete();
+            concrete.AddParameters(_parameters);
+            concrete.AppendWord(_text.ToString());
+            concrete.AppendListItem(item);
+
+            return concrete;
+        }
+
+        protected TInterface AppendParentheticalListItem<TInterface, TConcrete>(string item)
+            where TInterface : IStatement
+            where TConcrete : Statement, TInterface, new()
+        {
+            var concrete = new TConcrete();
+            concrete.AddParameters(_parameters);
+            concrete.AppendWord(_text.ToString());
+            concrete.AppendParentheticalListItem(item);
+
+            return concrete;
+        }
+
+        protected TInterface AppendParameter<TInterface, TConcrete>(string name, object value)
+            where TInterface : IStatement
+            where TConcrete : Statement, TInterface, new()
+        {
+            var concrete = new TConcrete();
+            concrete.AddParameters(_parameters);
+            concrete.AppendWord(_text.ToString());
+            concrete.AppendParameter(name, value);
+
+            return concrete;
+        }
+
+        #endregion
+
+        #region Regular Append Methods
 
         protected void AppendWord(string word)
         {
@@ -45,20 +117,20 @@ namespace Gnosis.Babel
 
         protected void AppendListItem(string item)
         {
-            if (listMode)
+            if (_listMode)
                 _text.AppendFormat(", {0}", item);
             else if (_text.Length > 0)
                 _text.AppendFormat(" {0}", item);
             else
                 _text.Append(item);
 
-            listMode = true;
-            hasParentheses = false;
+            _listMode = true;
+            _hasParentheses = false;
         }
 
         protected void AppendClause(string clause)
         {
-            if (listMode && hasParentheses)
+            if (_listMode && _hasParentheses)
                 _text.Append(")");
 
             if (_text.Length > 0)
@@ -66,26 +138,28 @@ namespace Gnosis.Babel
             else
                 _text.Append(clause);
 
-            listMode = false;
-            hasParentheses = false;
+            _listMode = false;
+            _hasParentheses = false;
         }
 
         protected void AppendParameter(string name, object value)
         {
-            AddParameter(name, value);
             AppendWord(name);
+            AddParameter(name, value);
         }
 
         protected void AppendParentheticalListItem(string item)
         {
-            if (listMode)
+            if (_listMode)
                 _text.AppendFormat(", {0}", item);
             else
                 _text.AppendFormat(" ({0}", item);
 
-            listMode = true;
-            hasParentheses = true;
+            _listMode = true;
+            _hasParentheses = true;
         }
+
+        #endregion
 
         public IEnumerable<KeyValuePair<string, object>> Parameters
         {
@@ -94,7 +168,7 @@ namespace Gnosis.Babel
 
         public override string ToString()
         {
-            var suffix = (listMode && hasParentheses) ? ");" : ";";
+            var suffix = (_listMode && _hasParentheses) ? ");" : ";";
             return _text.ToString() + suffix;
         }
     }
