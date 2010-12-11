1e6fd096d40092a86ec686f95f20a940d925b577
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2010-12-11 05:14:10 +0000

    Changed AsMd5Hash() extension method to use UTF encoding instead of ASCII

--- Gnosis/src/Gnosis.Core/StringExtensions.cs
+++ Gnosis/src/Gnosis.Core/StringExtensions.cs
@@ -918,7 +918,6 @@ namespace Gnosis.Core
 
         public static string AsMd5HashFromFileName(this string fileName)
         {
-            
             FileStream file = new FileStream(fileName, FileMode.Open);
             MD5 md5 = new MD5CryptoServiceProvider();
             byte[] retVal = md5.ComputeHash(file);
@@ -934,13 +933,11 @@ namespace Gnosis.Core
 
         public static string AsMd5Hash(this string input)
         {
-            // step 1, calculate MD5 hash from input
-            MD5 md5 = System.Security.Cryptography.MD5.Create();
-            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            var md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
             byte[] hash = md5.ComputeHash(inputBytes);
 
-            // step 2, convert byte array to hex string
-            StringBuilder sb = new StringBuilder();
+            var sb = new StringBuilder();
             for (int i = 0; i < hash.Length; i++)
             {
                 sb.Append(hash[i].ToString("x2"));
