6d7c54355a8460492ad9c9c749e60c1234a02b9b
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-01-02 23:52:43 +0000

    Started work creating the messaging API for Gnosis.Core Created a new ListMaker project

---
+++ Gnosis/src/Gnosis.Core/IPlaybackElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core
+{
+    public interface IPlaybackElement
+    {
+        string Identifier { get; set; }
+        PlaybackFunction Function { get; set; }
+        StreamingStatus StreamingStatus { get; set; }
+    }
+}
---
+++ Gnosis/src/Gnosis.ListMaker/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.ListMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
