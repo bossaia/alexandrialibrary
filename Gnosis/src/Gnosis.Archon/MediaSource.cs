3693254abacd5f796860d44cab0cda6d59f9104a
dan.poage <dan.poage@gmail.com>
2011-03-16 21:15:08 +0000

    Created generic IRepository interface and RepositoryBase base class Turned TrackRepository into a subclass of RepositoryBase Created ISource and several source implementations Added source tree view to main UI

---
+++ Gnosis/src/Gnosis.Archon/MediaSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Archon
+{
+    public class MediaSource : SourceBase
+    {
+        public MediaSource()
+            : base()
+        {
+        }
+
+        public MediaSource(Guid id)
+            : base(id)
+        {
+        }
+
+        public override void AddChild(ISource child)
+        {
+        }
+    }
+}
---
+++ Gnosis/src/Gnosis.Archon/SourceBase.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace Gnosis.Archon
+{
+    public abstract class SourceBase : ISource, INotifyPropertyChanged
+    {
+        protected SourceBase() : this(Guid.NewGuid())
+        {
+        }
+
+        protected SourceBase(Guid id)
+        {
+            this.id = id;
+        }
+
+        private Guid id;
+        private ISource parent;
+        private string name;
+        private readonly ObservableCollection<ISource> children = new ObservableCollection<ISource>();
+        private bool isExpanded;
+        private bool isSelected;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public Guid Id
+        {
+            get { return id; }
+        }
+
+        public ISource Parent
+        {
+            get { return parent; }
+            set
+            {
+                if (parent != value)
+                {
+                    parent = value;
+                    OnPropertyChanged("Parent");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public IEnumerable<ISource> Children
+        {
+            get { return children; }
+        }
+
+        public bool IsExpanded
+        {
+            get { return isExpanded; }
+            set
+            {
+                if (isExpanded != value)
+                {
+                    isExpanded = value;
+                    OnPropertyChanged("IsExpanded");
+                }
+
+                if (isExpanded && parent != null)
+                    parent.IsExpanded = true;
+            }
+        }
+
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set
+            {
+                if (isSelected != value)
+                {
+                    isSelected = value;
+                    OnPropertyChanged("IsSelected");
+                }
+            }
+        }
+
+        public virtual void AddChild(ISource child)
+        {
+            children.Add(child);
+            OnPropertyChanged("Children");
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
