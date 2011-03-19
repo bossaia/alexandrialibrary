8d37a45262d3ef6ffff783a0d63fd32cbb5f5491
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-03-19 19:06:49 +0000

    Created Models and Repositories folders and moved model and repository implementations into those folders Started work implementing SourceRepository

--- trunk/Gnosis/src/Gnosis.Archon/Models/SourcePropertyBase.cs
+++ trunk/Gnosis/src/Gnosis.Archon/Models/SourcePropertyBase.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Gnosis.Archon.Sources.Properties
+namespace Gnosis.Archon.Models
 {
     public class SourcePropertyBase<T> : ISourceProperty
     {
