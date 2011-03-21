7255cbf7ac9a2f08a0da79c30b21a35ca75e3ff8
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2011-03-21 18:46:11 +0000

    Added VisualHelper for aiding with drag and drop Finished support for drag and drop from TrackView to SourceView Started work on content menu for SourceView

---
+++ trunk/Gnosis/src/Gnosis.Archon/Helpers/VisualHelper.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Gnosis.Archon.Helpers
+{
+    public static class VisualHelper
+    {
+        public static childItem FindVisualChild<childItem>(DependencyObject obj)
+            where childItem : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is childItem)
+                    return (childItem)child;
+                else
+                {
+                    childItem childOfChild = FindVisualChild<childItem>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
+        }
+
+        private class IndexableVirtualizingStackPanel : VirtualizingStackPanel
+        {
+            /// <summary>
+            /// Publically expose BringIndexIntoView.
+            /// </summary>
+            public void BringIntoView(int index)
+            {
+
+                this.BringIndexIntoView(index);
+            }
+        }
+
+        /// <summary>
+        /// Go up the visual tree to find the TreeViewItem that contains the given UI element
+        /// </summary>
+        /// <param name="element">The UI element</param>
+        /// <returns>The TreeViewItem that contains the given element, or null if the given element is not a child of a TreeViewItem</returns>
+        public static TreeViewItem FindContainingTreeViewItem(UIElement element)
+        {
+            if (element != null)
+            {
+                var item = element as TreeViewItem;
+                if (item != null)
+                    return item;
+
+                var parent = VisualTreeHelper.GetParent(element) as UIElement;
+                if (parent != null)
+                    return FindContainingTreeViewItem(parent);
+            }
+
+            return null;
+        }
+
+        public static TreeViewItem FindTreeViewItem(ItemsControl container, object item)
+        {
+            if (container != null)
+            {
+                if (container.DataContext == item)
+                {
+                    return container as TreeViewItem;
+                }
+
+                // Expand the current container
+                if (container is TreeViewItem && !((TreeViewItem)container).IsExpanded)
+                {
+                    container.SetValue(TreeViewItem.IsExpandedProperty, true);
+                }
+
+                // Try to generate the ItemsPresenter and the ItemsPanel.
+                // by calling ApplyTemplate.  Note that in the
+                // virtualizing case even if the item is marked
+                // expanded we still need to do this step in order to
+                // regenerate the visuals because they may have been virtualized away.
+
+                container.ApplyTemplate();
+                ItemsPresenter itemsPresenter =
+                    (ItemsPresenter)container.Template.FindName("ItemsHost", container);
+                if (itemsPresenter != null)
+                {
+                    itemsPresenter.ApplyTemplate();
+                }
+                else
+                {
+                    // The Tree template has not named the ItemsPresenter,
+                    // so walk the descendents and find the child.
+                    itemsPresenter = FindVisualChild<ItemsPresenter>(container);
+                    if (itemsPresenter == null)
+                    {
+                        container.UpdateLayout();
+
+                        itemsPresenter = FindVisualChild<ItemsPresenter>(container);
+                    }
+                }
+
+                Panel itemsHostPanel = (Panel)VisualTreeHelper.GetChild(itemsPresenter, 0);
+
+
+                // Ensure that the generator for this panel has been created.
+                UIElementCollection children = itemsHostPanel.Children;
+
+                var virtualizingPanel =
+                    itemsHostPanel as IndexableVirtualizingStackPanel;
+
+                for (int i = 0, count = container.Items.Count; i < count; i++)
+                {
+                    TreeViewItem subContainer;
+                    if (virtualizingPanel != null)
+                    {
+                        // Bring the item into view so
+                        // that the container will be generated.
+                        virtualizingPanel.BringIntoView(i);
+
+                        subContainer =
+                            (TreeViewItem)container.ItemContainerGenerator.
+                            ContainerFromIndex(i);
+                    }
+                    else
+                    {
+                        subContainer =
+                            (TreeViewItem)container.ItemContainerGenerator.
+                            ContainerFromIndex(i);
+
+                        // Bring the item into view to maintain the
+                        // same behavior as with a virtualizing panel.
+                        subContainer.BringIntoView();
+                    }
+
+                    if (subContainer != null)
+                    {
+                        // Search the next level for the object.
+                        TreeViewItem resultContainer = FindTreeViewItem(subContainer, item);
+                        if (resultContainer != null)
+                        {
+                            return resultContainer;
+                        }
+                        else
+                        {
+                            // The object is not under this TreeViewItem
+                            // so collapse it.
+                            subContainer.IsExpanded = false;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
