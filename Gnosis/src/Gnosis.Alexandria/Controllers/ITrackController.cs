baca458260e4bd86b0a32b7611a57c86758d8ed9
dan.poage <dan.poage@gmail.com>
2011-03-31 21:29:38 +0000

    Moved playback controls into PlaybackView user control and cleaned up MainWindow

--- Gnosis/src/Gnosis.Alexandria/Controllers/ITrackController.cs
+++ Gnosis/src/Gnosis.Alexandria/Controllers/ITrackController.cs
@@ -8,7 +8,16 @@ namespace Gnosis.Alexandria.Controllers
 {
     public interface ITrackController : IRepository<ITrack>
     {
+        IEnumerable<ITrack> Tracks { get; }
+        int TrackCount { get; }
+        ITrack GetTrackAt(int index);
+
         ITrack ReadFromTag(string path);
         void LoadDirectory(DirectoryInfo directory);
+        void Filter(string search);
+        int IndexOf(ITrack track);
+        void ClearTracks();
+        void AddTrack(ITrack track);
+        ITrack GetSelectedTrack();
     }
 }
