fa496283f32b89ac699fe80a1be0f15096d569ca
dan.poage <dan.poage@gmail.com>
2011-03-31 00:42:22 +0000

    Created Controllers folder Created Controller interfaces and implementations for Tracks, Sources and Tags Created SourceView and moved all source related logic there Created Events folder and defined SourceLoadedEventArgs for SourceView

---
+++ Gnosis/src/Gnosis.Alexandria/Controllers/TagController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TagLib;
+
+namespace Gnosis.Alexandria.Controllers
+{
+    public class TagController : ITagController
+    {
+        public File GetFile(string path)
+        {
+            return File.Create(path);
+        }
+
+        public Tag GetTag(string path)
+        {
+            var file = GetFile(path);
+            return file.Tag;
+        }
+
+        public void LoadPicture(ITrack track)
+        {
+            var file = GetFile(track.Path);
+            if (file != null && file.Tag != null && file.Tag.Pictures.Length > 0)
+            {
+                track.ImageData = file.Tag.Pictures[0].Data;
+            }
+        }
+
+        public void SaveTag(ITrack track)
+        {
+            var file = GetFile(track.Path);
+            if (file.Tag != null)
+            {
+                if (!string.IsNullOrEmpty(track.Title))
+                    file.Tag.Title = track.Title;
+
+                if (!string.IsNullOrEmpty(track.Album))
+                    file.Tag.Album = track.Album;
+
+                file.Tag.Track = track.TrackNumber;
+                file.Tag.Disc = track.DiscNumber;
+
+                if (!string.IsNullOrEmpty(track.Artist))
+                    file.Tag.Performers = track.Artist.Split(',', ';');
+
+                if (!string.IsNullOrEmpty(track.Genre))
+                    file.Tag.Genres = track.Genre.Split(',', ';');
+
+                file.Tag.Year = Convert.ToUInt32(track.ReleaseYear);
+
+                file.Save();
+            }
+        }
+    }
+}
---
+++ Gnosis/src/Gnosis.Alexandria/Events/SourceLoadedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Alexandria.Events
+{
+    public class SourceLoadedEventArgs : EventArgs
+    {
+        public SourceLoadedEventArgs(ISource source)
+        {
+            Source = source;
+        }
+
+        public ISource Source
+        {
+            private set;
+            get;
+        }
+    }
+}
