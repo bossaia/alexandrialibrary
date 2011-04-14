9fac99262689fb8874b6896de7b2b51520ee21c3
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2011-04-14 19:03:25 +0000

    Cleaned up duration and elapsed labels.

---
+++ trunk/Gnosis/src/Gnosis.Core/TimeSpanExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core
+{
+    public static class TimeSpanExtensions
+    {
+        public static string ToFormattedString(this TimeSpan self)
+        {
+            if (self.Hours > 0)
+                return string.Format("{0}:{1:00}:{2:00}", self.Hours, self.Minutes, self.Seconds);
+            else return string.Format("{0}:{1:00}", self.Minutes, self.Seconds);
+        }
+    }
+}
