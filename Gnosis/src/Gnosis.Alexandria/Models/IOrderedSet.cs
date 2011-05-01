5e6cb88a54acc399e3849e8e1302a3ab124dfb14
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-05-01 07:00:41 +0000

    Started simplifying the new ITrack interface

---
+++ Gnosis/src/Gnosis.Alexandria/Models/IOrderedSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Alexandria.Models
+{
+    public interface IOrderedSet<T>
+        : IEnumerable<T>
+    {
+        int Count { get; }
+        T this[int index] { get; }
+
+        bool Contains(T item);
+        int IndexOf(T item);
+    }
+}
