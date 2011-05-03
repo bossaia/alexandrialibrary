c277507bca06b29ff7248ae7e9f4f54232efde8b
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-05-03 23:32:22 +0000

    More work on ChangeableOrderedSet implementation Created a Gnosis.Alexandria.Tests project for tests

---
+++ Gnosis/src/Gnosis.Alexandria.Tests/CollectionTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gnosis.Alexandria.Tests
+{
+    [TestClass]
+    public class CollectionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
