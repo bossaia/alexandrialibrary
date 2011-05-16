e8dbf3a32080241c4a3153c314b84d3799dbfadc
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-05-16 06:44:16 +0000

    Created FeedItem, FeedLink and FeedMetadata implementations More work on FeedRepository implementation Added Parameters property to ISearch

---
+++ Gnosis/src/Gnosis.Alexandria/Models/Feeds/FeedLink.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Alexandria.Models.Feeds
+{
+    public class FeedLink
+        : ValueBase, IFeedLink
+    {
+        public FeedLink(string relationship, Uri location, string mediaType, uint length, string language)
+        {
+            this.relationship = relationship;
+            this.location = location;
+            this.mediaType = mediaType;
+            this.length = length;
+            this.language = language;
+        }
+
+        private readonly string relationship;
+        private readonly Uri location;
+        private readonly string mediaType;
+        private readonly uint length;
+        private readonly string language;
+
+        #region IFeedLink Members
+
+        public string Relationship
+        {
+            get { return relationship; }
+        }
+
+        public Uri Location
+        {
+            get { return location; }
+        }
+
+        public string MediaType
+        {
+            get { return mediaType; }
+        }
+
+        public uint Length
+        {
+            get { return length; }
+        }
+
+        public string Language
+        {
+            get { return language; }
+        }
+
+        #endregion
+    }
+}
---
+++ Gnosis/src/Gnosis.Alexandria/Models/Feeds/FeedMetadata.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Alexandria.Models.Feeds
+{
+    public class FeedMetadata
+        : ValueBase, IFeedMetadata
+    {
+        public FeedMetadata(string mediaType, Uri scheme, string name, string content)
+        {
+            this.mediaType = mediaType;
+            this.scheme = scheme;
+            this.name = name;
+            this.content = content;
+        }
+
+        private readonly string mediaType;
+        private readonly Uri scheme;
+        private readonly string name;
+        private readonly string content;
+
+        #region IFeedMetadata Members
+
+        public string MediaType
+        {
+            get { return mediaType; }
+        }
+
+        public Uri Scheme
+        {
+            get { return scheme; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public string Content
+        {
+            get { return content; }
+        }
+
+        #endregion
+    }
+}
