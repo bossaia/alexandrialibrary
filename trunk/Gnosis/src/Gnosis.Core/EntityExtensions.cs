7db7dfeafc044f1cad795832e58b6c9b4e5eda6b
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2011-05-17 17:24:15 +0000

    Created EntityExtensions class More work on RepositoryBase and TypeExtensions

---
+++ trunk/Gnosis/src/Gnosis.Core/EntityExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+using Gnosis.Core.Commands;
+
+namespace Gnosis.Core
+{
+    public static class EntityExtensions
+    {
+        public static IDbCommand GetSaveCommand<T>(this IEnumerable<T> items, IDbConnection connection)
+            where T : IEntity
+        {
+            var builder = new SaveCommandBuilder();
+
+
+
+            return builder.GetCommand(connection);
+        }
+
+        public static IDbCommand GetDeleteCommand<T>(this IEnumerable<T> items, IDbConnection connection)
+            where T : IEntity
+        {
+            var builder = new SaveCommandBuilder();
+
+            return builder.GetCommand(connection);
+        }
+    }
+}
