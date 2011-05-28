d2b33abce7fed09333309b071167a1f29930db56
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2011-05-28 00:38:24 +0000

    Defined a default search for Feeds

---
+++ trunk/Gnosis/src/Gnosis.Alexandria/Repositories/Feeds/SearchAllFeeds.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Gnosis.Alexandria.Models.Feeds;
+
+namespace Gnosis.Alexandria.Repositories.Feeds
+{
+    public class SearchAllFeeds
+        : SearchBase<IFeed>
+    {
+        public SearchAllFeeds()
+            : base("SearchAllFeeds", string.Empty, "Authors ASC, UpdatedDate ASC, Title ASC", new List<string> { "Authors", "UpdatedDate", "Title" }, true)
+        {
+        }
+    }
+}
