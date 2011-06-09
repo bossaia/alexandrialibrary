4b0f418ba8b8ce4b41908798308a26311afb2cc0
dan.poage <dan.poage@gmail.com>
2011-06-09 23:11:10 +0000

    1. Added latest 1.0.60.0 version of SQLite libraries to lib and removed the old version that I had there 2. Finished changes to get repository tests working against in-memory database

--- Gnosis/src/Gnosis.Alexandria/Views/MainView.xaml.cs
+++ Gnosis/src/Gnosis.Alexandria/Views/MainView.xaml.cs
@@ -36,96 +36,13 @@ namespace Gnosis.Alexandria.Views
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Models.Feeds.IFeed GetTestFeed(IContext context, ILogger logger)
-        {
-            var feed = new Models.Feeds.Feed();
-            feed.Initialize(new EntityInitialState(context, logger));
-            feed.Authors = "Bill Simmons";
-            feed.Contributors = "Joe House, Marc Stein, John Hollinger";
-            feed.Copyright = "c 2009-2011";
-            feed.Description = "Sports etc.";
-            feed.FeedIdentifier = "12345ABC";
-            feed.Generator = "espn.go.com";
-            feed.Language = "en-us";
-            feed.Location = new Uri("http://espn.go.com/espnradio/feeds/rss/podcast.xml?id=2864045");
-            feed.MediaType = "application/xml+rss";
-            feed.OriginalLocation = new Uri("http://espn.go.com/espnradio/feeds/rss/podcast.xml?id=2864045");
-            feed.PublishedDate = new DateTime(2009, 2, 13);
-            feed.UpdatedDate = new DateTime(2011, 6, 7);
-            feed.Title = "BS Report";
-            feed.IconPath = new Uri("http://assets.espn.go.com/i/espnradio/podcast/bsreport_subway_300.jpg"); 
-            feed.ImagePath = new Uri("http://assets.espn.go.com/i/espnradio/podcast/bsreport_subway_300.jpg");
-            feed.AddCategory(new Models.Feeds.FeedCategory(feed.Id, UriExtensions.EmptyUri, "Sports", "Sports"));
-            feed.AddCategory(new Models.Feeds.FeedCategory(feed.Id, UriExtensions.EmptyUri, "Comedy", "Comedy"));
-            feed.AddLink(new Models.Feeds.FeedLink(feed.Id, "self", new Uri("http://espn.go.com/espnradio/feeds/rss/podcast.xml?id=2864045"), "application/xml+rss", 0, "en-us"));
-            feed.AddLink(new Models.Feeds.FeedLink(feed.Id, "alt", new Uri("http://espn.go.com/espnradio"), "text/html", 0, "en-us"));
-            feed.AddLink(new Models.Feeds.FeedLink(feed.Id, "alt", new Uri("http://espn.go.com/epsnradio?lang=es-mx"), "text/html", 0, "es-mx"));
-            feed.AddMetadatum(new Models.Feeds.FeedMetadata(feed.Id, "text/plain", UriExtensions.EmptyUri, "tag", "Bill Simmons"));
-            feed.AddMetadatum(new Models.Feeds.FeedMetadata(feed.Id, "application/xml", UriExtensions.EmptyUri, "marquee", "<marquee><title>BS Report</title><subtitle>with Bill Simmons</subtitle></marquee>"));
-            
-            var item = new Models.Feeds.FeedItem();
-            item.Initialize(new EntityInitialState(context, logger, feed.Id, 0));
-            item.Authors = "Bill Simmons";
-            item.Contributors = "Joe House, Joe Mead";
-            item.Copyright = "Copyright ESPN 2011"; 
-            item.FeedItemIdentifier = "ZYZ235AMQ3792206";
-            item.Summary = "Bill previews the NBA Finals with ESPN experts";
-            item.PublishedDate = new DateTime(2011, 6, 5);
-            item.UpdatedDate = new DateTime(2011, 6, 5);
-            item.Title = "NBA Finals Preview (Part 1)";
-            item.TitleMediaType = "text/plain";
-            //item.AddCategory(new Models.Feeds.FeedCategory(item.Id, UriExtensions.EmptyUri, "Basketball", "Basketball"));
-            item.AddLink(new Models.Feeds.FeedLink(item.Id, "self", new Uri("http://espn.go.com/espnradio/media/xyz1.mp3"), "audio/mpeg", 0, "en-us"));
-            item.AddMetadatum(new Models.Feeds.FeedMetadata(item.Id, "text/plain", UriExtensions.EmptyUri, "rating", "4/5"));
-            item.AddMetadatum(new Models.Feeds.FeedMetadata(item.Id, "application/xml", UriExtensions.EmptyUri, "rating", "<rating><score>4</score><max>5</max></rating>"));
-            feed.AddItem(item);
-
-            var item2 = new Models.Feeds.FeedItem();
-            item2.Initialize(new EntityInitialState(context, logger, feed.Id, 0));
-            item2.Authors = "Bill Simmons";
-            item2.Contributors = "Joe House, Joe Mead";
-            item2.Copyright = "Copyright ESPN 2011";
-            item2.FeedItemIdentifier = "ZYZ235AMQ38564587";
-            item2.Summary = "Bill previews the NBA Finals with ESPN experts";
-            item2.PublishedDate = new DateTime(2011, 6, 5);
-            item2.UpdatedDate = new DateTime(2011, 6, 5);
-            item2.Title = "NBA Finals Preview (Part 2)";
-            item2.TitleMediaType = "text/plain";
-            item2.AddCategory(new Models.Feeds.FeedCategory(item2.Id, UriExtensions.EmptyUri, "Basketball", "Basketball"));
-            item2.AddLink(new Models.Feeds.FeedLink(item2.Id, "self", new Uri("http://espn.go.com/espnradio/media/xyz2.mp3"), "audio/mpeg", 0, "en-us"));
-            item2.AddMetadatum(new Models.Feeds.FeedMetadata(item2.Id, "text/plain", UriExtensions.EmptyUri, "rating", "4/5"));
-            item2.AddMetadatum(new Models.Feeds.FeedMetadata(item2.Id, "application/xml", UriExtensions.EmptyUri, "rating", "<rating><score>4</score><max>5</max></rating>"));
-            feed.AddItem(item2);
-
-            return feed;
-        }
-
-        private void ModifyTestFeed(Models.Feeds.IFeed feed)
-        {
-            feed.Title = "Some other title";
-            feed.FeedIdentifier = "NEW-ID-5634634636-NEW";
-            feed.Generator = "other-generator";
-
-            var firstCategory = feed.Categories.FirstOrDefault();
-            feed.RemoveCategory(firstCategory);
-
-
-            feed.RemoveItem(feed.Items.LastOrDefault());
-            feed.Items.FirstOrDefault().RemoveMetadatum(feed.Items.FirstOrDefault().Metadata.FirstOrDefault());
-            feed.Items.FirstOrDefault().AddCategory(new Models.Feeds.FeedCategory(feed.Items.FirstOrDefault().Id, new Uri("http://example.com/scheme"), "Misc.", "Misc."));
-            feed.AddMetadatum(new Models.Feeds.FeedMetadata(feed.Id, "text/html", new Uri("http://other.com/1/different-scheme"), "review", "<html><body><p>This feed is awesome</p><p>I <b>love</b> it so much!</p></body></html>"));
-            feedRepository.Save(new List<Models.Feeds.IFeed> { feed });
-        }
-
         public MainWindow()
         {
-            InitializeComponent();
-
-            log4net.Config.XmlConfigurator.Configure();
-
-
             try
             {
+                InitializeComponent();
+
+                log4net.Config.XmlConfigurator.Configure();
                 logger.Info("MainWindow.ctor: started");
 
                 tagController = new TagController();
@@ -150,32 +67,8 @@ namespace Gnosis.Alexandria.Views
                 feedRepository = new FeedRepository(context, logger);
                 feedRepository.Initialize();
 
-                //trackRepository = new TrackRepository(context, factory);
-
-                try
-                {
-                    var feed = feedRepository.Lookup(new LookupByLocation(new Uri("http://espn.go.com/espnradio/feeds/rss/podcast.xml?id=2864045")));
-                    if (feed == null)
-                    {
-                        feed = GetTestFeed(context, logger);
-                        feedRepository.Save(new List<Models.Feeds.IFeed> { feed });
-                    }
-                    //else ModifyTestFeed(feed);
-                }
-                catch (Exception ex)
-                {
-                    logger.Error("feedRepository.Save()", ex);
-                }
-
-                try
-                {
-                    var allFeeds = feedRepository.Search();
-                    var x = allFeeds.Count();
-                }
-                catch (Exception ex)
-                {
-                    logger.Error("feedRepository.Search()", ex);
-                }
+                var feeds = feedRepository.Search();
+                var x = feeds.Count();
             }
             catch (Exception ex)
             {
--- Gnosis/src/Gnosis.Tests/Repositories/FeedRepositoryTests.cs
+++ Gnosis/src/Gnosis.Tests/Repositories/FeedRepositoryTests.cs
@@ -22,13 +22,100 @@ namespace Gnosis.Tests.Repositories
         private IRepository<IFeed> repository;
         private IDbConnection connection;
 
+        private readonly Uri feedLocation = new Uri("http://espn.go.com/espnradio/feeds/rss/podcast.xml?id=2864045");
+
+        #region Helper Methods
+
+        private IFeed GetTestFeed(IContext context, ILogger logger)
+        {
+            var feed = new Feed();
+            feed.Initialize(new EntityInitialState(context, logger));
+            feed.Authors = "Bill Simmons";
+            feed.Contributors = "Joe House, Marc Stein, John Hollinger";
+            feed.Copyright = "c 2009-2011";
+            feed.Description = "Sports etc.";
+            feed.FeedIdentifier = "12345ABC";
+            feed.Generator = "espn.go.com";
+            feed.Language = "en-us";
+            feed.Location = feedLocation;
+            feed.MediaType = "application/xml+rss";
+            feed.OriginalLocation = new Uri("http://espn.go.com/espnradio/feeds/rss/podcast.xml?id=2864045");
+            feed.PublishedDate = new DateTime(2009, 2, 13);
+            feed.UpdatedDate = new DateTime(2011, 6, 7);
+            feed.Title = "BS Report";
+            feed.IconPath = new Uri("http://assets.espn.go.com/i/espnradio/podcast/bsreport_subway_300.jpg");
+            feed.ImagePath = new Uri("http://assets.espn.go.com/i/espnradio/podcast/bsreport_subway_300.jpg");
+            feed.AddCategory(new FeedCategory(feed.Id, UriExtensions.EmptyUri, "Sports", "Sports"));
+            feed.AddCategory(new FeedCategory(feed.Id, UriExtensions.EmptyUri, "Comedy", "Comedy"));
+            feed.AddLink(new FeedLink(feed.Id, "self", new Uri("http://espn.go.com/espnradio/feeds/rss/podcast.xml?id=2864045"), "application/xml+rss", 0, "en-us"));
+            feed.AddLink(new FeedLink(feed.Id, "alt", new Uri("http://espn.go.com/espnradio"), "text/html", 0, "en-us"));
+            feed.AddLink(new FeedLink(feed.Id, "alt", new Uri("http://espn.go.com/epsnradio?lang=es-mx"), "text/html", 0, "es-mx"));
+            feed.AddMetadatum(new FeedMetadata(feed.Id, "text/plain", UriExtensions.EmptyUri, "tag", "Bill Simmons"));
+            feed.AddMetadatum(new FeedMetadata(feed.Id, "application/xml", UriExtensions.EmptyUri, "marquee", "<marquee><title>BS Report</title><subtitle>with Bill Simmons</subtitle></marquee>"));
+
+            var item = new FeedItem();
+            item.Initialize(new EntityInitialState(context, logger, feed.Id, 0));
+            item.Authors = "Bill Simmons";
+            item.Contributors = "Joe House, Joe Mead";
+            item.Copyright = "Copyright ESPN 2011";
+            item.FeedItemIdentifier = "ZYZ235AMQ3792206";
+            item.Summary = "Bill previews the NBA Finals with ESPN experts";
+            item.PublishedDate = new DateTime(2011, 6, 5);
+            item.UpdatedDate = new DateTime(2011, 6, 5);
+            item.Title = "NBA Finals Preview (Part 1)";
+            item.TitleMediaType = "text/plain";
+            //item.AddCategory(new Models.Feeds.FeedCategory(item.Id, UriExtensions.EmptyUri, "Basketball", "Basketball"));
+            item.AddLink(new FeedLink(item.Id, "self", new Uri("http://espn.go.com/espnradio/media/xyz1.mp3"), "audio/mpeg", 0, "en-us"));
+            item.AddMetadatum(new FeedMetadata(item.Id, "text/plain", UriExtensions.EmptyUri, "rating", "4/5"));
+            item.AddMetadatum(new FeedMetadata(item.Id, "application/xml", UriExtensions.EmptyUri, "rating", "<rating><score>4</score><max>5</max></rating>"));
+            feed.AddItem(item);
+
+            var item2 = new FeedItem();
+            item2.Initialize(new EntityInitialState(context, logger, feed.Id, 0));
+            item2.Authors = "Bill Simmons";
+            item2.Contributors = "Joe House, Joe Mead";
+            item2.Copyright = "Copyright ESPN 2011";
+            item2.FeedItemIdentifier = "ZYZ235AMQ38564587";
+            item2.Summary = "Bill previews the NBA Finals with ESPN experts";
+            item2.PublishedDate = new DateTime(2011, 6, 5);
+            item2.UpdatedDate = new DateTime(2011, 6, 5);
+            item2.Title = "NBA Finals Preview (Part 2)";
+            item2.TitleMediaType = "text/plain";
+            item2.AddCategory(new FeedCategory(item2.Id, UriExtensions.EmptyUri, "Basketball", "Basketball"));
+            item2.AddLink(new FeedLink(item2.Id, "self", new Uri("http://espn.go.com/espnradio/media/xyz2.mp3"), "audio/mpeg", 0, "en-us"));
+            item2.AddMetadatum(new FeedMetadata(item2.Id, "text/plain", UriExtensions.EmptyUri, "rating", "4/5"));
+            item2.AddMetadatum(new FeedMetadata(item2.Id, "application/xml", UriExtensions.EmptyUri, "rating", "<rating><score>4</score><max>5</max></rating>"));
+            feed.AddItem(item2);
+
+            return feed;
+        }
+
+        private void ModifyTestFeed(IFeed feed)
+        {
+            feed.Title = "Some other title";
+            feed.FeedIdentifier = "NEW-ID-5634634636-NEW";
+            feed.Generator = "other-generator";
+
+            var firstCategory = feed.Categories.FirstOrDefault();
+            feed.RemoveCategory(firstCategory);
+
+
+            feed.RemoveItem(feed.Items.LastOrDefault());
+            feed.Items.FirstOrDefault().RemoveMetadatum(feed.Items.FirstOrDefault().Metadata.FirstOrDefault());
+            feed.Items.FirstOrDefault().AddCategory(new FeedCategory(feed.Items.FirstOrDefault().Id, new Uri("http://example.com/scheme"), "Misc.", "Misc."));
+            feed.AddMetadatum(new FeedMetadata(feed.Id, "text/html", new Uri("http://other.com/1/different-scheme"), "review", "<html><body><p>This feed is awesome</p><p>I <b>love</b> it so much!</p></body></html>"));
+            //feedRepository.Save(new List<Models.Feeds.IFeed> { feed });
+        }
+
+        #endregion
+
         [TestFixtureSetUp]
         public void Setup()
         {
             context = new SingleThreadedContext();
             logger = new DummyLogger();
             
-            connection = new SQLiteConnection(":memory:");
+            connection = new SQLiteConnection("Data Source=:memory:;Version=3;");
             connection.Open();
 
             repository = new FeedRepository(context, logger, connection);
@@ -45,6 +132,22 @@ namespace Gnosis.Tests.Repositories
         [Test]
         public void TestInsert()
         {
+            var initialFeeds = repository.Search();
+            Assert.AreEqual(0, initialFeeds.Count());
+
+            var feed = GetTestFeed(context, logger);
+
+            repository.Save(new List<IFeed> { feed });
+
+            var createdFeed = repository.Lookup(new LookupByLocation(feedLocation));
+
+            Assert.IsNotNull(createdFeed);
+
+            repository.Delete(new List<IFeed> { feed });
+
+            var deletedFeed = repository.Lookup(new LookupByLocation(feedLocation));
+
+            Assert.IsNull(deletedFeed);
         }
     }
 }
