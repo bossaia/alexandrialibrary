9b0f1c8101269a696a18cd3291ab6634ed189e7e
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2011-06-13 23:02:29 +0000

    Added album cover image to test assembly for testing track pictures More work on TrackRepositoryTests

---
+++ trunk/Gnosis/src/Gnosis.Core/ImageExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core
+{
+    public static class ImageExtensions
+    {
+        public static byte[] ToBytes(this Image image)
+        {
+            var stream = new MemoryStream();
+            image.Save(stream, System.Drawing.Imaging.ImageFormat.Bmp);
+            return stream.ToArray();
+        }
+
+        public static Image ToImage(this byte[] bytes)
+        {
+            var stream = new MemoryStream(bytes);
+            return Image.FromStream(stream);
+        }
+    }
+}
