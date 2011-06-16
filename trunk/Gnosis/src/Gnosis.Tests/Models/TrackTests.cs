7d47aabb698326f08f67cb09c0a53f036914ba0d
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2011-06-16 13:58:59 +0000

    Started work on TrackTests

---
+++ trunk/Gnosis/src/Gnosis.Tests/Models/TrackTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace Gnosis.Tests.Models
+{
+    [TestFixture]
+    public class TrackTests
+    {
+        private const string location = @"Files\03 - Antes De Las Seis.mp3";
+
+        [Test]
+        public void TestLyrics()
+        {
+            Assert.IsTrue(File.Exists(location));
+        }
+    }
+}
