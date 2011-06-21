ed5d93810cb95eedf39e2573e7a73d7c0549c129
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2011-06-21 19:31:01 +0000

    1. Added an IsMixedAlphaNumeric extension method to StringExtensions for use in LanguageTag parsing 2. Finished implementing Parse an ToString methods on LanguageTag 3. Created LanguageTagTests to test all of the various parsing scenarios

--- trunk/Gnosis/src/Gnosis.Core/Ietf/LanguageTag.cs
+++ trunk/Gnosis/src/Gnosis.Core/Ietf/LanguageTag.cs
@@ -12,15 +12,24 @@ namespace Gnosis.Core.Ietf
     public class LanguageTag
         : ILanguageTag
     {
-        private LanguageTag(ILanguage primaryLanguage)
+        private LanguageTag(ILanguage primaryLanguage, ICountry country)
+            : this(primaryLanguage, null, null, country, null, new List<string>(), new List<string>(), null)
         {
-            this.primaryLanguage = primaryLanguage;
         }
 
-        private LanguageTag(ILanguage primaryLanguage, ICountry country)
+        private LanguageTag(ILanguage primaryLanguage, string extendedLanguage, IScript script, ICountry country, IRegion region, IEnumerable<string> variants, IEnumerable<string> extensions, string privateUse)
         {
+            if (primaryLanguage == null)
+                throw new ArgumentNullException("primaryLanguage");
+
             this.primaryLanguage = primaryLanguage;
+            this.extendedLanguage = extendedLanguage;
+            this.script = script;
             this.country = country;
+            this.region = region;
+            this.variants = variants;
+            this.extensions = extensions;
+            this.privateUse = privateUse;
         }
 
         private readonly ILanguage primaryLanguage;
@@ -31,47 +40,7 @@ namespace Gnosis.Core.Ietf
         private readonly IEnumerable<string> variants;
         private readonly IEnumerable<string> extensions;
         private readonly string privateUse;
-        private readonly IDictionary<string, ILanguage> grandfatheredTags = new Dictionary<string, ILanguage>();
-        //private readonly IDictionary<string, ILanguage> redundantTags = new Dictionary<string, ILanguage>();
-
-        #region Grandfathered And Redundant Tags
-
-        /// <summary>
-        /// Initialize the list of valid grandfathered tags mapped to their preferred language value
-        /// </summary>
-        /// <remarks>Taken from the IANA registry at: http://www.iana.org/assignments/language-subtag-registry </remarks>
-        private void InitializeGrandfatheredTags()
-        {
-            grandfatheredTags.Add("art-lojban",  Language.Lojban);
-            grandfatheredTags.Add("cel-gualish", Language.Celtic);
-            grandfatheredTags.Add("en-gb-oed", Language.English);
-            grandfatheredTags.Add("i-ami", Language.Amis);
-            grandfatheredTags.Add("i-bnn", Language.Bunun);
-            grandfatheredTags.Add("i-default", Language.Undetermined);
-            grandfatheredTags.Add("i-enochian", Language.Sumerian);
-            grandfatheredTags.Add("i-hak", Language.Hakka);
-            grandfatheredTags.Add("i-klingon", Language.Klingon);
-            grandfatheredTags.Add("i-lux", Language.Luxembourgish);
-            grandfatheredTags.Add("i-mingo", Language.Iroquoian);
-            grandfatheredTags.Add("i-navajo", Language.Navajo);
-            grandfatheredTags.Add("i-pwn", Language.Paiwan);
-            grandfatheredTags.Add("i-tao", Language.Tao);
-            grandfatheredTags.Add("i-tay", Language.Tayal);
-            grandfatheredTags.Add("i-tsu", Language.Tsou);
-            grandfatheredTags.Add("no-bok", Language.NorwegianBokmal);
-            grandfatheredTags.Add("no-nyn", Language.NorwegianNynorsk);
-            grandfatheredTags.Add("sgn-be-fr", Language.BelgianFrenchSignLanguage);
-            grandfatheredTags.Add("sgn-be-nl", Language.BelgianFlemishSignLanguage);
-            grandfatheredTags.Add("sgn-ch-de", Language.SwissGermanSignLanguage);
-            grandfatheredTags.Add("zh-guoyu", Language.MandarinChinese);
-            grandfatheredTags.Add("zh-hakka", Language.Hakka);
-            grandfatheredTags.Add("zh-min", Language.Chinese);
-            grandfatheredTags.Add("zh-min-nan", Language.MinanHokkien);
-            grandfatheredTags.Add("zh-xiang", Language.Xiang);
-        }
-
-        #endregion
-
+        
         #region ILanguageTag Members
 
         public ILanguage PrimaryLanguage
@@ -118,9 +87,202 @@ namespace Gnosis.Core.Ietf
 
         public override string ToString()
         {
+            const string alphaFormat = "-{0}";
+            const string numFormat = "-{0:000}";
+
  	        var builder = new StringBuilder();
+
+            builder.Append(primaryLanguage.ToString());
+
+            if (!string.IsNullOrEmpty(extendedLanguage))
+                builder.AppendFormat(alphaFormat, extendedLanguage);
+
+            if (script != null)
+                builder.AppendFormat(alphaFormat, script.ToString());
+
+            if (country != null)
+                builder.AppendFormat(alphaFormat, country.ToString());
+
+            if (region != null)
+                builder.AppendFormat(numFormat, region.Code);
+
+            if (variants != null && variants.Count() > 0)
+            {
+                foreach (var variant in variants)
+                    builder.AppendFormat(alphaFormat, variant);
+            }
+
+            if (extensions != null && extensions.Count() > 0)
+            {
+                foreach (var extension in extensions)
+                    builder.AppendFormat(alphaFormat, extension);
+            }
+
+            if (!string.IsNullOrEmpty(privateUse))
+                builder.AppendFormat(alphaFormat, privateUse);
 
             return builder.ToString();
         }
+
+        static LanguageTag()
+        {
+            InitializeGrandfatheredTags();
+        }
+
+        private static readonly IDictionary<string, Tuple<ILanguage, ICountry>> grandfatheredTags = new Dictionary<string, Tuple<ILanguage, ICountry>>();
+
+        #region InitializeGrandfatheredTags
+
+        /// <summary>
+        /// Initialize the list of valid grandfathered tags mapped to their preferred language value and (optionally) country
+        /// </summary>
+        /// <remarks>Taken from the IANA registry at: http://www.iana.org/assignments/language-subtag-registry </remarks>
+        private static void InitializeGrandfatheredTags()
+        {
+            grandfatheredTags.Add("art-lojban", new Tuple<ILanguage, ICountry>(Language.Lojban, null));
+            grandfatheredTags.Add("cel-gualish", new Tuple<ILanguage, ICountry>(Language.Celtic, null));
+            grandfatheredTags.Add("en-gb-oed", new Tuple<ILanguage, ICountry>(Language.English, Iso.Country.UnitedKingdom));
+            grandfatheredTags.Add("i-ami", new Tuple<ILanguage, ICountry>(Language.Amis, null));
+            grandfatheredTags.Add("i-bnn", new Tuple<ILanguage, ICountry>(Language.Bunun, null));
+            grandfatheredTags.Add("i-default", new Tuple<ILanguage, ICountry>(Language.UncodedLanguages, null));
+            grandfatheredTags.Add("i-enochian", new Tuple<ILanguage, ICountry>(Language.UncodedLanguages, null));
+            grandfatheredTags.Add("i-hak", new Tuple<ILanguage, ICountry>(Language.Hakka, null));
+            grandfatheredTags.Add("i-klingon", new Tuple<ILanguage, ICountry>(Language.Klingon, null));
+            grandfatheredTags.Add("i-lux", new Tuple<ILanguage, ICountry>(Language.Luxembourgish, null));
+            grandfatheredTags.Add("i-mingo", new Tuple<ILanguage, ICountry>(Language.Iroquoian, null));
+            grandfatheredTags.Add("i-navajo", new Tuple<ILanguage, ICountry>(Language.Navajo, null));
+            grandfatheredTags.Add("i-pwn", new Tuple<ILanguage, ICountry>(Language.Paiwan, null));
+            grandfatheredTags.Add("i-tao", new Tuple<ILanguage, ICountry>(Language.Tao, null));
+            grandfatheredTags.Add("i-tay", new Tuple<ILanguage, ICountry>(Language.Tayal, null));
+            grandfatheredTags.Add("i-tsu", new Tuple<ILanguage, ICountry>(Language.Tsou, null));
+            grandfatheredTags.Add("no-bok", new Tuple<ILanguage, ICountry>(Language.NorwegianBokmal, null));
+            grandfatheredTags.Add("no-nyn", new Tuple<ILanguage, ICountry>(Language.NorwegianNynorsk, null));
+            grandfatheredTags.Add("sgn-be-fr", new Tuple<ILanguage, ICountry>(Language.BelgianFrenchSignLanguage, null));
+            grandfatheredTags.Add("sgn-be-nl", new Tuple<ILanguage, ICountry>(Language.BelgianFlemishSignLanguage, null));
+            grandfatheredTags.Add("sgn-ch-de", new Tuple<ILanguage, ICountry>(Language.SwissGermanSignLanguage, null));
+            grandfatheredTags.Add("zh-guoyu", new Tuple<ILanguage, ICountry>(Language.MandarinChinese, null));
+            grandfatheredTags.Add("zh-hakka", new Tuple<ILanguage, ICountry>(Language.Hakka, null));
+            grandfatheredTags.Add("zh-min", new Tuple<ILanguage, ICountry>(Language.Chinese, Iso.Country.Taiwan));
+            grandfatheredTags.Add("zh-min-nan", new Tuple<ILanguage, ICountry>(Language.MinanHokkien, null));
+            grandfatheredTags.Add("zh-xiang", new Tuple<ILanguage, ICountry>(Language.Xiang, null));
+        }
+
+        #endregion
+
+        #region Public Static Methods
+
+        public static ILanguageTag Parse(string value)
+        {
+            if (value == null)
+                return null;
+
+            ILanguage primaryLanguage = null;
+            string extendedLanguage = null;
+            IScript script = null;
+            ICountry country = null;
+            var regionCode = -1;
+            IRegion region = null;
+            var variants = new List<string>();
+            var extensions = new List<string>();
+            string privateUse = null;
+
+            var lower = value.ToLower();
+            if (grandfatheredTags.ContainsKey(lower))
+            {
+                primaryLanguage = grandfatheredTags[lower].Item1;
+                country = grandfatheredTags[lower].Item2;
+                return new LanguageTag(primaryLanguage, country);
+            }
+
+            var count = 1;
+            var singleton = string.Empty;
+            foreach (var token in value.Split(new char[] {'-'}, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (count == 1)
+                {
+                    primaryLanguage = Language.GetLanguageByCode(token);
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(singleton))
+                    {
+                        switch (token.Length)
+                        {
+                            case 1:
+                                singleton = token;
+                                break;
+                            case 2:
+                                {
+                                    if (token.IsMixedAlphaNumeric())
+                                    {
+                                        variants.Add(token);
+                                    }
+                                    else
+                                    {
+                                        country = Iso.Country.GetCountryByCode(token);
+                                    }
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    if (token.IsMixedAlphaNumeric())
+                                    {
+                                        variants.Add(token);
+                                    }
+                                    else
+                                    {
+                                        if (int.TryParse(token, out regionCode))
+                                            region = UN.Region.GetRegionByCode(regionCode);
+                                        else
+                                            extendedLanguage = token;
+                                    }
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    if (token.IsMixedAlphaNumeric())
+                                    {
+                                        variants.Add(token);
+                                    }
+                                    else
+                                    {
+                                        script = Iso.Script.GetScriptByCode(token);
+                                    }
+                                    break;
+                                }
+                            case 5:
+                            case 6:
+                            case 7:
+                            case 8:
+                                {
+                                    variants.Add(token);
+                                    break;
+                                }
+                        }
+                    }
+                    else
+                    {
+                        if (singleton.ToLower() == "x")
+                        {
+                            if (token.Length >= 1 && token.Length <= 8)
+                            {
+                                privateUse = string.Format("{0}-{1}", singleton, token);
+                            }
+                        }
+                        else if (token.Length >= 2 && token.Length <= 8)
+                        {
+                            extensions.Add(string.Format("{0}-{1}", singleton, token));
+                        }
+                        singleton = string.Empty;
+                    }
+                }
+
+                count++;
+            }
+
+            return (primaryLanguage != null && primaryLanguage != Language.Undetermined) ? new LanguageTag(primaryLanguage, extendedLanguage, script, country, region, variants, extensions, privateUse) : null;
+        }
+
+        #endregion
     }
 }
