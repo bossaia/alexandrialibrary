643f7a3282e9e383682480114ba7f75bfc52d1b9
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2011-06-28 01:24:40 +0000

    Created ViewModels namespace in Alexandria and started work defining ITreeNode Defined all of the interfaces associated with RSS Feeds

--- trunk/Gnosis/src/Gnosis.Core/Rss/IRssFeed.cs
+++ trunk/Gnosis/src/Gnosis.Core/Rss/IRssFeed.cs
@@ -3,9 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Gnosis.Core.Ietf;
+using Gnosis.Core.W3c;
+
 namespace Gnosis.Core.Rss
 {
+    /// <summary>
+    /// Defines an RSS 2.0 Feed based on the unofficial RSS specification
+    /// </summary>
+    /// <remarks>http://cyber.law.harvard.edu/rss/rss.html</remarks>
     public interface IRssFeed
     {
+        string Title { get; set; }
+        Uri Link { get; set; }
+        string Description { get; set; }
+        ILanguageTag Language { get; set; }
+        string Copyright { get; set; }
+        string ManagingEditorName { get; set; }
+        Uri ManagingEditorEmail { get; set; }
+        string WebMasterName { get; set; }
+        Uri WebMasterEmail { get; set; }
+        DateTime PublicationDate { get; set; }
+        DateTime LastBuildDate { get; set; }
+        string Generator { get; set; }
+        Uri Documents { get; set; }
+        string Cloud { get; set; }
+        TimeSpan TimeToLive { get; set; }
+        IRssImage Image { get; set; }
+        IPicsRating Rating { get; set; }
+        string TextInput { get; set; }
+        IEnumerable<RssHour> SkipHours { get; set; }
+        IEnumerable<RssDay> SkipDays { get; set; }
+
+        IEnumerable<IRssCategory> Categories { get; }
+        IEnumerable<IRssFeedItem> Items { get; }
+
+        void AddCategory(IRssCategory category);
+        void RemoveCategory(IRssCategory category);
+
+        void AddItem(IRssFeedItem item);
+        void RemoveItem(IRssFeedItem item);
     }
 }
---
+++ trunk/Gnosis/src/Gnosis.Core/Rss/RssDay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core.Rss
+{
+    public enum RssDay
+    {
+        Sunday = 1,
+        Monday = 2,
+        Tuesday = 3,
+        Wednesday = 4,
+        Thursday = 5,
+        Friday = 6,
+        Saturday = 7
+    }
+}
