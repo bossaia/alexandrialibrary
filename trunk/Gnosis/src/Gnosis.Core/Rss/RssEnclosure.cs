6a88d78d2b4304f14ec5f76b1e43dd289f8ab590
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2011-07-01 07:08:42 +0000

    1. Changes to IRssEnclosure to match the RSS 2.0 specification and implemented RssEnclosure 2. Added logic to ContentType.GetContentType to look for byte order marks in text files to determine the character set 3. Added logic to ContentType.GetContentType to look for an encoding attribute in the XML declaration to determine the character set 4. More work on content type tests

---
+++ trunk/Gnosis/src/Gnosis.Core/Rss/RssEnclosure.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Gnosis.Core.W3c;
+
+namespace Gnosis.Core.Rss
+{
+    public class RssEnclosure
+        : IRssEnclosure
+    {
+        public RssEnclosure(Uri url, IMediaType type, int length)
+        {
+            this.url = url;
+            this.type = type;
+            this.length = length;
+        }
+
+        private readonly Uri url;
+        private readonly IMediaType type;
+        private readonly int length;
+
+        #region IRssEnclosure Members
+
+        public Uri Url
+        {
+            get { return url; }
+        }
+
+        public IMediaType Type
+        {
+            get { return type; }
+        }
+
+        public int Length
+        {
+            get { return length; }
+        }
+
+        #endregion
+    }
+}
