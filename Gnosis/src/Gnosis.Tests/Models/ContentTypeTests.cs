85e85a5882254def28caef0dd9e1ef1120279960
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-07-01 03:09:38 +0000

    Finished implementing ContentType.GetContentType(Uri location) Created ContentTypeTests Changed the implementation of the ToMediaType Uri extension method to use GetContentType

---
+++ Gnosis/src/Gnosis.Tests/Models/ContentTypeTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using Gnosis.Core;
+using Gnosis.Core.W3c;
+
+namespace Gnosis.Tests.Models
+{
+    [TestFixture]
+    public class ContentTypeTests
+    {
+        [Test]
+        public void GetContentTypeForRssFeedWithInvalidContentType()
+        {
+            var location = new Uri("http://feeds.arstechnica.com/arstechnica/index");
+            var contentType = location.ToContentType();
+            Assert.IsNotNull(contentType);
+            Assert.AreNotEqual(ContentType.Empty, contentType);
+            Assert.AreEqual(MediaType.RssFeed, contentType.Type);
+            Assert.AreEqual(CharacterSet.Utf8, contentType.CharSet);
+            Assert.IsNull(contentType.Boundary);
+        }
+    }
+}
