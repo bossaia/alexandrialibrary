01801eac411e5f2100c935b9d0a575a4c539d8c4
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2011-07-05 23:04:10 +0000

    Implemented PicsRating More work implementing RssFeedFactory for RSS 2.0 specification More work on RssTests to test RssFeedFactory

---
+++ trunk/Gnosis/src/Gnosis.Core/W3c/PicsRating.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core.W3c
+{
+    public class PicsRating
+        : IPicsRating
+    {
+        public PicsRating(string value)
+        {
+            this.value = value;
+        }
+
+        private readonly string value;
+
+        #region IPicsRating Members
+
+        public string Value
+        {
+            get { return value; }
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return value;
+        }
+    }
+}
