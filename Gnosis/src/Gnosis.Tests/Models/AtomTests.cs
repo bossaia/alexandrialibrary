63200c79e4805b01fa11a99779d1a4fc2c685d7d
dan.poage <dan.poage@gmail.com>
2011-07-08 22:54:55 +0000

    Added a test of a remote Atom feed

--- Gnosis/src/Gnosis.Tests/Models/AtomTests.cs
+++ Gnosis/src/Gnosis.Tests/Models/AtomTests.cs
@@ -207,5 +207,39 @@ namespace Gnosis.Tests.Models
             Assert.AreEqual(entryExtension1Prefix, feed.Entries.First().Extensions.First().Prefix);
             Assert.AreEqual(entryExtension1Name, feed.Entries.First().Extensions.First().Name);
         }
+
+        [Test]
+        public void CreateAtomFeedFromRemoteLocation()
+        {
+            #region Constants
+
+            var location = new Uri("http://feeds2.feedburner.com/oreilly/radar/atom");
+            var feed = location.ToAtomFeed();
+            const string generatorName = "Movable Type Pro 4.21-en";
+            const string generatorUri = "http://www.sixapart.com/movabletype/";
+            
+            var entry1ContentLang = LanguageTag.Parse("en");
+            var entry1ContentBase = "http://radar.oreilly.com/";
+            var entry1ContentType = AtomTextType.Html;
+
+            #endregion
+
+            Assert.IsNotNull(feed);
+            Assert.IsNotNull(feed.Id);
+            Assert.IsNotNull(feed.Title);
+
+            Assert.IsNotNull(feed.Generator);
+            Assert.AreEqual(generatorName, feed.Generator.Name);
+            Assert.AreEqual(generatorUri, feed.Generator.Uri.ToString());
+
+            Assert.IsTrue(feed.Entries.Count() > 0);
+            Assert.IsTrue(feed.Entries.First().Categories.Count() > 0);
+            Assert.IsNotNull(feed.Entries.First().Content);
+            Assert.AreEqual(entry1ContentLang, feed.Entries.First().Content.Lang);
+            Assert.AreEqual(entry1ContentBase, feed.Entries.First().Content.BaseId.ToString());
+            Assert.AreEqual(entry1ContentType, feed.Entries.First().Content.Type);
+            Assert.IsTrue(feed.Links.Count() > 0);
+            Assert.IsTrue(feed.Extensions.Count() > 0);
+        }
     }
 }
