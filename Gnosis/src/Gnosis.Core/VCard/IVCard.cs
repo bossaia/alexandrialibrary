2689356287fc3a478416b061db3318ed489fdbab
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-07-09 07:07:34 +0000

    Added VCard namespace to Gnosis.Core Defined IVCard interface based on vCard standard

---
+++ Gnosis/src/Gnosis.Core/VCard/IVCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core.VCard
+{
+    /// <summary>
+    /// Defines a vCard based on IETF standard RFC 2426 
+    /// </summary>
+    public interface IVCard
+    {
+        string Version { get; }
+        string N { get; }
+        string FN { get; }
+        string Org { get; }
+        string Title { get; }
+        DateTime Rev { get; }
+    }
+}
