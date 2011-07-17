307f64e1e05723cec2498818ab4aeb1c5d6e7184
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-07-17 22:52:19 +0000

    Started work defining XML extensions for DublinCore

---
+++ trunk/Gnosis/src/Gnosis.Core/DublinCore/IDcTitle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Gnosis.Core.W3c;
+
+namespace Gnosis.Core.DublinCore
+{
+    public interface IDcTitle
+        : IXmlExtension
+    {
+        string Title { get; }
+    }
+}
