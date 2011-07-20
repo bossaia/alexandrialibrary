df39236ecbb8b173d892e9e66c7cbca9e52d3437
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-07-20 01:59:44 +0000

    More work defining DublinCore interfaces

---
+++ Gnosis/src/Gnosis.Core/DublinCore/IDcContributor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Gnosis.Core.W3c;
+
+namespace Gnosis.Core.DublinCore
+{
+    public interface IDcContributor
+        : IXmlExtension
+    {
+        string Content { get; }
+    }
+}
