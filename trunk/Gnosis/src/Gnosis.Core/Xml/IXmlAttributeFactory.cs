bfd7239f3941afc41773687c0fb85b00c3ad84c7
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-07-25 19:12:58 +0000

    Created XmlBaseAttribute as an example of an Attribute plug-in class Defined IXmlAttributeFactory and started work implementing plug-in support in XmlNodeExtensions

---
+++ trunk/Gnosis/src/Gnosis.Core/Xml/IXmlAttributeFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core.Xml
+{
+    public interface IXmlAttributeFactory
+    {
+        string AttributeName { get; }
+        bool IsValidFor(IXmlAttribute attribute);
+        IXmlAttribute Create(System.Xml.XmlAttribute attribute);
+    }
+}
