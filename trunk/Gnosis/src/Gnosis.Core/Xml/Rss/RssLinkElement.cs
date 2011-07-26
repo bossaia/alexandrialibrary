2a3159972a85b18c8fd23ac6b8620b0584bc8030
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2011-07-26 20:29:29 +0000

    Added Atom and Rss namespaces to Gnosis.Core.Xml Defined IRssLinkElement and implemented it Added more XmlTests

---
+++ trunk/Gnosis/src/Gnosis.Core/Xml/Rss/RssLinkElement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core.Xml.Rss
+{
+    public class RssLinkElement
+        : XmlElement
+    {
+        public RssLinkElement(IXmlNode parent, IEnumerable<IXmlNode> children, IXmlQualifiedName name, IEnumerable<IXmlAttribute> attributes)
+            : base(parent, children, name, attributes)
+        {
+        }
+
+        public Uri Content
+        {
+            get
+            {
+                var child = Children.FirstOrDefault() as IXmlEscapedSection;
+
+                return (child != null && child.Content != null) ? 
+                    child.Content.ToUri() 
+                    : null;
+            }
+        }
+    }
+}
