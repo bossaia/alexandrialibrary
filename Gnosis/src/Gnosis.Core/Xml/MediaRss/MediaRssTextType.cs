89ed132205f3d4106b72cdb890261ecf7ec59873
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-08-15 00:50:30 +0000

    Added MediaRss namespace elements

---
+++ Gnosis/src/Gnosis.Core/Xml/MediaRss/MediaRssTextType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core.Xml.MediaRss
+{
+    public enum MediaRssTextType
+    {
+        Unspecified = 0,
+        Html,
+        Plain
+    }
+}
