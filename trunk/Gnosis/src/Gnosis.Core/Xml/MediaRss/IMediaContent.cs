4e3687cb47cc127e4973be4bde945597a7f7f8e1
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-08-15 00:50:30 +0000

    Added MediaRss namespace elements

---
+++ trunk/Gnosis/src/Gnosis.Core/Xml/MediaRss/IMediaContent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Gnosis.Core.W3c;
+
+namespace Gnosis.Core.Xml.MediaRss
+{
+    public interface IMediaContent
+        : IPrimaryMediaRssElement
+    {
+        Uri Url { get; }
+        long FileSize { get; }
+        IMediaType Type { get; }
+        MediaRssMedium Medium { get; }
+        bool IsDefault { get; }
+        MediaRssExpression Expression { get; }
+        int BitRate { get; }
+        int SamplingRate { get; }
+        int Channels { get; }
+        int Duration { get; }
+        int Height { get; }
+        int Width { get; }
+        ILanguageTag Lang { get; }
+    }
+}
---
+++ trunk/Gnosis/src/Gnosis.Core/Xml/MediaRss/MediaTitle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core.Xml.MediaRss
+{
+    public class MediaTitle
+        : Element, IMediaTitle
+    {
+        public MediaTitle(INode parent, IQualifiedName name)
+            : base(parent, name)
+        {
+        }
+
+        public MediaRssTextType Type
+        {
+            get { return GetAttributeEnum<MediaRssTextType>("type", MediaRssTextType.Plain); }
+        }
+
+        public string Content
+        {
+            get { return GetContentString(); }
+        }
+    }
+}
