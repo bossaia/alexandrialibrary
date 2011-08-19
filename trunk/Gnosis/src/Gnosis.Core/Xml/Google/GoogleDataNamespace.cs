e53925438a44a869c49b2ac10156dd4359a69a64
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-08-19 04:34:09 +0000

    Added youtube_playlists.xml as test file Defined GoogleDataNamespace Defined IGoogleDataElement and started work defining Google Data element interfaces

---
+++ trunk/Gnosis/src/Gnosis.Core/Xml/Google/GoogleDataNamespace.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core.Xml.Google
+{
+    public class GoogleDataNamespace
+        : Namespace
+    {
+        public GoogleDataNamespace(INode parent)
+            : base(parent, name, identifier)
+        {
+        }
+
+        private static readonly IQualifiedName name = QualifiedName.Parse("xmlns:gd");
+        private static readonly Uri identifier = new Uri("http://schemas.google.com/g/2005");
+    }
+}
