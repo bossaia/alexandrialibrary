f8bf6b2962d47ad5c47789399fe7473d3048650e
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-08-24 03:57:26 +0000

    More work on Atom element implementation Created AtomNamespace and added constructor functions for newly-created Atom element classes

---
+++ Gnosis/src/Gnosis.Core/Xml/Atom/AtomPerson.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core.Xml.Atom
+{
+    public abstract class AtomPerson
+        : AtomCommon, IAtomPerson
+    {
+        protected AtomPerson(INode parent, IQualifiedName name)
+            : base(parent, name)
+        {
+        }
+
+        #region IAtomPerson Members
+
+        public string PersonName
+        {
+            get { return GetChildString("name"); }
+        }
+
+        public Uri Uri
+        {
+            get { return GetChildUri("uri"); }
+        }
+
+        public string Email
+        {
+            get { return GetChildString("email"); }
+        }
+
+        #endregion
+    }
+}
