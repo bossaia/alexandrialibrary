ae2dcdf13de9069cb26bd584e786ec03b82fa892
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-08-24 03:57:26 +0000

    More work on Atom element implementation Created AtomNamespace and added constructor functions for newly-created Atom element classes

---
+++ trunk/Gnosis/src/Gnosis.Core/Xml/Atom/AtomNamespace.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core.Xml.Atom
+{
+    public class AtomNamespace
+        : Namespace
+    {
+        public AtomNamespace()
+            : base("atom", new Uri("http://www.w3.org/2005/Atom"))
+        {
+            AddElementConstructor("author", (parent, name) => new AtomAuthor(parent, name));
+            AddElementConstructor("category", (parent, name) => new AtomCategory(parent, name));
+            AddElementConstructor("contributor", (parent, name) => new AtomContributor(parent, name));
+            AddElementConstructor("link", (parent, name) => new AtomLink(parent, name));
+        }
+    }
+}
