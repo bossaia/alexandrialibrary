a68ff1b3d05952b9fde0477b87dd36a6390d8aa7
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-08-28 23:06:03 +0000

    Defined interfaces related to IResourceLocation

---
+++ trunk/Gnosis/src/Gnosis.Core/IResourceQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core
+{
+    public interface IResourceQuery
+    {
+        IEnumerable<KeyValuePair<string, string>> Parameters { get; }
+    }
+}
