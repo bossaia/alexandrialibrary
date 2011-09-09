e7d44316b3ae7d29534f60451d004005f3b87482
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-09-09 05:05:42 +0000

    Defined ConnectionFactory classes in SQLite32 and SQLite64 projects Defined a new SQLite project with conditional compilation logic to use 32 or 64 bit connection factories based on a x64 compilation flag

---
+++ Gnosis/src/Gnosis.Data.SQLite/SQLiteRepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Data.SQLite
+{
+    public abstract class SQLiteRepositoryBase
+    {
+        protected IDbConnection GetConnection(string connectionString)
+        {
+            if (connectionString == null)
+                throw new ArgumentNullException("connectionString");
+
+#if x64
+            return Gnosis.Data.SQLite64.ConnectionFactory.Create(connectionString);
+#else
+            return Gnosis.Data.SQLite32.ConnectionFactory.Create(connectionString);
+#endif
+        }
+    }
+}
