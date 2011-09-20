6dafee8d27ce0ca86d5c0a92d74b134c526cf601
dan.poage <dan.poage@gmail.com>
2011-09-20 00:25:57 +0000

    Added references to needed Firebird libraries Created FirebirdConnectionFactory as a Firebird implementation of IConnectionFactory Added FirebirdTests to test new FirebirdConnectionFactory

---
+++ Gnosis/src/Gnosis.Data.Firebird/FirebirdConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+using FirebirdSql.Data.FirebirdClient;
+
+namespace Gnosis.Data.Firebird
+{
+    public class FirebirdConnectionFactory
+        : IConnectionFactory
+    {
+        public IDbConnection Create(string connectionString)
+        {
+            return new FbConnection(connectionString);
+        }
+    }
+}
---
+++ Gnosis/src/Gnosis.Tests/Data/FirebirdTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Gnosis.Data;
+using Gnosis.Data.Firebird;
+
+using NUnit.Framework;
+
+namespace Gnosis.Tests.Data
+{
+    [TestFixture]
+    public class FirebirdTests
+    {
+        private readonly IConnectionFactory factory = new FirebirdConnectionFactory();
+
+        [Test]
+        public void TestConnection()
+        {
+            try
+            {
+                using (var connection = factory.Create("Database=Alexandria.fbd;ServerType=1;User='';Password='';Charset=UTF8"))
+                {
+                    connection.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
