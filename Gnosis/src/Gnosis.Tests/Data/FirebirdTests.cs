15786dcbf065b98b422b69c2141c6be4819b5329
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-09-20 04:07:52 +0000

    More work on FirebirdTests

--- Gnosis/src/Gnosis.Tests/Data/FirebirdTests.cs
+++ Gnosis/src/Gnosis.Tests/Data/FirebirdTests.cs
@@ -14,17 +14,20 @@ namespace Gnosis.Tests.Data
     public class FirebirdTests
     {
         private readonly IConnectionFactory factory = new FirebirdConnectionFactory();
-        private const string connectionString = @"Database=.\Alexandria.fbd;ServerType=1;User='';Password='';Charset=UTF8";
+        private const string databaseFile = @".\ALEXANDRIA.FDB";
+        private const string connectionString = @"Database=.\ALEXANDRIA.FDB;ServerType=1;User='';Password='';Charset=UTF8";
 
         [Test]
         public void TestConnection()
         {
 
-            try
+            var file = new System.IO.FileInfo(databaseFile);
+            if (!file.Exists)
             {
+                System.Diagnostics.Debug.WriteLine("Creating Firebird database: " + file.FullName);
                 factory.CreateDatabase(connectionString);
             }
-            catch (Exception)
+            else
             {
                 System.Diagnostics.Debug.WriteLine("Database already exists");
             }
@@ -34,6 +37,33 @@ namespace Gnosis.Tests.Data
                 using (var connection = factory.Create(connectionString))
                 {
                     connection.Open();
+
+                    var command = connection.CreateCommand();
+                    command.CommandText =
+                        @"EXECUTE BLOCK AS BEGIN
+if (not exists(select 1 from rdb$relations where rdb$relation_name = 'FOO')) then 
+execute statement 'create table Foo (Id integer not null primary key, Name varchar(100) not null unique, Score decimal, Born date);';
+END";
+                    command.ExecuteNonQuery();
+
+                    var query = connection.CreateCommand();
+                    query.CommandText = "select count(*) from Foo;";
+                    var result = query.ExecuteScalar();
+                    Assert.IsNotNull(result);
+                    System.Diagnostics.Debug.WriteLine(result.ToString());
+
+                    if (Convert.ToInt32(result.ToString()) == 0)
+                    {
+                        System.Diagnostics.Debug.WriteLine("table is empty");
+
+                        var insert = connection.CreateCommand();
+                        insert.CommandText = "insert into Foo (Id, Name, Score) values (1, 'Bob Smith', 3.7);";
+                        insert.ExecuteNonQuery();
+                    }
+
+                    var result2 = query.ExecuteScalar();
+                    Assert.IsNotNull(result2);
+                    Assert.AreEqual(1, Convert.ToInt32(result2.ToString()));
                 }
             }
             catch (Exception ex)
