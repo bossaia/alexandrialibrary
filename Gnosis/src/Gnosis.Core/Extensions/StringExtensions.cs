7f0b8f72a1de3877cb3e3196fc1cbddf8dcbbd57
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-09-25 07:50:05 +0000

    Implemented ToLevenshteinDistance string extension method Added ExtensionTests with a test of ToLevenshteinDistance extension method

--- Gnosis/src/Gnosis.Core/Extensions/StringExtensions.cs
+++ Gnosis/src/Gnosis.Core/Extensions/StringExtensions.cs
@@ -87,6 +87,53 @@ namespace Gnosis.Core
 
         #endregion
 
+        public static int ToLevenshteinDistance(this string self, string value)
+        {
+            if (self == null)
+                throw new ArgumentNullException("self");
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            int n = self.Length;
+            int m = value.Length;
+
+            if (n == 0)
+                return m;
+
+            if (m == 0)
+                return n;
+
+            int[,] d = new int[n + 1, m + 1];
+
+            // Step 2
+            for (int i = 0; i <= n; d[i, 0] = i++)
+            {
+            }
+
+            for (int j = 0; j <= m; d[0, j] = j++)
+            {
+            }
+
+            // Step 3
+            for (int i = 1; i <= n; i++)
+            {
+                //Step 4
+                for (int j = 1; j <= m; j++)
+                {
+                    // Step 5
+                    int cost = (value[j - 1] == self[i - 1]) ? 0 : 1;
+
+                    // Step 6
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+            // Step 7
+            return d[n, m];
+        }
+
+
         public static string ToDoubleMetaphoneString(this string self)
         {
 			MetaphoneData metaphoneData = new MetaphoneData();
---
+++ Gnosis/src/Gnosis.Tests/Core/ExtensionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Gnosis.Core;
+
+using NUnit.Framework;
+
+namespace Gnosis.Tests.Core
+{
+    [TestFixture]
+    public class ExtensionTests
+    {
+        [Test]
+        public void StringToLevenshteinDistanceTest()
+        {
+            Assert.AreEqual(3, "kitten".ToLevenshteinDistance("sitting"));
+            Assert.AreEqual(2, "CHROMEO".ToLevenshteinDistance("ROMEO"));
+            Assert.AreEqual(5, "adz".ToLevenshteinDistance("hazard"));
+        }
+    }
+}
