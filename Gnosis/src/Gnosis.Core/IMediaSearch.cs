5fdf23dc65f6f7f9c831d495c4d33bbe6d8f495a
dan.poage <dan.poage@gmail.com>
2011-09-30 23:45:08 +0000

    Simplified IMediaRepository Defined IMediaSearch with Execute and ExecuteAsync methods that take an IMediaRequest

---
+++ Gnosis/src/Gnosis.Core/IMediaSearch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core
+{
+    public interface IMediaSearch
+    {
+        IEnumerable<IMedia> Execute(IMediaRequest request);
+        void ExecuteAsync(IMediaRequest request);
+    }
+}
