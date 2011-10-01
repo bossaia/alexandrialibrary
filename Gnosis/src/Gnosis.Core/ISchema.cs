f44daab35ec6114dbc2a4b87c4b20653f6405ab0
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-10-01 21:47:46 +0000

    Defined ISchema, ISchemaFactory and ISchemaRepository interfaces

---
+++ Gnosis/src/Gnosis.Core/ISchema.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core
+{
+    public interface ISchema
+    {
+        long LocalId { get; }
+        Uri Identifier { get; }
+        string Name { get; }
+    }
+}
---
+++ Gnosis/src/Gnosis.Core/ISchemaRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core
+{
+    public interface ISchemaRepository
+    {
+        ISchema Lookup(long localId);
+        ISchema Lookup(Uri identifier);
+
+        void Delete(ISchema schema);
+        void Save(ISchema schema);
+    }
+}
