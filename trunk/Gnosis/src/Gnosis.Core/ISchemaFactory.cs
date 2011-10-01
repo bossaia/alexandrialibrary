b0e8f594e52e89c9988e8c45d883b909bcdc3128
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-10-01 21:47:46 +0000

    Defined ISchema, ISchemaFactory and ISchemaRepository interfaces

---
+++ trunk/Gnosis/src/Gnosis.Core/ISchemaFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core
+{
+    public interface ISchemaFactory
+    {
+        ISchema Create(Uri identifier, string name);
+        ISchema Create(Uri identifier, string name, long localId);
+    }
+}
