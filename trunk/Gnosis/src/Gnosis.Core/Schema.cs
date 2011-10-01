d2cebb7308f32d428681b2cffc61a3e55f0f5a82
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-10-01 23:08:51 +0000

    Implemented Schema and SchemaRepository

---
+++ trunk/Gnosis/src/Gnosis.Core/Schema.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core
+{
+    public class Schema
+        : ISchema
+    {
+        protected Schema(Uri identifier, string name)
+            : this(identifier, name, null)
+        {
+        }
+
+        protected Schema(Uri identifier, string name, ISchema parent)
+        {
+            if (identifier == null)
+                throw new ArgumentNullException("identifier");
+            if (name == null)
+                throw new ArgumentNullException("name");
+
+            this.identifier = identifier;
+            this.name = name;
+            this.parent = parent;
+        }
+
+        private readonly Uri identifier;
+        private readonly string name;
+        private readonly ISchema parent;
+        private readonly IDictionary<string, ISchema> children = new Dictionary<string, ISchema>();
+
+        protected void AddChild(ISchema child)
+        {
+            if (child == null)
+                throw new ArgumentNullException("child");
+
+            children.Add(child.Name, child);
+        }
+
+        #region ISchema Members
+
+        public Uri Identifier
+        {
+            get { return identifier; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public ISchema Parent
+        {
+            get { return parent; }
+        }
+
+        public IEnumerable<ISchema> Children
+        {
+            get { return children.Values; }
+        }
+
+        public ISchema GetChild(string name)
+        {
+            if (name == null)
+                throw new ArgumentNullException("name");
+
+            return children.ContainsKey(name) ?
+                children[name]
+                : null;
+        }
+
+        #endregion
+
+        public static readonly ISchema Default = new Schema(new Uri("http://gn0s1s.com/ns/1/schemas/default"), "Default");
+    }
+}
---
+++ trunk/Gnosis/src/Gnosis.Core/SchemaRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Core
+{
+    public class SchemaRepository
+        : ISchemaRepository
+    {
+        private IDictionary<string, ISchema> byIdentifier = new Dictionary<string, ISchema>();
+
+        #region ISchemaRepository Members
+
+        public ISchema Get(Uri identifier)
+        {
+            if (identifier == null)
+                throw new ArgumentNullException("identifier");
+
+            var key = identifier.ToString();
+
+            return byIdentifier.ContainsKey(key) ?
+                byIdentifier[key]
+                : Schema.Default;
+        }
+
+        public void Add(ISchema schema)
+        {
+            if (schema == null)
+                throw new ArgumentNullException("schema");
+
+            var key = schema.Identifier.ToString();
+            if (!byIdentifier.ContainsKey(key))
+                byIdentifier.Add(key, schema);
+
+            foreach (var child in schema.Children)
+                Add(child);
+        }
+
+        public void Remove(ISchema schema)
+        {
+            if (schema == null)
+                throw new ArgumentNullException("schema");
+
+            foreach (var child in schema.Children)
+                Remove(child);
+
+            var key = schema.Identifier.ToString();
+            if (byIdentifier.ContainsKey(key))
+                byIdentifier.Remove(key);
+        }
+
+        #endregion
+    }
+}
