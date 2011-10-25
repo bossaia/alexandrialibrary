ceea4a692f907a29149fd77048e96958ad1e2e7f
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2011-10-25 01:33:57 +0000

    Moved IAudio, IDocument and IImage to Core namespace Defined IVideo interface Replaced ToId3v1Genre in TagTuple with a generic ToEnum method

--- trunk/Gnosis/src/Gnosis.Core/IVideo.cs
+++ trunk/Gnosis/src/Gnosis.Core/IVideo.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Gnosis.Core.Image
+namespace Gnosis.Core
 {
-    public interface IImage
+    public interface IVideo
         : IMedia
     {
-        //byte[] GetData();
     }
 }
