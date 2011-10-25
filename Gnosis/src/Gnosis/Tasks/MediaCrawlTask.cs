9886c19fd7419271f26b7323108773813f62ec3c
dan.poage <dan.poage@gmail.com>
2011-10-25 23:29:29 +0000

    Added depth parameter to ISpider.Crawl method Started work implementing LocalFilesystemSpider and MediaCrawlTask

---
+++ Gnosis/src/Gnosis/Tasks/MediaCrawlTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Tasks
+{
+    public class MediaCrawlTask
+        : TaskBase<IEnumerable<IMedia>>
+    {
+        public MediaCrawlTask(ILogger logger, ISpider spider)
+            : base(logger)
+        {
+        }
+
+        protected override void DoWork()
+        {
+        }
+    }
+}
