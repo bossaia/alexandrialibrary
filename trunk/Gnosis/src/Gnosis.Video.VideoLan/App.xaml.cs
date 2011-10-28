6cb06786b62c528cbd47d03a77b57a72dae85d8a
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2011-10-28 05:16:43 +0000

    Added Gnosis.Video.VideoLan project and removed broken Gnosis.Video.Vlc project

---
+++ trunk/Gnosis/src/Gnosis.Video.VideoLan/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Gnosis.Video.VideoLan
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
