8c4656c06a2ae31bf3be6a714953f99312ed8a84
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2011-10-28 01:01:25 +0000

    Started work on Gnosis.Video.Vlc to add video support to Alexandria

---
+++ trunk/Gnosis/src/Gnosis.Alexandria/Views/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gnosis.Alexandria.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            //videoPlayer.ShutDown();
+        }
+
+        private void launchVideoPlayerButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var videoPlayer = new Gnosis.Video.Vlc.VideoPlayer();
+                videoPlayer.Show();
+            }
+            catch (Exception ex)
+            {
+                var m = ex.Message;
+            }
+        }
+    }
+}
