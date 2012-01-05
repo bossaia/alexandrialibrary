68bbdb2236d0c42ae43b06d844452a3ea6c75888
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2012-01-05 23:22:02 +0000

    Defined IPlaylistController interface Added vlc.exe to Gnosis.Video.Vlc project Created ThumbnailGenerator class in Gnosis.Video.Vlc Added temp directory to Gnosis.Alexandria with a readme.txt file placeholder

---
+++ Gnosis/src/Gnosis.Video.Vlc/ThumbnailGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Video.Vlc
+{
+    public class ThumbnailGenerator
+    {
+        public void GenerateThumbails(Uri outputPath, Uri videoPath)
+        {
+            if (outputPath == null)
+                throw new ArgumentNullException("outputPath");
+            if (videoPath == null)
+                throw new ArgumentNullException("videoPath");
+
+            const string fileName = "vlc";
+            const string format = "--video-filter scene -V dummy --aout=dummy --scene-ratio=24 --scene-format=jpg --start-time=6 --run-time=2 --scene-prefix=thumb --scene-path=\"{0}\" \"{1}\" vlc://quit";
+            var arguments = string.Format(format, outputPath.LocalPath, videoPath.LocalPath);
+
+            using (var process = new Process())
+            {
+                process.StartInfo = new ProcessStartInfo(fileName, arguments);
+                process.Start();
+            }
+        }
+    }
+}
