9eb60621f9a4a6c59bc9ed896eebd48e5c7e96b1
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2012-01-29 21:18:37 +0000

    Started work on Gnosis.Identification namespace

---
+++ trunk/Gnosis/src/Gnosis/Identification/MediaInfo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Identification
+{
+    public class MediaInfo
+        : IMediaInfo
+    {
+        public MediaInfo(Uri location)
+            : this(location, null, null, null, null, null, null)
+        {
+        }
+
+        public MediaInfo(Uri location, string fileExtension, IContentType responseContentType, ICharacterSet bomCharacterSet, byte[] contentMagicNumber, IMediaType contentMediaType, ICharacterSet contentCharacterSet)
+        {
+            this.location = location;
+            this.fileExtension = fileExtension;
+            this.responseContentType = responseContentType;
+            this.bomCharacterSet = bomCharacterSet;
+            this.contentMagicNumber = contentMagicNumber;
+            this.contentMediaType = contentMediaType;
+            this.contentCharacterSet = contentCharacterSet;
+        }
+
+        private Uri location;
+        private string fileExtension;
+        private IContentType responseContentType;
+        private ICharacterSet bomCharacterSet;
+        private byte[] contentMagicNumber;
+        private IMediaType contentMediaType;
+        private ICharacterSet contentCharacterSet;
+
+        public Uri Location
+        {
+            get { return location; }
+        }
+
+        public string FileExtension
+        {
+            get { return fileExtension; }
+        }
+
+        public IContentType ResponseContentType
+        {
+            get { return responseContentType; }
+        }
+
+        public ICharacterSet BomCharacterSet
+        {
+            get { return bomCharacterSet; }
+        }
+
+        public byte[] ContentMagicNumber
+        {
+            get { return contentMagicNumber; }
+        }
+
+        public IMediaType ContentMediaType
+        {
+            get { return contentMediaType; }
+        }
+
+        public ICharacterSet ContentCharacterSet
+        {
+            get { return contentCharacterSet; }
+        }
+
+        public IContentType ToContentType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trunk/Gnosis/src/Gnosis/Identification/MediaIsAvailable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Identification
+{
+    public class MediaIsAvailable
+        : MediaHasLocation
+    {
+        protected override IMediaInfo DoExamine(IMediaInfo info)
+        {
+            if (info.Location.IsFile)
+            {
+                if (System.IO.Directory.Exists(info.Location.LocalPath))
+                {
+                }
+                else if (!System.IO.File.Exists(info.Location.LocalPath))
+                {
+                }
+                else
+                    return info;
+            }
+            else
+            {
+            }
+
+            return info;
+        }
+    }
+}
