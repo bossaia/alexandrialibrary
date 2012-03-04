d8b790f8b2fd4b6136f6347b59bb1b4a39b1bbc0
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2012-03-04 07:07:38 +0000

    Finished basic implementation of TrackRepository and added test in Program

--- trunk/Utils/Tests/GnosisTests/Program.cs
+++ trunk/Utils/Tests/GnosisTests/Program.cs
@@ -11,66 +11,81 @@ namespace GnosisTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Gnosis Tests");
+            try
+            {
+                Console.WriteLine("Gnosis Tests");
 
-            //const int TOTAL = 500000; //1000000;
-            //1 Million Tracks = 200 MB
+                //const int TOTAL = 500000; //1000000;
+                //1 Million Tracks = 200 MB
 
-            var start = DateTime.Now;
-            var artistRepo = new Repository<Artist>(new ArtistSerializer());
-            var albumRepo = new Repository<Album>(new AlbumSerializer());
-            var trackRepo = new Repository<Track>(new TrackSerializer());
+                var start = DateTime.Now;
+                var trackDatabaseRepo = new TrackRepository();
+                trackDatabaseRepo.Initialize();
 
-            artistRepo.Initialize();
-            albumRepo.Initialize();
-            trackRepo.Initialize();
+                //var artistRepo = new Repository<Artist>(new ArtistSerializer());
+                //var albumRepo = new Repository<Album>(new AlbumSerializer());
+                //var trackRepo = new Repository<Track>(new TrackSerializer());
 
-            //trackRepo.Compact();
-            //trackRepo.Update(trackRepo.Entities.Where(x => x.Name.StartsWith("a")), "Album", "101");
-            //trackRepo.Update(trackRepo.Entities.Where(x => x.Name.StartsWith("1")), "Artist", "202");
-            //trackRepo.Delete(trackRepo.Entities.Where(x => x.Name.StartsWith("4")).Select(x => x.Id).ToList());
+                //artistRepo.Initialize();
+                //albumRepo.Initialize();
+                //trackRepo.Initialize();
 
-            //for (uint i = 1; i <= TOTAL; i++)
-            //{
-            //    var track = new Track() { Id = i, Album = 100, Artist = 200, Disc = 1, Number = 2, Duration = 300, Name = Guid.NewGuid().ToString().Replace('-', ' ') };
-            //    trackRepo.Create(track);
-            //}
+                //trackRepo.Compact();
+                //trackRepo.Update(trackRepo.Entities.Where(x => x.Name.StartsWith("a")), "Album", "101");
+                //trackRepo.Update(trackRepo.Entities.Where(x => x.Name.StartsWith("1")), "Artist", "202");
+                //trackRepo.Delete(trackRepo.Entities.Where(x => x.Name.StartsWith("4")).Select(x => x.Id).ToList());
 
-            //for (uint i = 1; i < TOTAL; i++)
-            //{
-            //    var artist = new Artist() { Id = i, Name = Guid.NewGuid().ToString().Replace('-', ' ') };
+                //for (uint i = 1; i <= TOTAL; i++)
+                //{
+                //    var track = new Track() { Album = 100, Artist = 200, Disc = 1, Number = 2, Duration = 300, Name = Guid.NewGuid().ToString().Replace('-', ' ') };
+                //    trackDatabaseRepo.Create(new List<Track> { track });
+                //    //trackRepo.Create(track);
+                //}
 
-            //}
+                var tracks = trackDatabaseRepo.GetAll();
 
-            Console.WriteLine("  Elapsed: {0} (s)", DateTime.Now.Subtract(start).TotalSeconds);
+                //for (uint i = 1; i < TOTAL; i++)
+                //{
+                //    var artist = new Artist() { Id = i, Name = Guid.NewGuid().ToString().Replace('-', ' ') };
 
-            Console.WriteLine("  Count  : {0}", trackRepo.Entities.Count());
-            Console.WriteLine("  Count where Name like 4%: {0}", trackRepo.Entities.Where(x => x.Name.StartsWith("4")).Count());
-            Console.WriteLine("  Count where Name like 1% and Artist = 202: {0}", trackRepo.Entities.Where(x => x.Name.StartsWith("1") && x.Artist == 202).Count());
+                //}
 
-            //var trackByIdStart = DateTime.Now;
-            //var trackByIdResult = trackById[1001];
-            //if (trackByIdResult != null)
-            //    Console.WriteLine("  By Id Elapsed: {0} (ms) [< 150 is ideal]", DateTime.Now.Subtract(trackByIdStart).TotalMilliseconds);
-            //else
-            //    Console.WriteLine("  Could not lookup track by id!");
+                Console.WriteLine("  Elapsed: {0} (s)", DateTime.Now.Subtract(start).TotalSeconds);
 
-            //var trackByNameFastStart = DateTime.Now;
-            //var trackByNameFastResult = trackByName[nameToSearch];
-            //if (trackByNameFastResult != null)
-            //    Console.WriteLine("  By Name (Fast) Elapsed: {0} (ms) [<150 is ideal]", DateTime.Now.Subtract(trackByNameFastStart).TotalMilliseconds);
-            //else
-            //    Console.WriteLine("  Could not lookup track name (fast)!");
+                Console.WriteLine("  Count  : {0}", tracks.Count());
+                //Console.WriteLine("  Count where Name like 4%: {0}", trackRepo.Entities.Where(x => x.Name.StartsWith("4")).Count());
+                //Console.WriteLine("  Count where Name like 1% and Artist = 202: {0}", trackRepo.Entities.Where(x => x.Name.StartsWith("1") && x.Artist == 202).Count());
 
-            //var trackByNameSlowStart = DateTime.Now;
-            //var trackByNameSlowResult = trackById.Values.Where(x => x.Name == nameToSearch).FirstOrDefault();
-            //if (trackByNameSlowResult != null)
-            //    Console.WriteLine("  By Name (Slow) Elapsed: {0} (ms) [<150 is ideal]", DateTime.Now.Subtract(trackByNameSlowStart).TotalMilliseconds);
-            //else
-            //    Console.WriteLine("  Could not lookup track name (slow)!");
+                //var trackByIdStart = DateTime.Now;
+                //var trackByIdResult = trackById[1001];
+                //if (trackByIdResult != null)
+                //    Console.WriteLine("  By Id Elapsed: {0} (ms) [< 150 is ideal]", DateTime.Now.Subtract(trackByIdStart).TotalMilliseconds);
+                //else
+                //    Console.WriteLine("  Could not lookup track by id!");
 
-            Console.WriteLine("Press ENTER to close");
-            Console.ReadLine();
+                //var trackByNameFastStart = DateTime.Now;
+                //var trackByNameFastResult = trackByName[nameToSearch];
+                //if (trackByNameFastResult != null)
+                //    Console.WriteLine("  By Name (Fast) Elapsed: {0} (ms) [<150 is ideal]", DateTime.Now.Subtract(trackByNameFastStart).TotalMilliseconds);
+                //else
+                //    Console.WriteLine("  Could not lookup track name (fast)!");
+
+                //var trackByNameSlowStart = DateTime.Now;
+                //var trackByNameSlowResult = trackById.Values.Where(x => x.Name == nameToSearch).FirstOrDefault();
+                //if (trackByNameSlowResult != null)
+                //    Console.WriteLine("  By Name (Slow) Elapsed: {0} (ms) [<150 is ideal]", DateTime.Now.Subtract(trackByNameSlowStart).TotalMilliseconds);
+                //else
+                //    Console.WriteLine("  Could not lookup track name (slow)!");
+
+                Console.WriteLine("Press ENTER to close");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR");
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
--- trunk/Utils/Tests/GnosisTests/TrackRepository.cs
+++ trunk/Utils/Tests/GnosisTests/TrackRepository.cs
@@ -5,6 +5,8 @@ using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 
+using GnosisTests.Entities;
+
 namespace GnosisTests
 {
     class TrackRepository
@@ -12,6 +14,76 @@ namespace GnosisTests
         private IDbConnection GetConnection()
         {
             return new SQLiteConnection("Data Source=Track.db");
+        }
+
+        public void Initialize()
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "create table if not exists Track (Id integer primary key, Name text not null, Album integer not null, Artist integer not null, Disc integer not null, Number integer not null, Duration integer not null);";
+                    command.CommandType = CommandType.Text;
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Create(IEnumerable<Track> tracks)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                foreach (var track in tracks)
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "insert into Track (Name, Album, Artist, Disc, Number, Duration) values (@Name, @Album, @Artist, @Disc, @Number, @Duration);";
+                        command.Parameters.Add(new SQLiteParameter("@Name", track.Name));
+                        command.Parameters.Add(new SQLiteParameter("@Album", track.Album));
+                        command.Parameters.Add(new SQLiteParameter("@Artist", track.Artist));
+                        command.Parameters.Add(new SQLiteParameter("@Disc", track.Disc));
+                        command.Parameters.Add(new SQLiteParameter("@Number", track.Number));
+                        command.Parameters.Add(new SQLiteParameter("@Duration", track.Duration));
+                        command.CommandType = CommandType.Text;
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<Track> GetAll()
+        {
+            var tracks = new List<Track>();
+
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "select Id, Name, Album, Artist, Disc, Number, Duration from Track;";
+                    command.CommandType = CommandType.Text;
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var track = new Track();
+                            track.Id = uint.Parse(reader.GetValue(0).ToString());
+                            track.Name = reader.GetString(1);
+                            track.Album = uint.Parse(reader.GetValue(2).ToString());
+                            track.Artist = uint.Parse(reader.GetValue(3).ToString());
+                            track.Disc = byte.Parse(reader.GetValue(4).ToString());
+                            track.Number = byte.Parse(reader.GetValue(5).ToString());
+                            track.Duration = ushort.Parse(reader.GetValue(6).ToString());
+
+                            tracks.Add(track);
+                        }
+                    }
+                }
+            }
+
+            return tracks;
         }
     }
 }
