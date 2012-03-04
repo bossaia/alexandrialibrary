50ff6d0b2d39035a6832ab9290ba7dcd748c74d6
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2012-03-04 04:59:20 +0000

    Added SQLite dependencies to GnosisTests for testing with a database-backed repository Started work implmenting TrackRepository using SQLite

---
+++ trunk/Utils/Tests/GnosisTests/TrackRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+
+namespace GnosisTests
+{
+    class TrackRepository
+    {
+        private IDbConnection GetConnection()
+        {
+            return new SQLiteConnection("Data Source=Track.db");
+        }
+    }
+}
