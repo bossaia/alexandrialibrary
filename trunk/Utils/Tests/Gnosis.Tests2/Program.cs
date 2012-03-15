d051b14d24cc3ee79325e0c8c517cf369104da9e
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2012-03-15 06:02:36 +0000

    Added logic to Repository.Initialize to read from the database and cache Artists and Works Added test of Repository.Initialize to Program

--- trunk/Utils/Tests/Gnosis.Tests2/Program.cs
+++ trunk/Utils/Tests/Gnosis.Tests2/Program.cs
@@ -17,23 +17,26 @@ namespace Gnosis.Tests2
                 var repository = new Repository(cache);
                 repository.Initialize();
 
-                var artist = new Artist { Type = ArtistType.Group, Name = "Tool", Year = 1991 };
-                var album = new Work { Type = WorkType.Album, Artist = artist, Name = "Aenima", Year = 1996 };
-                var track1 = new Work { Type = WorkType.Track, Artist = artist, Parent = album, Name = "Stinkfist", Number = 1 };
-                var track2 = new Work { Type = WorkType.Track, Artist = artist, Parent = album, Name = "Eulogy", Number = 2 };
-                var track3 = new Work { Type = WorkType.Track, Artist = artist, Parent = album, Name = "H.", Number = 3 };
-                artist.AddWork(album);
-                album.AddChild(track1);
-                album.AddChild(track2);
-                album.AddChild(track3);
+                //var artist = new Artist { Type = ArtistType.Group, Name = "Tool", Year = 1991 };
+                //var album = new Work { Type = WorkType.Album, Artist = artist, Name = "Aenima", Year = 1996 };
+                //var track1 = new Work { Type = WorkType.Track, Artist = artist, Parent = album, Name = "Stinkfist", Number = 1 };
+                //var track2 = new Work { Type = WorkType.Track, Artist = artist, Parent = album, Name = "Eulogy", Number = 2 };
+                //var track3 = new Work { Type = WorkType.Track, Artist = artist, Parent = album, Name = "H.", Number = 3 };
+                //artist.AddWork(album);
+                //album.AddChild(track1);
+                //album.AddChild(track2);
+                //album.AddChild(track3);
 
-                repository.Save(new List<Entity> { artist });
+                //repository.Save(new List<Entity> { artist });
 
-                Console.WriteLine("Artist Id: {0}", cache.GetId(artist));
-                Console.WriteLine("Album Id: {0}", cache.GetId(album));
-                Console.WriteLine("Track #1 Id: {0}", cache.GetId(track1));
-                Console.WriteLine("Track #2 Id: {0}", cache.GetId(track2));
-                Console.WriteLine("Track #3 Id: {0}", cache.GetId(track3));
+                //Console.WriteLine("Artist Id: {0}", cache.GetId(artist));
+                //Console.WriteLine("Album Id: {0}", cache.GetId(album));
+                //Console.WriteLine("Track #1 Id: {0}", cache.GetId(track1));
+                //Console.WriteLine("Track #2 Id: {0}", cache.GetId(track2));
+                //Console.WriteLine("Track #3 Id: {0}", cache.GetId(track3));
+
+                Console.WriteLine("Artist Count: {0}", cache.Artists.Count());
+                Console.WriteLine("Work Count: {0}", cache.Works.Count());
             }
             catch (Exception ex)
             {
