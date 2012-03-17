f2412c90ae491151dfbc222881a828ec889418ec
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2012-03-17 20:43:54 +0000

    Renamed IDatabase to IEntityStore Defined IChange and updated IEntityStore to use it

---
+++ trunk/Utils/Tests/Gnosis.Tests2/IEntityStore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Gnosis.Tests2
+{
+    public interface IEntityStore<T>
+        where T : Entity
+    {
+        void Initialize(Action entityAction, Action linkAction, Action tagAction);
+        
+        IChange DeleteEntity(uint id);
+        IChange DeleteLink(uint id);
+        IChange DeleteTag(uint id);
+
+        IChange SaveEntity(uint id, T entity);
+        IChange SaveLink(uint id, Link link, uint entityId);
+        IChange SaveTag(uint id, Tag tag, uint entityId);
+
+        IBatch<T> CreateBatch();
+    }
+}
