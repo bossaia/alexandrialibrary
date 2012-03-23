2bacac43ce4922eabe10eeb086a56b432f351ac5
dan.poage <dan.poage@gmail.com>
2012-03-23 22:17:11 +0000

    More work on Tagger implementation Added values to Category and Source enums

--- Alexandria3/src/Gnosis.Tagging.TagLib/Tagger.cs
+++ Alexandria3/src/Gnosis.Tagging.TagLib/Tagger.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 
 using Gnosis.Data;
+using GTag=Gnosis.Data.Tag;
 
 namespace Gnosis.Tagging.TagLib
 {
@@ -20,49 +21,69 @@ namespace Gnosis.Tagging.TagLib
             var source = Source.Embedded;
             if ((tag.TagTypes & TagTypes.Id3v2) == TagTypes.Id3v2)
             {
-                source = Source.Embedded_Id3v2;
+                source = Source.Id3v2;
             }
             else if ((tag.TagTypes & TagTypes.Id3v1) == TagTypes.Id3v1)
             {
-                source = Source.Embedded_Id3v1;
+                source = Source.Id3v1;
+            }
+            else if ((tag.TagTypes & TagTypes.RiffInfo) == TagTypes.RiffInfo)
+            {
+                source = Source.Riff;
             }
 
             switch (category)
             {
                 case Category.Album:
-                    return tag.Album != null ? new Gnosis.Data.Tag(tag.Album, category, source) : null;
+                    return tag.Album != null ? new GTag(tag.Album, category, source) : null;
                 case Category.AlbumArtist:
-                    return tag.JoinedAlbumArtists != null ? new Gnosis.Data.Tag(tag.JoinedAlbumArtists, category, source) : null;
+                    return tag.JoinedAlbumArtists != null ? new GTag(tag.JoinedAlbumArtists, category, source) : null;
                 case Category.Artist:
-                    return tag.JoinedPerformers != null ? new Gnosis.Data.Tag(tag.JoinedPerformers, category, source) : null;
+                    return tag.JoinedPerformers != null ? new GTag(tag.JoinedPerformers, category, source) : null;
                 case Category.BeatsPerMinute:
-                    return tag.BeatsPerMinute > 0 ? new Gnosis.Data.Tag(tag.BeatsPerMinute.ToString(), category, source) : null;
+                    return tag.BeatsPerMinute > 0 ? new GTag(tag.BeatsPerMinute.ToString(), category, source) : null;
                 case Category.Comment:
-                    return tag.Comment != null ? new Gnosis.Data.Tag(tag.Comment, category, source) : null;
+                    return tag.Comment != null ? new GTag(tag.Comment, category, source) : null;
                 case Category.Composer:
-                    return tag.JoinedComposers != null ? new Gnosis.Data.Tag(tag.JoinedComposers, category, source) : null;
+                    return tag.JoinedComposers != null ? new GTag(tag.JoinedComposers, category, source) : null;
                 case Category.Conductor:
-                    return tag.Conductor != null ? new Gnosis.Data.Tag(tag.Conductor, category, source) : null;
+                    return tag.Conductor != null ? new GTag(tag.Conductor, category, source) : null;
                 case Category.Copyright:
-                    return tag.Copyright != null ? new Gnosis.Data.Tag(tag.Copyright, category, source) : null;
+                    return tag.Copyright != null ? new GTag(tag.Copyright, category, source) : null;
                 case Category.Disc:
-                    return tag.Disc > 0 ? new Gnosis.Data.Tag(tag.Disc.ToString(), category, source) : null;
+                    return tag.Disc > 0 ? new GTag(tag.Disc.ToString(), category, source) : null;
                 case Category.DiscCount:
-                    return tag.DiscCount > 0 ? new Gnosis.Data.Tag(tag.DiscCount.ToString(), category, source) : null;
+                    return tag.DiscCount > 0 ? new GTag(tag.DiscCount.ToString(), category, source) : null;
                 case Category.Genre:
-                    return tag.JoinedGenres != null ? new Gnosis.Data.Tag(tag.JoinedGenres, category, source) : null;
+                    return tag.JoinedGenres != null ? new GTag(tag.JoinedGenres, category, source) : null;
                 case Category.Grouping:
-                    return tag.Grouping != null ? new Gnosis.Data.Tag(tag.Grouping, category, source) : null;
+                    return tag.Grouping != null ? new GTag(tag.Grouping, category, source) : null;
                 case Category.Lyrics:
-                    return tag.Lyrics != null ? new Gnosis.Data.Tag(tag.Lyrics, category, source) : null;
+                    return tag.Lyrics != null ? new GTag(tag.Lyrics, category, source) : null;
                 case Category.Title:
-                    return tag.Title != null ? new Gnosis.Data.Tag(tag.Title, category, source) : null;
+                    return tag.Title != null ? new GTag(tag.Title, category, source) : null;
                 case Category.Track:
-                    return tag.Track > 0 ? new Gnosis.Data.Tag(tag.Track.ToString(), category, source) : null;
+                    return tag.Track > 0 ? new GTag(tag.Track.ToString(), category, source) : null;
                 case Category.TrackCount:
-                    return tag.TrackCount > 0 ? new Gnosis.Data.Tag(tag.TrackCount.ToString(), category, source) : null;
+                    return tag.TrackCount > 0 ? new GTag(tag.TrackCount.ToString(), category, source) : null;
                 case Category.Year:
-                    return tag.Year > 0 ? new Gnosis.Data.Tag(tag.Year.ToString(), category, source) : null;
+                    return tag.Year > 0 ? new GTag(tag.Year.ToString(), category, source) : null;
+                case Category.AudioBitrate:
+                    return file.Properties != null && file.Properties.AudioBitrate > 0 ? new GTag(file.Properties.AudioBitrate.ToString(), category, source) : null;
+                case Category.AudioChannels:
+                    return file.Properties != null && file.Properties.AudioChannels > 0 ? new GTag(file.Properties.AudioChannels.ToString(), category, source) : null;
+                case Category.AudioSampleRate:
+                    return file.Properties != null && file.Properties.AudioSampleRate > 0 ? new GTag(file.Properties.AudioSampleRate.ToString(), category, source) : null;
+                case Category.MediaCodec:
+                    return file.Properties != null && file.Properties.Codecs != null && file.Properties.Codecs.Count() > 0 ? new GTag(string.Join(", ", file.Properties.Codecs.Select(x => x.Description)), category, source) : null;
+                case Category.MediaDescription:
+                    return file.Properties != null && file.Properties.Description != null ? new GTag(file.Properties.Description, category, source) : null;
+                case Category.MediaDuration:
+                    return file.Properties != null && file.Properties.Duration != TimeSpan.Zero ? new GTag(file.Properties.Duration.ToString(), category, source) : null;
+                case Category.VideoHeight:
+                    return file.Properties != null && file.Properties.VideoHeight > 0 ? new GTag(file.Properties.VideoHeight.ToString(), category, source) : null;
+                case Category.VideoWidth:
+                    return file.Properties != null && file.Properties.VideoWidth > 0 ? new GTag(file.Properties.VideoWidth.ToString(), category, source) : null;
                 default:
                     return null;
             }
--- Alexandria3/src/Gnosis/Data/Source.cs
+++ Alexandria3/src/Gnosis/Data/Source.cs
@@ -12,14 +12,16 @@ namespace Gnosis.Data
         User = 100,
         
         System = 200,
-        System_DoubleMetaphone = 201,
-        System_Americanized = 202,
+        DoubleMetaphone = 201,
+        Americanized = 202,
         
         Embedded = 400,
-        Embedded_Id3v1 = 401,
-        Embedded_Id3v2 = 402,
-        
+        Id3v1 = 401,
+        Id3v2 = 402,
+        Riff = 403,
+
         Service = 800,
-        Service_LastFm = 801
+        MusicBrainz = 801,
+        LastFm = 802
     }
 }
