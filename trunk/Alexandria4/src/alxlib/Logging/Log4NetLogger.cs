17c8e8b8f7e11fa6df296734a660ee68a2dd0041
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2012-08-01 21:26:22 +0000

    Created alxlib and alxvalidate projects in Alexandria4

---
+++ trunk/Alexandria4/src/alxlib/Logging/Log4NetLogger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using log4net;
+
+namespace Gnosis.Alexandria.Logging
+{
+    public class Log4NetLogger
+        : ILogger
+    {
+        public Log4NetLogger(string name)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            log = LogManager.GetLogger(name);
+        }
+
+        public Log4NetLogger(Type type)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            log = LogManager.GetLogger(type);
+        }
+
+        private readonly ILog log;
+
+        public void Debug(string message)
+        {
+            log.Debug(message);
+        }
+
+        public void Info(string message)
+        {
+            log.Info(message);
+        }
+
+        public void Error(string message)
+        {
+            log.Error(message);
+        }
+
+        public void Error(string message, Exception ex)
+        {
+            log.Error(message, ex);
+        }
+
+        public void Warn(string message)
+        {
+            log.Warn(message);
+        }
+    }
+}
