1c5d984f5ead289a902776635c6cd7dd720e8290
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2012-10-08 05:21:09 +0000

    Defined IPlayerLoader interface and implemented PlayerLoader class Implemented CardInPlayBase Made changes to IPlayer interface and implemented Player class Made changes to Program class to display loaded player classes Created a SampleDeck.txt file for testing with PlayerLoader

---
+++ LotR/src/LotR/IPlayerLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LotR
+{
+    public interface IPlayerLoader
+    {
+        IEnumerable<IPlayerCard> PlayerCards { get; }
+
+        IPlayer Load(string path);
+    }
+}
