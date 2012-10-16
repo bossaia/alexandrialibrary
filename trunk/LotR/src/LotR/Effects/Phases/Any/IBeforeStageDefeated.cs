993a74e96fbe82509271660d343fb40c53ded452
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2012-10-16 00:56:53 +0000

    Finished implementing OldForestRoad location card Worked on implementing quest cards for Passage Through Mirkwood

---
+++ trunk/LotR/src/LotR/Effects/Phases/Any/IBeforeStageDefeated.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using LotR.States.Phases.Any;
+
+namespace LotR.Effects.Phases.Any
+{
+    public interface IBeforeStageDefeated
+    {
+        void BeforeStageDefeated(ICurrentQuestStage state);
+    }
+}
