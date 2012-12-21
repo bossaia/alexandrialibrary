fd5a94c75a2bbaa52da2f2e1e70a4eefd6f17e6d
dan.poage <dan.poage@c130ed34-6229-0410-82e0-19f99743e38a>
2012-12-21 18:18:22 +0000

    Implemented GetRevealedEncounterCard Changed TreacheryInPlay to implement IEncounterInPlay

--- trunk/LotR/src/LotR/States/TreacheryInPlay.cs
+++ trunk/LotR/src/LotR/States/TreacheryInPlay.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using LotR.Cards.Encounter;
+
 using LotR.Cards.Encounter.Treacheries;
 
 namespace LotR.States
 {
     public class TreacheryInPlay
-        : CardInPlay<ITreacheryCard>
+        : CardInPlay<ITreacheryCard>, IEncounterInPlay
     {
         public TreacheryInPlay(IGame game, ITreacheryCard card)
             : base(game, card)
         {
+        }
+
+        IEncounterCard ICardInPlay<IEncounterCard>.Card
+        {
+            get { return Card as IEncounterCard; }
         }
     }
 }
