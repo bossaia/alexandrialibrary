4919bcb787186e1514db0cd19bbbfc7f7f9129a8
dan.poage <dan.poage@gmail.com>
2013-08-04 16:41:07 +0000

    Worked on implementing Catalog page

---
+++ HallOfBeorn/HallOfBeorn/Models/CardType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HallOfBeorn.Models
+{
+    public enum CardType
+    {
+        Quest = 0,
+        Enemy = 1,
+        Location = 2,
+        Treachery = 3,
+        Hero = 4,
+        Ally = 5,
+        Attachment = 6,
+        Event = 7
+    }
+}
