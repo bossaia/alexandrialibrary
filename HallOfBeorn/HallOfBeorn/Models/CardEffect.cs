23a658fb50b46315fc81316ad655eacfc0ee61db
dan.poage <dan.poage@gmail.com>
2013-11-04 02:48:39 +0000

    Made improvements to Catalog, Search and Show features

---
+++ HallOfBeorn/HallOfBeorn/Models/CardEffect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HallOfBeorn.Models
+{
+    public class CardEffect
+    {
+        public CardEffectType EffectType { get; set; }
+        public string Text { get; set; }
+
+        public string Prefix
+        {
+            get
+            {
+                switch (EffectType)
+                {
+                    case CardEffectType.Action:
+                        return "Action: ";
+                    case CardEffectType.Forced:
+                        return "Forced: ";
+                    case CardEffectType.Response:
+                        return "Response: ";
+                    case CardEffectType.Setup:
+                        return "Setup: ";
+                    default:
+                        return string.Empty;
+                }
+            }
+        }
+    }
+}
