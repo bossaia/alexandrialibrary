f52068b893fd9ebfb8b8dd46a56ea82e990c4e80
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2014-04-13 21:18:45 +0000

    Added the encounter set icon for Conflict-at-the-Carrock-Nightmare Finished adding the cards for AJourneyToRhosgobelNightmare

--- trunk/HallOfBeorn/HallOfBeorn/Models/Sets/ConflictAtTheCarrockNightmare.cs
+++ trunk/HallOfBeorn/HallOfBeorn/Models/Sets/ConflictAtTheCarrockNightmare.cs
@@ -19,7 +19,7 @@ namespace HallOfBeorn.Models.Sets
                 Title = "Conflict at the Carrock Nightmare",
                 HasSecondImage = true,
                 ImageType = Models.ImageType.Png,
-                Id = "3AB0C72E-CF30-4486-A8B4-EE816BBFC90F",
+                Id = "2E094DA7-6760-45AB-9E2E-E2BAA5F58BE7",
                 CardType = CardType.Nightmare_Setup,
                 Text =
 @"You are playing in Nightmare mode.
@@ -66,75 +66,180 @@ Finally, flip this setup card over and place it next to the quest deck. Its effe
             });
             Cards.Add(new Card()
             {
-                Title = "",
+                Title = "Savage Hill-troll",
                 ImageType = Models.ImageType.Png,
-                Id = "",
+                Id = "8028643D-4E92-4EC0-B001-2BC5789E09C6",
                 CardType = CardType.Enemy,
-                EngagementCost = 0,
-                Threat = 0,
-                Attack = 0,
-                Defense = 0,
-                HitPoints = 0,
+                EngagementCost = 21,
+                Threat = 1,
+                Attack = 3,
+                Defense = 2,
+                HitPoints = 6,
                 Traits = new List<string> { "Troll." },
-                Text = "",
-                Shadow = "Shadow:",
+                Text = "While Savage Hill-troll is in the victory display, each Troll enemy gets +1 hit point.",
+                Shadow = "Shadow: If attacking enemy is a Troll, defending character does not ready during the refresh phase this round.",
+                VictoryPoints = 1,
                 EncounterSet = "Conflict at the Carrock Nightmare",
-                Quantity = 1,
+                Quantity = 4,
                 Number = 2
             });
             Cards.Add(new Card()
             {
-                Title = "",
+                Title = "Rob and Bob",
                 ImageType = Models.ImageType.Png,
-                Id = "",
+                Id = "7BC5EAA2-B25B-418F-ACC7-5B755EC429EE",
+                IsUnique = true,
                 CardType = CardType.Enemy,
-                EngagementCost = 0,
-                Threat = 0,
-                Attack = 0,
-                Defense = 0,
-                HitPoints = 0,
+                EngagementCost = 34,
+                Threat = 4,
+                Attack = 5,
+                Defense = 4,
+                HitPoints = 10,
                 Traits = new List<string> { "Troll." },
-                Text = "",
-                Shadow = "Shadow:",
+                Text = "Forced: After Rob and Bob makes its first attack each round, if there are two or more players, it engages the next player and makes an additional attack (discard and deal a new shadow card for this attack).",
+                FlavorText = "Yes, I am afraid trolls do behave like that, even those with only one head each. -The Hobbit",
                 EncounterSet = "Conflict at the Carrock Nightmare",
                 Quantity = 1,
                 Number = 3
             });
             Cards.Add(new Card()
             {
-                Title = "",
+                Title = "Louis",
                 ImageType = Models.ImageType.Png,
-                Id = "",
+                Id = "A3757C06-9FF4-469D-984A-B21B41CCF0CE",
+                IsUnique = true,
                 CardType = CardType.Enemy,
-                EngagementCost = 0,
-                Threat = 0,
-                Attack = 0,
-                Defense = 0,
-                HitPoints = 0,
+                EngagementCost = 34,
+                Threat = 4,
+                Attack = 4,
+                Defense = 2,
+                HitPoints = 10,
                 Traits = new List<string> { "Troll." },
-                Text = "",
-                Shadow = "Shadow:",
+                Text = "Forced: After Louis attacks, the defending player must raise his threat by 3.\r\n\r\nWhile Louis is in the victory display, each Troll enemy gains, \"Forced: After this enemy attacks, the defending player must raise his threat by 1.\"",
+                VictoryPoints = 2,
                 EncounterSet = "Conflict at the Carrock Nightmare",
                 Quantity = 1,
                 Number = 4
             });
             Cards.Add(new Card()
             {
-                Title = "",
+                Title = "Morris",
                 ImageType = Models.ImageType.Png,
-                Id = "",
+                Id = "A1484DD8-DCD3-4D39-BBD6-F23DBBE794D4",
+                IsUnique = true,
                 CardType = CardType.Enemy,
-                EngagementCost = 0,
-                Threat = 0,
-                Attack = 0,
-                Defense = 0,
-                HitPoints = 0,
+                EngagementCost = 34,
+                Threat = 3,
+                Attack = 4,
+                Defense = 2,
+                HitPoints = 10,
                 Traits = new List<string> { "Troll." },
-                Text = "",
-                Shadow = "Shadow:",
+                Keywords = new List<string> { "Immune to player events." },
+                Text = "While Morris is in the victory display, each Troll enemy gets +1 Attack and gains \"Immune to player events.\".",
+                VictoryPoints = 2,
                 EncounterSet = "Conflict at the Carrock Nightmare",
                 Quantity = 1,
                 Number = 5
+            });
+            Cards.Add(new Card()
+            {
+                Title = "Rupert",
+                ImageType = Models.ImageType.Png,
+                Id = "F7CEA583-D1F7-4466-A318-28071E408E29",
+                IsUnique = true,
+                CardType = CardType.Enemy,
+                EngagementCost = 34,
+                Threat = 2,
+                Attack = 5,
+                Defense = 3,
+                HitPoints = 10,
+                Traits = new List<string> { "Troll." },
+                Keywords = new List<string> { "Rupert's Defense cannot be reduced." },
+                Text = "While Rupert is in the victory display, the Defense of each Troll enemy cannot be reduced.",
+                VictoryPoints = 2,
+                EncounterSet = "Conflict at the Carrock Nightmare",
+                Quantity = 1,
+                Number = 6
+            });
+            Cards.Add(new Card()
+            {
+                Title = "Stuart",
+                ImageType = Models.ImageType.Png,
+                Id = "0ECCBF56-5D7D-498B-BC04-D05E7C072979",
+                IsUnique = true,
+                CardType = CardType.Enemy,
+                EngagementCost = 34,
+                Threat = 2,
+                Attack = 4,
+                Defense = 4,
+                HitPoints = 10,
+                Traits = new List<string> { "Troll." },
+                Keywords = new List<string> { "Cannot have player attachments." },
+                Text = "While Stuart is in the victory display, each Troll enemy gets +1 Defense and gains \"Cannot have player attachments.\"",
+                VictoryPoints = 2,
+                EncounterSet = "Conflict at the Carrock Nightmare",
+                Quantity = 1,
+                Number = 7
+            });
+            Cards.Add(new Card()
+            {
+                Title = "Troll Lair",
+                ImageType = Models.ImageType.Png,
+                Id = "A964F86A-7DC9-4892-9A35-A4E665A8AA43",
+                CardType = CardType.Location,
+                Threat = 2,
+                QuestPoints = 5,
+                Traits = new List<string> { "Forest." },
+                Text = "While Troll Lair is in the staging area, players cannot reduce their threat.",
+                Shadow = "Shadow: If attacking enemy is a Troll, remove 4 damage tokens from it.",
+                EncounterSet = "Conflict at the Carrock Nightmare",
+                Quantity = 3,
+                Number = 8
+            });
+            Cards.Add(new Card()
+            {
+                Title = "Hives and Hives",
+                ImageType = Models.ImageType.Png,
+                Id = "3681AF57-1689-445D-A594-E5F0156EC90C",
+                CardType = CardType.Location,
+                Threat = 3,
+                QuestPoints = 3,
+                Traits = new List<string> { "Wilderlands." },
+                Text = "Forced: After any amount of progress is placed on Hives and Hives, deal 1 damage to each character in play.",
+                Shadow = "Shadow: Attacking enemy gets +2 Attack.",
+                FlavorText = "There was a buzzing and a whirring and a droning in the air. Bees were busy everywhere. And such bees! Bilbo had never seen anything like them. -The Hobbit",
+                EncounterSet = "Conflict at the Carrock Nightmare",
+                Quantity = 3,
+                Number = 9
+            });
+            Cards.Add(new Card()
+            {
+                Title = "Time for Supper",
+                ImageType = Models.ImageType.Png,
+                Id = "BF6EFB7A-88A6-4B10-BFB2-38027057332E",
+                CardType = CardType.Treachery,
+                Text = "When Revealed: The first player chooses a hero in any player's discard pile and removes that hero from the game. Then, heal all damage from each Troll enemy in play. If no damage is removed from any Troll enemy as a result of this effect, Time for Supper gains surge.",
+                FlavorText = "In the end they decided to mince them fine and boil them. So they got a black pot, and they took out their knives. -The Hobbit",
+                EncounterSet = "Conflict at the Carrock Nightmare",
+                Quantity = 3,
+                Number = 10
+            });
+            Cards.Add(new Card()
+            {
+                Title = "Grimbeorn's Folly",
+                ImageType = Models.ImageType.Png,
+                HasSecondImage = true,
+                Id = "ED52944F-1499-4A0A-BB84-3CE5C18952DE",
+                CardType = CardType.Quest,
+                StageNumber = 3,
+                QuestPoints = null,
+                Text = "When Revealed: Add Rob and Bob to the staging area. Then, each player must search the encounter deck and discard pile for a non-unique Troll enemy and add it to the staging area, if able. Shuffle the encounter deck.",
+                FlavorText = "Just as you think the fight is over, you find tracks that lead you to what appears to be the leader of this group of Trolls. You can see Grimbeorn the Old, captured and ready to be cooked. It's up to you to save him!",
+                OppositeText = "When Rob and Bob have been defeated, the players have rescued Grimbeon the Old from certain death, and won the game.",
+                OppositeFlavorText = "A nice pickle they were all in now. -The Hobbit",
+                EncounterSet = "Conflict at the Carrock Nightmare",
+                Quantity = 3,
+                Number = 11
             });
         }
     }
