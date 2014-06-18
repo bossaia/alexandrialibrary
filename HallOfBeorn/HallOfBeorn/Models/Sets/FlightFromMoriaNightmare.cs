5b19808e9c9271ad674460e08dc8cabf67789527
dan.poage <dan.poage@gmail.com>
2014-06-18 05:39:39 +0000

    Added images for Khazad-dûm Nightmare Decks. Made change to CardViewModel so that a different image is used for the opposite side of quest cards when SlugIncludesOppositeTitle is true.

--- HallOfBeorn/HallOfBeorn/Models/Sets/FlightFromMoriaNightmare.cs
+++ HallOfBeorn/HallOfBeorn/Models/Sets/FlightFromMoriaNightmare.cs
@@ -57,6 +57,8 @@ Finally, flip this setup card over and place it next to the quest deck. Its effe
             {
                 Title = "Search for an Exit",
                 OppositeTitle = "Pursued By Shadow",
+                HasSecondImage = true,
+                ImageType = Models.ImageType.Png,
                 SlugIncludesOppositeTitle = true,
                 Id = "A1380AE9-1895-48A7-9C5D-FF3D941DD1D9",
                 CardType = CardType.Quest,
@@ -79,6 +81,8 @@ Players may bypass this quest card at the end of the combat phase.",
             {
                 Title = "Search for an Exit",
                 OppositeTitle = "Blocked by Flame",
+                HasSecondImage = true,
+                ImageType = Models.ImageType.Png,
                 SlugIncludesOppositeTitle = true,
                 Id = "1A4E5458-13CC-41B1-8A44-6FC3FF25693E",
                 CardType = CardType.Quest,
@@ -101,6 +105,8 @@ If The ~Nameless Fear has 10 or more damage on it, remove all damage from it and
             {
                 Title = "Search for an Exit",
                 OppositeTitle = "Ghâsh",
+                HasSecondImage = true,
+                ImageType = Models.ImageType.Png,
                 SlugIncludesOppositeTitle = true,
                 Id = "A1EDC2C0-0F79-45C1-BFF6-B314BEDFBC6B",
                 CardType = CardType.Quest,
@@ -123,7 +129,7 @@ Players may bypass this quest card at the end of the combat phase.",
             Cards.Add(new Card()
             {
                 Title = "Swarming Goblins",
-                ImageType = ImageType.Jpg,
+                ImageType = ImageType.Png,
                 Id = "AE6D95BC-A85D-49F9-93D1-58730B3A2982",
                 CardType = CardType.Enemy,
                 EngagementCost = 10,
@@ -142,7 +148,7 @@ Players may bypass this quest card at the end of the combat phase.",
             Cards.Add(new Card()
             {
                 Title = "Coal-Black Orc",
-                ImageType = ImageType.Jpg,
+                ImageType = ImageType.Png,
                 Id = "7DDD73A9-F6E1-4610-B78D-3D349C9CC057",
                 CardType = CardType.Enemy,
                 EngagementCost = 34,
@@ -163,7 +169,7 @@ Players may bypass this quest card at the end of the combat phase.",
             Cards.Add(new Card()
             {
                 Title = "Shadowed Corridor",
-                ImageType = ImageType.Jpg,
+                ImageType = ImageType.Png,
                 Id = "0653EEE7-63F0-4A2B-AB65-B487A842A288",
                 CardType = CardType.Location,
                 Threat = 4,
@@ -179,7 +185,7 @@ Players may bypass this quest card at the end of the combat phase.",
             Cards.Add(new Card()
             {
                 Title = "Dismay and Fear",
-                ImageType = ImageType.Jpg,
+                ImageType = ImageType.Png,
                 Id = "8724509F-5380-42E1-9434-FFFF1B03091D",
                 CardType = CardType.Treachery,
                 Text = "When Revealed: Choose and remove characters from the quest until the total Willpower of characters removed from the quest is equal to or higher than The ~Nameless Fear's Threat.",
@@ -192,7 +198,7 @@ Players may bypass this quest card at the end of the combat phase.",
             Cards.Add(new Card()
             {
                 Title = "An Evil Fortune",
-                ImageType = ImageType.Jpg,
+                ImageType = ImageType.Png,
                 Id = "D69789C5-FA48-44FA-80B2-158134CFEF33",
                 CardType = CardType.Treachery,
                 Traits = new List<string> { "Shadow.", "Flame." },
