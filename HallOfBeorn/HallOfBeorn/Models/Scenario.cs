d4ef44c535233bae2f4fabd29619182a9e6f1903
dan.poage <dan.poage@gmail.com>
2014-07-16 04:24:45 +0000

    Added all of the new files for encounter set and product features.

---
+++ HallOfBeorn/HallOfBeorn/Models/Scenario.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HallOfBeorn.Models
+{
+    public class Scenario
+    {
+        public Scenario()
+        {
+        }
+
+        private readonly List<Card> questCards = new List<Card>();
+        private readonly List<ScenarioCard> scenarioCards = new List<ScenarioCard>();
+
+        public int Number { get; set; }
+        public string GroupName { get; set; }
+        public string Title { get; set; }
+
+        public IEnumerable<Card> QuestCards
+        {
+            get { return questCards.OrderBy(x => x.StageNumber).ToList(); }
+        }
+
+        public IEnumerable<ScenarioCard> ScenarioCards
+        {
+            get { return scenarioCards.OrderBy(x => x.EncounterSet).ThenBy(x => x.Title).ToList(); }
+        }
+
+        public void AddQuestCard(Card card)
+        {
+            questCards.Add(card);
+        }
+
+        public void AddScenarioCard(Card card)
+        {
+            scenarioCards.Add(new ScenarioCard(card));
+        }
+
+        public bool IncludesEncounterSet(string encounterSet)
+        {
+            foreach (var questCard in questCards)
+            {
+                if (questCard.IncludedEncounterSets.Any(x => x.Name == encounterSet))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
