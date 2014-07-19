31866aaf34ec3a1c3d5b335ae73d4ac9504cca8a
dan.poage@gmail.com <dan.poage@gmail.com@c130ed34-6229-0410-82e0-19f99743e38a>
2014-07-19 19:39:18 +0000

    Added NormalizedSet property to EncounterSet to fix a bug with encounter set image paths that contain diacritical characters.

--- trunk/HallOfBeorn/HallOfBeorn/Models/EncounterSetViewModel.cs
+++ trunk/HallOfBeorn/HallOfBeorn/Models/EncounterSetViewModel.cs
@@ -24,7 +24,7 @@ namespace HallOfBeorn.Models
             get
             {
                 if (_encounterSet.Set != null)
-                    return string.Format("/Images/Cards/{0}/{1}.png", _encounterSet.Set.ToUrlSafeString(), _encounterSet.Name.ToUrlSafeString());
+                    return string.Format("/Images/Cards/{0}/{1}.png", _encounterSet.NormalizedSet.ToUrlSafeString(), _encounterSet.Name.ToUrlSafeString());
                 else
                     return string.Empty;
             }
