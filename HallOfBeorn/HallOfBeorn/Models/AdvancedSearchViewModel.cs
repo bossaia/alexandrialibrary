f99b6fceac48aaa3f84c3d138f955e62ca72876a
dan.poage <dan.poage@gmail.com>
2014-08-12 06:16:18 +0000

    Finished adding player cards for TroubleInTharbad

--- HallOfBeorn/HallOfBeorn/Models/AdvancedSearchViewModel.cs
+++ HallOfBeorn/HallOfBeorn/Models/AdvancedSearchViewModel.cs
@@ -8,59 +8,104 @@ namespace HallOfBeorn.Models
 {
     public class AdvancedSearchViewModel
     {
+        public AdvancedSearchViewModel()
+        {
+            AdvancedSearchFilter lastFilter = null;
+            AdvancedSearchFilter filter = null; 
+
+            for (var i = 0; i < 10; i++)
+            {
+                filter = new AdvancedSearchFilter { Number = i, IsEnabled = true };
+
+                if (lastFilter != null)
+                {
+                    lastFilter.Next = filter;
+                }
+
+                _filters.Add(filter);
+
+                lastFilter = filter;
+            }
+        }
+
+        private readonly List<AdvancedSearchFilter> _filters = new List<AdvancedSearchFilter>();
+
         public string Query { get; set; }
 
-        public bool Sets_Any { get; set; }
-        public bool Sets_CoreSet { get; set; }
-        public bool Sets_TheHuntForGollum { get; set; }
-        public bool Sets_ConflictAtTheCarrock { get; set; }
-        public bool Sets_AJoruneyToRhosgobel { get; set; }
-        public bool Sets_TheHillsOfEmynMuil { get; set; }
-        public bool Sets_TheDeadMarshes { get; set; }
-        public bool Sets_ReturnToMirkwood { get; set; }
-        public bool Sets_KhazadDum { get; set; }
-        public bool Sets_RedhornGate { get; set; }
-        public bool Sets_RoadToRivendell { get; set; }
-        public bool Sets_WatcherInTheWater { get; set; }
-        public bool Sets_TheLongDark { get; set; }
-        public bool Sets_FoundationsOfStone { get; set; }
-        public bool Sets_ShadowAndFlame { get; set; }
+        public List<CardViewModel> Cards { get; set; }
 
-        public bool Spheres_Any { get; set; }
-        public bool Spheres_Leadership { get; set; }
-        public bool Spheres_Tactics { get; set; }
-        public bool Spheres_Spirit { get; set; }
-        public bool Spheres_Lore { get; set; }
-        public bool Spheres_Neutral { get; set; }
-
-        public string CostOperator { get; set; }
-        public string CostValue { get; set; }
-
-        public static List<SelectListItem> NumericOperators = new List<SelectListItem> { 
-            new SelectListItem { Text = "=", Value = "=" }, 
-            new SelectListItem { Text = "<", Value = "<" },
-            new SelectListItem { Text = "<=", Value = "<=" },
-            new SelectListItem { Text = ">", Value = ">" },
-            new SelectListItem { Text = ">=", Value = ">=" },
-            new SelectListItem { Text = "not", Value = "not" }
-        };
-
-        public static List<SelectListItem> ResourceCosts = new List<SelectListItem> {
-            new SelectListItem { Text = "Any", Value = "Any" },
-            new SelectListItem { Text = "0", Value = "0" },
-            new SelectListItem { Text = "1", Value = "1" },
-            new SelectListItem { Text = "2", Value = "2" },
-            new SelectListItem { Text = "3", Value = "3" },
-            new SelectListItem { Text = "4", Value = "4" },
-            new SelectListItem { Text = "5", Value = "5" },
-            new SelectListItem { Text = "6", Value = "6" }
-        };
+        public IEnumerable<AdvancedSearchFilter> Filters
+        {
+            get { return _filters; }
+        }
     }
 
-    public enum NumericOperator
+    public enum FilterType
     {
-        Equals = 0,
-        GreaterThan = 1,
-        LessThan = 2
+        Set,
+        Type,
+        Sphere,
+        Cost,
+        Trait,
+        Keyword,
+        Encounter,
+        Artist
+    }
+
+    public class AdvancedSearchFilter
+    {
+        public int Number { get; set; }
+        public bool IsEnabled { get; set; } 
+        public AdvancedSearchFilter Next { get; set; }
+        public FilterType FilterType { get; set; }
+
+        public static IEnumerable<SelectListItem> CardTypes
+        {
+            get { return typeof(CardType).GetSelectListItems(); }
+        }
+
+        public static IEnumerable<SelectListItem> Keywords
+        {
+            get;
+            set;
+        }
+
+        public static IEnumerable<SelectListItem> Traits
+        {
+            get;
+            set;
+        }
+
+        public static IEnumerable<SelectListItem> FilterTypes
+        {
+            get { return typeof(FilterType).GetSelectListItems(); }
+        }
+
+        public static IEnumerable<SelectListItem> CardSets { get; set; }
+
+        public static IEnumerable<SelectListItem> EncounterSets { get; set; }
+
+        public static IEnumerable<SelectListItem> Costs { get; set; }
+
+        public static IEnumerable<SelectListItem> Spheres
+        {
+            get { return typeof(Sphere).GetSelectListItems(); }
+        }
+
+        public static IEnumerable<SelectListItem> Sorts
+        {
+            get { return typeof(Sort).GetSelectListItems(", "); }
+        }
+
+        public static IEnumerable<SelectListItem> Artists
+        {
+            get
+            {
+                yield return new SelectListItem { Text = "Any", Value = "Any" };
+
+                foreach (var artist in HallOfBeorn.Models.Artist.All())
+                    yield return new SelectListItem { Text = artist.Name, Value = artist.Name };
+            }
+        }
     }
 }
--- HallOfBeorn/HallOfBeorn/Models/Sets/TroubleInTharbad.cs
+++ HallOfBeorn/HallOfBeorn/Models/Sets/TroubleInTharbad.cs
@@ -47,6 +47,7 @@ namespace HallOfBeorn.Models.Sets
                 PublicImageURL = "http://www.cardgamedb.com/forums/uploads/lotr/hearld-of-anorien-trouble-in-tharbad-57.jpg",
                 Id = "3B790E51-01A5-4305-A273-8694964AFDAD",
                 CardType = CardType.Ally,
+                Sphere = Models.Sphere.Leadership,
                 ResourceCost = 2,
                 Willpower = 1,
                 Attack = 0,
@@ -58,6 +59,143 @@ namespace HallOfBeorn.Models.Sets
                 Number = 57,
                 Quantity = 3,
                 Artist = Artist.Adam_Lane
+            });
+            Cards.Add(new Card()
+            {
+                Title = "O Lórien!",
+                NormalizedTitle = "O Lorien!",
+                ImageType = ImageType.Jpg,
+                PublicImageURL = "http://www.cardgamedb.com/forums/uploads/lotr/o-lorien-trouble-in-tharbad-58.jpg",
+                Id = "879112FB-689B-45AA-BD4A-DE7FDB1D31AB",
+                CardType = CardType.Attachment,
+                Sphere = Models.Sphere.Leadership,
+                ResourceCost = 1,
+                Traits = new List<string> { "Song." },
+                Keywords = new List<string> { "Attach to a hero." },
+                Text = "Action: Exhaust O Lorien! to lower the cost of the next Silvan ally played this phase by 1 (to a minimum of 1).",
+                FlavorText = "\"I sang of leaves, of leaves of gold, and\r\nleaves of gold there grew:\r\nOf wind I sang, a wind there came and in the\r\nbranches blew.\" -Galadriel, The Fellowship of the Ring",
+                Number = 58,
+                Quantity = 3,
+                Artist = Artist.Carolina_Eade
+            });
+            Cards.Add(new Card()
+            {
+                Title = "Gwaihir",
+                ImageType = Models.ImageType.Jpg,
+                PublicImageURL = "http://www.cardgamedb.com/forums/uploads/lotr/gwaihir-trouble-in-tharbad-59.jpg",
+                Id = "AAD1F231-94C0-4AF7-BD97-F1FA4A04A561",
+                CardType = Models.CardType.Ally,
+                Sphere = Models.Sphere.Tactics,
+                ResourceCost = 5,
+                Willpower = 2,
+                Attack = 3,
+                Defense = 1,
+                HitPoints = 4,
+                Traits = new List<string> { "Creature.", "Eagle." },
+                Keywords = new List<string> { "Cannot have restricted attachments." },
+                Text = "Response: After Gwaihir enters play, search your discard pile for an Eagle ally and put it into play under your control. At the end of the round, if that ally is still in play, add it to your hand.",
+                FlavorText = "\"The North Wind blows, but we shall outfly it.\" -Gwaihir, The Return of the King",
+                Number = 59,
+                Quantity = 3,
+                Artist = Artist.Jake_Murray
+            });
+            Cards.Add(new Card()
+            {
+                Title = "Pursuing the Enemy",
+                ImageType = ImageType.Jpg,
+                PublicImageURL = "http://www.cardgamedb.com/forums/uploads/lotr/pursuing-the-enemy-trouble-in-tharbad-60.jpg",
+                Id = "29FFD74F-E9E7-43EA-A259-7A22420B94BF",
+                CardType = CardType.Event,
+                Sphere = Sphere.Tactics,
+                ResourceCost = 0,
+                Text = "Action: Return a Silvan ally you control to your hand to choose a player. Deal 1 damage to each enemy engaged with that player.",
+                FlavorText = "The marauding orcs had been waylaid and almost all destroyed; the remnant had fled westward towards the mountains, and were being pursued. -The Fellowship of the Ring",
+                Number = 60,
+                Quantity = 3,
+                Artist = Artist.Cristi_Balanescu
+            });
+            Cards.Add(new Card()
+            {
+                Title = "Courage Awakened",
+                PublicImageURL = "http://www.cardgamedb.com/forums/uploads/lotr/courage-awakened-trouble-in-tharbad-61.jpg",
+                Id = "EE30C4EE-61A8-4A34-A516-B95CB2672F23",
+                CardType = CardType.Event,
+                Sphere = Sphere.Spirit,
+                ResourceCost = 1,
+                Text = "Action: Choose a hero. That hero gets +2 Willpower until the end of the phase. Then, if your threat is 20 or less and this is the first time you played a copy of Courage Awakened this round, return this card to your hand instead of discarding it.",
+                FlavorText = "But the courage that had been awakened in him was now too strong: he could not leave his friends so easily. -The Fellowship of the Ring",
+                Number = 61,
+                Quantity = 3,
+                Artist = Artist.Romana_Kendelic
+            });
+            Cards.Add(new Card()
+            {
+                Title = "Free to Choose",
+                PublicImageURL = "http://www.cardgamedb.com/forums/uploads/lotr/free-to-choose-trouble-in-tharbad-62.jpg",
+                Id = "B200B774-99B8-43F6-BFBA-2EF9063270D3",
+                CardType = CardType.Event,
+                Sphere = Sphere.Spirit,
+                ResourceCost = 0,
+                Text = "Response: After your threat is raised by an encounter card effect, reduce your threat by an equal amount.",
+                FlavorText = "Suddenly he was aware of himself again. Frodo, neither the Voice nor the Eye; free to choose, and with one remaining instant in which to do so. -The Fellowship of the Ring",
+                Number = 62,
+                Quantity = 3,
+                Artist = Artist.Romana_Kendelic
+            });
+            Cards.Add(new Card()
+            {
+                Title = "Galadhrim Minstrel",
+                PublicImageURL = "http://www.cardgamedb.com/forums/uploads/lotr/galadhrim-minstrel-trouble-in-tharbad-63.jpg",
+                Id = "7894DA0E-F6DF-4BFF-8128-B02E66371728",
+                CardType = CardType.Ally,
+                Sphere = Sphere.Lore,
+                ResourceCost = 2,
+                Willpower = 1,
+                Attack = 0,
+                Defense = 0,
+                HitPoints = 1,
+                Traits = new List<string> { "Silvan.", "Minstrel." },
+                Text = "Response: After Galadhrim Minstrel enters play, search the top five cards of your deck for an event card and add it to your hand. Shuffle the other cards back into your deck.",
+                FlavorText = "...the language was that of Elven-song and spoke of things little known on Middle-earth. -The Fellowship of the Ring",
+                Number = 63,
+                Quantity = 3,
+                Artist = Artist.Arden_Beckwith
+            });
+            Cards.Add(new Card()
+            {
+                Title = "Lembas",
+                PublicImageURL = "http://www.cardgamedb.com/forums/uploads/lotr/lembas-trouble-in-tharbad-64.jpg",
+                Id = "81A13DA7-EFE9-483F-B53D-17EAD498D5B2",
+                CardType = CardType.Attachment,
+                Sphere = Sphere.Lore,
+                ResourceCost = 1,
+                Traits = new List<string> { "Item." },
+                Keywords = new List<string> { "Play only if you control a Noldor or Silvan hero.", "Attach to a hero." },
+                Text = "Action: Discard Lembas to ready attached hero and heal 3 damage from it.",
+                FlavorText = "\"...it is more strengthing than any food made by Men, and it is more pleasant than cram, by all accounts. -Lórien Elf, The Fellowship of the Ring",
+                Number = 64,
+                Quantity = 3,
+                Artist = Artist.Sara_Biddle
+            });
+            Cards.Add(new Card()
+            {
+                Title = "Defender of the Naith",
+                PublicImageURL = "http://www.cardgamedb.com/forums/uploads/lotr/defender-of-naith-trouble-in-tharbad-65.jpg",
+                Id = "EEB587DB-D1F6-49A5-91A3-136F14B97320",
+                CardType = CardType.Ally,
+                Sphere = Sphere.Neutral,
+                ResourceCost = 3,
+                Willpower = 0,
+                Attack = 1,
+                Defense = 2,
+                HitPoints = 2,
+                Traits = new List<string> { "Silvan.", "Warrior." },
+                Keywords = new List<string> { "Sentinel." },
+                Text = "Response: After a Silvan ally you control leaves play, ready Defender of the Naith.",
+                FlavorText = "\"We have been keeping watch on the rivers, ever since we saw a great troop of Orcs going North toward Moria...\" -Haldir, The Fellowship of the Ring",
+                Number = 65,
+                Quantity = 3,
+                Artist = Artist.Christine_Griffin
             });
         }
     }
